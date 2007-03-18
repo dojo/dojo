f2eaa6bc232fc45a4c662ce07b84636cd7546e47
Alex Russell <slightlyoff@chromium.org>
2007-03-18 07:00:51 +0000

    updating the throw-away bootstrapper to reference the new locations of the loader files. Refs #2500
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@7645 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo.js
+++ dojo.js
@@ -1,30 +1,33 @@
 if(typeof dojo == "undefined"){
-	dj_usingBootstrap = true; //Needed for bootstrap2.js to work properly.
 	(function(){
 		var hostEnv = "browser";
 		var isRhino = false;
 		var isSpidermonkey = false;
-		var isDashboard = false;
-		if((typeof this["load"] == "function")&&((typeof this["Packages"] == "function")||(typeof this["Packages"] == "object"))){
+		if(
+			(typeof this["load"] == "function")&&
+			(
+				(typeof this["Packages"] == "function")||
+				(typeof this["Packages"] == "object")
+			)
+		){
 			var isRhino = true;
 			hostEnv = "rhino";
 		}else if(typeof this["load"] == "function"){
 			isSpidermonkey  = true;
 			hostEnv = "spidermonkey";
-		}else if(window.widget){
-			isDashboard = true;
-			hostEnv = "dashboard";
 		}
-		var tmps = ["bootstrap1.js", "loader.js", "hostenv_"+hostEnv+".js"];
+		var tmps = ["bootstrap.js", "loader.js", "hostenv_"+hostEnv+".js"];
 	
-		if((this["djConfig"])&&((djConfig["forceXDomain"])||(djConfig["useXDomain"]))){
+		if(
+			(this["djConfig"])&&
+			(
+				(djConfig["forceXDomain"])||
+				(djConfig["useXDomain"])
+			)
+		){
 			tmps.push("loader_xd.js");
 		}
 	
-		if(hostEnv == "dashboard"){
-			tmps.splice(1, 0, "hostenv_browser.js");
-		}
-
 		if((this["djConfig"])&&(djConfig["baseScriptUri"])){
 			var root = djConfig["baseScriptUri"];
 		}else if((this["djConfig"])&&(djConfig["baseRelativePath"])){
@@ -42,14 +45,14 @@ if(typeof dojo == "undefined"){
 			// attempt to figure out the path to dojo if it isn't set in the config
 			if((this["document"])&&(this["document"]["getElementsByTagName"])){
 				var scripts = document.getElementsByTagName("script");
-				var rePkg = /(__package__|dojo)\.js([\?\.]|$)/i;
-				for(var i = 0; i < scripts.length; i++) {
+				var rePkg = /dojo\.js([\?\.]|$)/i;
+				for(var i = 0; i < scripts.length; i++){
 					var src = scripts[i].getAttribute("src");
-					if(!src) { continue; }
+					if(!src){ continue; }
 					var m = src.match(rePkg);
-					if(m) {
+					if(m){
 						root = src.substring(0, m.index);
-						if(!this["djConfig"]) { djConfig = {}; }
+						if(!this["djConfig"]){ djConfig = {}; }
 						djConfig["baseScriptUri"] = djConfig["baseRelativePath"] = root;
 						break;
 					}
@@ -63,13 +66,13 @@ if(typeof dojo == "undefined"){
 		}
 	
 		for(var x=0; x < tmps.length; x++){
-			var spath = loaderRoot+"src/"+tmps[x];
+			var spath = loaderRoot+"_base/_loader/"+tmps[x];
 			if(isRhino||isSpidermonkey){
 				load(spath);
-			} else {
-				try {
+			}else{
+				try{
 					document.write("<scr"+"ipt type='text/javascript' src='"+spath+"'></scr"+"ipt>");
-				} catch (e) {
+				}catch(e){
 					var script = document.createElement("script");
 					script.src = spath;
 					document.getElementsByTagName("head")[0].appendChild(script);

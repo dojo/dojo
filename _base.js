e65c000028c73f9e4215e8c41dc8f4ae00b5ae98
Alex Russell <slightlyoff@chromium.org>
2007-03-20 07:45:03 +0000

    add NodeList and query to the Base. Refs #2501
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@7696 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base.js
+++ _base.js
@@ -2,5 +2,7 @@ dojo.require("dojo._base.lang");
 dojo.require("dojo._base.Deferred");
 dojo.require("dojo._base.json");
 dojo.require("dojo._base.array");
+dojo.require("dojo._base.NodeList");
+dojo.require("dojo._base.query");
 
 // dojo.requireIf(dojo.isBrowser, "dojo._base.cookie");

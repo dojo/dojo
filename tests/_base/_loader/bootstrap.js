8009f4ba137bfcea8599fd72d265cc8c4db6312f
James Burke <jrburke@gmail.com>
2007-03-20 06:06:32 +0000

    Refs #2500. Tests for dojo.getObject()
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@7691 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/_base/_loader/bootstrap.js
+++ tests/_base/_loader/bootstrap.js
@@ -14,6 +14,56 @@ tests.register("tests._base._loader.bootstrap",
 		},
 
 		{
+			name: "getObject",
+			setUp: function(){
+				//Set an object in global scope.
+				dojo.global().globalValue = {
+					color: "blue",
+					size: 20
+				};
+				
+				//Set up an object in a specific scope.
+				this.foo = {
+					bar: {
+						color: "red",
+						size: 100
+					}
+				};
+			},
+			runTest: function(t){
+				//Test for existing object using global as root path.
+				var globalVar = dojo.getObject("globalValue");
+				t.assertTrue(typeof(globalVar) == "object");
+				t.assertEqual("blue", globalVar.color);
+				t.assertEqual(20, globalVar.size);
+				t.assertEqual("blue", dojo.getObject("globalValue.color"));
+				
+				//Test for non-existent object using global as root path.
+				//Then create it.
+				t.assertFalse(dojo.getObject("something.thatisNew"));
+				t.assertTrue(typeof(dojo.getObject("something.thatisNew", true)) == "object");
+				
+				//Test for existing object using another object as root path.
+				var scopedVar = dojo.getObject("foo.bar", false, this);
+				t.assertTrue(typeof(scopedVar) == "object");
+				t.assertEqual("red", scopedVar.color);
+				t.assertEqual(100, scopedVar.size);
+				t.assertEqual("red", dojo.getObject("foo.bar.color", true, this));
+				
+				//Test for existing object using another object as root path.
+				//Then create it.
+				t.assertFalse(dojo.getObject("something.thatisNew", false, this));
+				t.assertTrue(typeof(dojo.getObject("something.thatisNew", true, this)) == "object");
+			},
+			tearDown: function(){
+				//Clean up global object that should not exist if
+				//the test is re-run.
+				delete dojo.global().something;
+				delete this.something;
+			}
+		},
+		
+		{
 			name: "exists",
 			setUp: function(){
 				this.foo = {

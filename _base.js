24a8e726392f624587febd4fea8806f2d7b7e955
Alex Russell <slightlyoff@chromium.org>
2007-03-26 22:37:41 +0000

    fixes requireIf's for rhino testing. Refs #2501
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@7798 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base.js
+++ _base.js
@@ -2,7 +2,10 @@ dojo.require("dojo._base.lang");
 dojo.require("dojo._base.Deferred");
 dojo.require("dojo._base.json");
 dojo.require("dojo._base.array");
-dojo.require("dojo._base.NodeList");
-dojo.require("dojo._base.query");
+// dojo.requireIf(dojo.isBrowser, "dojo._base.browserConnect");
+// dojo.requireIf(dojo, "dojo._base.connect");
+// dojo.requireIf(dojo.isBrowser, "dojo._base.html");
+dojo.requireIf(dojo.isBrowser, "dojo._base.NodeList");
+dojo.requireIf(dojo.isBrowser, "dojo._base.query");
 
 // dojo.requireIf(dojo.isBrowser, "dojo._base.cookie");

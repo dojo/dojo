1a4f7a759451918c43e9f85df78605845064651f
Alex Russell <slightlyoff@chromium.org>
2007-03-30 06:09:02 +0000

    refs #2615
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@7845 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tests/_base/query.js
@@ -0,0 +1,6 @@
+dojo.provide("tests._base.query");
+if(dojo.isBrowser){
+	// FIXME: need moduleUrl!!!
+	// alert("about to register: "+dojo.moduleUrl("tests", "_base/query.html"));
+	tests.registerUrl("tests._base.query", dojo.moduleUrl("tests", "_base/query.html"));
+}

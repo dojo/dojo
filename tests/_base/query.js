36ceccc22d7bb41d20bc9465232b63cd7645fc79
Alex Russell <slightlyoff@chromium.org>
2007-04-01 08:36:39 +0000

    beginning of tests for the NodeList class. Refs #2615
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@7888 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/_base/query.js
+++ tests/_base/query.js
@@ -3,4 +3,5 @@ if(dojo.isBrowser){
 	// FIXME: need moduleUrl!!!
 	// alert("about to register: "+dojo.moduleUrl("tests", "_base/query.html"));
 	tests.registerUrl("tests._base.query", dojo.moduleUrl("tests", "_base/query.html"));
+	tests.registerUrl("tests._base.NodeList", dojo.moduleUrl("tests", "_base/NodeList.html"));
 }

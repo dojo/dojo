c33a5e69916d0a415b8eb1463dc2d82cd6162049
James Burke <jrburke@gmail.com>
2007-04-08 05:24:11 +0000

    Refs #2608. Fixes json tests so that they pass in Rhino.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@7996 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/_base/json.js
+++ tests/_base/json.js
@@ -2,26 +2,25 @@ dojo.provide("tests._base.json");
 
 tests.register("tests._base.json", 
 	[
-		// convert a moderately complex object to json in both dense and pretty modes
-		function toJson(t){
-			var testObj = {a:"a", b:1, c:"c", d:"d", e:{e1:"e1", e2:2}, f:[1,2,3], g:"g",h:{h1:{h2:{h3:"h3"}}}};
-			var denseOutput = '{"a":"a","b":1,"c":"c","d":"d","e":{"e1":"e1","e2":2},"f":[1,2,3],"g":"g","h":{"h1":{"h2":{"h3":"h3"}}}}';
-			var prettyOutput = '{\n\t"a":"a",\n\t"b":1,\n\t"c":"c",\n\t"d":"d",\n\t"e":{\n\t\t"e1":"e1",\n\t\t"e2":2\n\t},\n\t"f":[\n\t\t1,\n\t\t2,\n\t\t3\n\t],\n\t"g":"g",\n\t"h":{\n\t\t"h1":{\n\t\t\t"h2":{\n\t\t\t\t"h3":"h3"\n\t\t\t}\n\t\t}\n\t}\n}';
-			dojo.toJsonIndentStr = "\t";
-			
-			t.assertTrue(dojo.toJson(testObj) == denseOutput);
-			t.assertTrue(dojo.toJson(testObj, true) == prettyOutput);
-		},
+		//Not testing dojo.toJson() on its own since Rhino will output the object properties in a different order.
+		//Still valid json, but just in a different order than the source string.
 
-		// take some json code, put convert it to objects, then put it back into json
+		// take a json-compatible object, convert it to a json string, then put it back into json.
 		function toAndFromJson(t){
 			var testObj = {a:"a", b:1, c:"c", d:"d", e:{e1:"e1", e2:2}, f:[1,2,3], g:"g",h:{h1:{h2:{h3:"h3"}}}};
-			var denseOutput = '{"a":"a","b":1,"c":"c","d":"d","e":{"e1":"e1","e2":2},"f":[1,2,3],"g":"g","h":{"h1":{"h2":{"h3":"h3"}}}}';
-			var prettyOutput = '{\n\t"a":"a",\n\t"b":1,\n\t"c":"c",\n\t"d":"d",\n\t"e":{\n\t\t"e1":"e1",\n\t\t"e2":2\n\t},\n\t"f":[\n\t\t1,\n\t\t2,\n\t\t3\n\t],\n\t"g":"g",\n\t"h":{\n\t\t"h1":{\n\t\t\t"h2":{\n\t\t\t\t"h3":"h3"\n\t\t\t}\n\t\t}\n\t}\n}';
-			dojo.toJsonIndentStr = "\t";
-			
-			t.assertTrue(dojo.toJson(dojo.fromJson(denseOutput)) == denseOutput);
-			t.assertTrue(dojo.toJson(dojo.fromJson(prettyOutput), true) == prettyOutput);
+
+			var mirrorObj = dojo.fromJson(dojo.toJson(testObj));
+			t.assertEqual("a", mirrorObj.a);
+			t.assertEqual(1, mirrorObj.b);
+			t.assertEqual("c", mirrorObj.c);
+			t.assertEqual("d", mirrorObj.d);
+			t.assertEqual("e1", mirrorObj.e.e1);
+			t.assertEqual(2, mirrorObj.e.e2);
+			t.assertEqual(1, mirrorObj.f[0]);
+			t.assertEqual(2, mirrorObj.f[1]);
+			t.assertEqual(3, mirrorObj.f[2]);
+			t.assertEqual("g", mirrorObj.g);
+			t.assertEqual("h3", mirrorObj.h.h1.h2.h3);
 		}
 	]
 );

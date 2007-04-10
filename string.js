be900618945dc516f04bc7fdc5adfa297fc1f9ee
Adam Peller <github@peller.org>
2007-04-10 03:06:46 +0000

    Port dojo.number and currency along with string and regexp dependencies.  Refs #2719
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8028 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ string.js
@@ -0,0 +1,48 @@
+dojo.provide("dojo.string");
+
+dojo.string.pad = function(/*String*/text, /*int*/size, /*String?*/ch, /*boolean?*/end) {
+	// summary:
+	//	Pad a string to guarantee that it is at least 'size' length by filling with the character 'c' at either the 
+	//	start or end of the string. Pads at the start, by default.
+	// text: the string to pad
+	// size: length to provide padding
+	// ch: character to pad, defaults to '0'
+	// end: adds padding at the end if true, otherwise pads at start
+
+	var out = String(text);
+	if(!ch) {
+		ch = '0';
+	}
+	while(out.length < size) {
+		if(!dir) {
+			out = ch + out;
+		} else {
+			out += ch;
+		}
+	}
+	return out;	// String
+}
+
+dojo.string.substitute = function(/*String*/template, /*Object or Array*/map, /*Function?*/transform, /*Object?*/thisObject){
+// summary:
+//	Performs parameterized substitutions on a string. Throws an exception if any parameter is unmatched.
+//
+// description:
+//	For example,
+//		dojo.string.substitute("File '${0}' is not found in directory '${1}'.",["foo.html","/temp"]);
+//		dojo.string.substitute("File '${name}' is not found in directory '${info.dir}'.",{name: "foo.html", info: {dir: "/temp"}});
+//	both return
+//		"File 'foo.html' is not found in directory '/temp'."
+//
+// template: a string with expressions in the form ${key} to be replaced or ${key:format} which specifies a format function.  NOTE syntax has changed from %{key}
+// map: where to look for substitutions
+// transform: a function to process all parameters before substitution takes place, e.g. dojo.string.encodeXML
+// thisObject: where to look for optional format function; default to the global namespace
+
+	return template.replace(/\$\{([^\s\:\}]+)(?:\:(\S+))?\}/g, function(match, key, format){
+		var value = dojo.getObject(key,false,map).toString();
+		if(format){ value = dojo.getObject(format,false,thisObject)(value);}
+		if(transform){ value = transform(value); }
+		return value;
+	}); // string
+};

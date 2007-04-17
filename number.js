2c8095533e8b818b74f1e198561df91042b9336a
Alex Russell <slightlyoff@chromium.org>
2007-04-17 04:52:04 +0000

    starting to make comments readable. Still need more work on this front. Refs #2749
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8146 560b804f-0ae3-0310-86f3-f6aa0a117693

--- number.js
+++ number.js
@@ -6,25 +6,25 @@ dojo.require("dojo.string");
 dojo.require("dojo.regexp");
 
 dojo.number.format = function(/*Number*/value, /*Object?*/options){
-// summary:
-//		Format a Number as a String, using locale-specific settings
-//
-// description:
-//		Create a string from a Number using a known localized pattern.
-//		Formatting patterns appropriate to the locale are chosen from the CLDR http://unicode.org/cldr
-//		as well as the appropriate symbols and delimiters.  See http://www.unicode.org/reports/tr35/#Number_Elements
-//
-// value:
-//		the number to be formatted.
-//
-// options: object {pattern: String?, type: String?, places: Number?, round: Number?, currency: String?, symbol: String?, locale: String?}
-//		pattern- override formatting pattern with this string (see dojo.number.applyPattern)
-//		type- choose a format type based on the locale from the following: decimal, scientific, percent, currency. decimal by default.
-//		places- fixed number of decimal places to show.  This overrides any information in the provided pattern.
-//		round- 5 rounds to nearest .5; 0 rounds to nearest whole (default). -1 means don't round.
-//		currency- iso4217 currency code
-//		symbol- localized currency symbol
-//		locale- override the locale used to determine formatting rules
+	// summary:
+	//		Format a Number as a String, using locale-specific settings
+	//
+	// description:
+	//		Create a string from a Number using a known localized pattern.
+	//		Formatting patterns appropriate to the locale are chosen from the CLDR http://unicode.org/cldr
+	//		as well as the appropriate symbols and delimiters.  See http://www.unicode.org/reports/tr35/#Number_Elements
+	//
+	// value:
+	//		the number to be formatted.
+	//
+	// options: object {pattern: String?, type: String?, places: Number?, round: Number?, currency: String?, symbol: String?, locale: String?}
+	//		pattern- override formatting pattern with this string (see dojo.number.applyPattern)
+	//		type- choose a format type based on the locale from the following: decimal, scientific, percent, currency. decimal by default.
+	//		places- fixed number of decimal places to show.  This overrides any information in the provided pattern.
+	//		round- 5 rounds to nearest .5; 0 rounds to nearest whole (default). -1 means don't round.
+	//		currency- iso4217 currency code
+	//		symbol- localized currency symbol
+	//		locale- override the locale used to determine formatting rules
 
 	options = dojo.mixin({}, options || {});
 	var locale = dojo.i18n.normalizeLocale(options.locale);
@@ -104,14 +104,19 @@ dojo.number.round = function(/*Number*/value, /*Number*/places, /*Number?*/multi
 }
 
 dojo.number.formatAbsolute = function(/*Number*/value, /*String*/pattern, /*Object?*/options){
-	// summary: Apply numeric pattern to absolute value using options.  Gives no consideration to local customs
-	// value: the number to be formatted, ignores sign
-	// pattern: the number portion of a pattern (e.g. #,##0.00)
-	// options: object {decimal: String?, group: String?, places: Number?}
-	//  decimal- the decimal separator
-	//  group- the group separator
-	//  places- number of decimal places
-	//  round- 5 rounds to nearest .5; 0 rounds to nearest whole (default). -1 means don't round.
+	// summary: 
+	//		Apply numeric pattern to absolute value using options. Gives no
+	//		consideration to local customs.
+	// value:
+	//		the number to be formatted, ignores sign
+	// pattern:
+	//		the number portion of a pattern (e.g. #,##0.00)
+	// options:
+	//		object {decimal: String?, group: String?, places: Number?}
+	//  		decimal: the decimal separator
+	//  		group: the group separator
+	//  		places: number of decimal places
+	//  		round: 5 rounds to nearest .5; 0 rounds to nearest whole (default). -1 means don't round.
 	options = options || {};
 	if(options.places === true){options.places=0;}
 	if(options.places === Infinity){options.places=6;} // avoid a loop; pick a limit
@@ -184,19 +189,20 @@ dojo.number.formatAbsolute = function(/*Number*/value, /*String*/pattern, /*Obje
 };
 
 dojo.number.regexp = function(/*Object?*/options){
-//
-// summary:
-//		Builds the regular needed to parse a number
-//
-// description:
-//		returns regular expression with positive and negative match, group and decimal separators
-//
-// options: object {pattern: String, type: String locale: String, strict: Boolean, places: mixed}
-//		pattern- override pattern with this string
-//		type- choose a format type based on the locale from the following: decimal, scientific, percent, currency. decimal by default.
-//		locale- override the locale used to determine formatting rules
-//		strict- strict parsing, false by default
-//		places- number of decimal places to accept: Infinity, a positive number, or a range "n,m"
+	//
+	// summary:
+	//		Builds the regular needed to parse a number
+	//
+	// description:
+	//		returns regular expression with positive and negative match, group
+	//		and decimal separators
+	//
+	// options: object {pattern: String, type: String locale: String, strict: Boolean, places: mixed}
+	//		pattern- override pattern with this string
+	//		type- choose a format type based on the locale from the following: decimal, scientific, percent, currency. decimal by default.
+	//		locale- override the locale used to determine formatting rules
+	//		strict- strict parsing, false by default
+	//		places- number of decimal places to accept: Infinity, a positive number, or a range "n,m"
 	return dojo.number._parseInfo(options).regexp; // String
 }
 
@@ -273,24 +279,24 @@ dojo.number._parseInfo = function(/*Object?*/options){
 }
 
 dojo.number.parse = function(/*String*/expression, /*Object?*/options){
-//
-// summary:
-//		Convert a properly formatted string to a primitive Number,
-//		using locale-specific settings.
-//
-// description:
-//		Create a Number from a string using a known localized pattern.
-//		Formatting patterns are chosen appropriate to the locale.
-//		Formatting patterns are implemented using the syntax described at *URL*
-//
-// expression: A string representation of a Number
-//
-// options: object {pattern: string, locale: string, strict: boolean}
-//		pattern- override pattern with this string
-//		type- choose a format type based on the locale from the following: decimal, scientific, percent, currency. decimal by default.
-//		locale- override the locale used to determine formatting rules
-//		strict- strict parsing, false by default
-//		currency- object with currency information
+	// summary:
+	//		Convert a properly formatted string to a primitive Number,
+	//		using locale-specific settings.
+	//
+	// description:
+	//		Create a Number from a string using a known localized pattern.
+	//		Formatting patterns are chosen appropriate to the locale.
+	//		Formatting patterns are implemented using the syntax described at
+	//		*URL*
+	//
+	// expression: A string representation of a Number
+	//
+	// options: object {pattern: string, locale: string, strict: boolean}
+	//		pattern- override pattern with this string
+	//		type- choose a format type based on the locale from the following: decimal, scientific, percent, currency. decimal by default.
+	//		locale- override the locale used to determine formatting rules
+	//		strict- strict parsing, false by default
+	//		currency- object with currency information
 
 	var info = dojo.number._parseInfo(options);
 
@@ -316,16 +322,25 @@ dojo.number._realNumberRegexp = function(/*Object?*/flags){
 	// summary: Builds a regular expression to match a real number in exponential notation
 	//
 	// flags:An object
-	//    flags.places  The integer number of decimal places or a range given as "n,m"
-	//      If not given, the decimal part is optional and the number of places is unlimited.
-	//    flags.decimal  A string for the character used as the decimal point.  Default is ".".
-	//    flags.fractional  Whether decimal places are allowed.
-	//      Can be true, false, or [true, false].  Default is [true, false]
-	//    flags.exponent  Express in exponential notation.  Can be true, false, or [true, false].
-	//      Default is [true, false], (i.e. will match if the exponential part is present are not).
-	//    flags.eSigned  The leading plus-or-minus sign on the exponent.  Can be true, false, 
-	//      or [true, false].  Default is [true, false], (i.e. will match if it is signed or unsigned).
-	//    flags in regexp.integer can be applied.
+	//		flags.places:
+	//			The integer number of decimal places or a range given as "n,m".
+	//			If not given, the decimal part is optional and the number of
+	//			places is unlimited.
+	//		flags.decimal:
+	//			A string for the character used as the decimal point.  Default
+	//			is ".".
+	//    flags.fractional:
+	//			Whether decimal places are allowed.  Can be true, false, or
+	//			[true, false].  Default is [true, false]
+	//    flags.exponent:
+	//			Express in exponential notation.  Can be true, false, or [true,
+	//			false]. Default is [true, false], (i.e. will match if the
+	//			exponential part is present are not).
+	//    flags.eSigned:
+	//			The leading plus-or-minus sign on the exponent.  Can be true,
+	//			false, or [true, false].  Default is [true, false], (i.e. will
+	//			match if it is signed or unsigned).  flags in regexp.integer
+	//			can be applied.
 
 	// assign default values to missing paramters
 	flags = (typeof flags == "object") ? flags : {};

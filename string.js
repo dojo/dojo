cf2cc993d917ae065aabbd528fa6847d49bf137b
Alex Russell <slightlyoff@chromium.org>
2007-04-28 21:17:08 +0000

    fixing formatting for comments. C'mon people, if it doesn't look readable to you, it's not. Refs #2719
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8343 560b804f-0ae3-0310-86f3-f6aa0a117693

--- string.js
+++ string.js
@@ -2,8 +2,9 @@ dojo.provide("dojo.string");
 
 dojo.string.pad = function(/*String*/text, /*int*/size, /*String?*/ch, /*boolean?*/end){
 	// summary:
-	//	Pad a string to guarantee that it is at least 'size' length by filling with the character 'c' at either the 
-	//	start or end of the string. Pads at the start, by default.
+	//		Pad a string to guarantee that it is at least 'size' length by
+	//		filling with the character 'c' at either the start or end of the
+	//		string. Pads at the start, by default.
 	// text: the string to pad
 	// size: length to provide padding
 	// ch: character to pad, defaults to '0'
@@ -23,21 +24,30 @@ dojo.string.pad = function(/*String*/text, /*int*/size, /*String?*/ch, /*boolean
 	return out;	// String
 }
 
-dojo.string.substitute = function(/*String*/template, /*Object or Array*/map, /*Function?*/transform, /*Object?*/thisObject){
-// summary:
-//	Performs parameterized substitutions on a string. Throws an exception if any parameter is unmatched.
-//
-// description:
-//	For example,
-//		dojo.string.substitute("File '${0}' is not found in directory '${1}'.",["foo.html","/temp"]);
-//		dojo.string.substitute("File '${name}' is not found in directory '${info.dir}'.",{name: "foo.html", info: {dir: "/temp"}});
-//	both return
-//		"File 'foo.html' is not found in directory '/temp'."
-//
-// template: a string with expressions in the form ${key} to be replaced or ${key:format} which specifies a format function.  NOTE syntax has changed from %{key}
-// map: where to look for substitutions
-// transform: a function to process all parameters before substitution takes place, e.g. dojo.string.encodeXML
-// thisObject: where to look for optional format function; default to the global namespace
+dojo.string.substitute = function(	/*String*/template, 
+									/*Object or Array*/map, 
+									/*Function?*/transform, 
+									/*Object?*/thisObject){
+	// summary:
+	//		Performs parameterized substitutions on a string. Throws an
+	//		exception if any parameter is unmatched.
+	// description:
+	//		For example,
+	//			dojo.string.substitute("File '${0}' is not found in directory '${1}'.",["foo.html","/temp"]);
+	//			dojo.string.substitute("File '${name}' is not found in directory '${info.dir}'.",{name: "foo.html", info: {dir: "/temp"}});
+	//		both return
+	//			"File 'foo.html' is not found in directory '/temp'."
+	// template: 
+	//		a string with expressions in the form ${key} to be replaced or
+	//		${key:format} which specifies a format function.  NOTE syntax has
+	//		changed from %{key}
+	// map: where to look for substitutions
+	// transform: 
+	//		a function to process all parameters before substitution takes
+	//		place, e.g. dojo.string.encodeXML
+	// thisObject: 
+	//		where to look for optional format function; default to the global
+	//		namespace
 
 	return template.replace(/\$\{([^\s\:\}]+)(?:\:(\S+))?\}/g, function(match, key, format){
 		var value = dojo.getObject(key,false,map).toString();

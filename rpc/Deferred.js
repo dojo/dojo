6ca8ee18ce121129ee0a656625a7279cac511d08
Dustin Machi <dmachi@dojotoolkit.org>
2007-05-11 04:34:45 +0000

    refs #2915. Reoorg of rpc
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8560 560b804f-0ae3-0310-86f3-f6aa0a117693

--- rpc/Deferred.js
+++
@@ -1,6 +0,0 @@
-dojo.provide("dojo.rpc.Deferred");
-dojo.require("dojo.Deferred");
-
-dojo.deprecated("dojo.rpc.Deferred", "replaced by dojo.Deferred", "0.6");
-dojo.rpc.Deferred = dojo.Deferred;
-dojo.rpc.Deferred.prototype = dojo.Deferred.prototype;
--- rpc/__package__.js
+++
@@ -1,4 +0,0 @@
-dojo.kwCompoundRequire({
-	common: [["dojo.rpc.JsonService", false, false]]
-});
-dojo.provide("dojo.rpc.*");

0ffaa32eec774a742d78a0e3d09823159463b345
Dustin Machi <dmachi@dojotoolkit.org>
2007-05-11 04:36:24 +0000

    refs #2915 . Reorg of rpc
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8562 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tests/resources/testClass.php
@@ -0,0 +1,20 @@
+<?
+class testClass {
+
+	function myecho ($somestring) {
+		return "<P>" . $somestring . "<P>";
+	}
+
+	function contentB () {
+		return "<P>Content B</p>";
+	}
+
+	function contentC () {
+		return "<P>Content C</p>";
+	}
+
+	function add($x,$y) {
+		return $x + $y;
+	}
+}
+?>

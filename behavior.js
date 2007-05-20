a4aab7fde09f7115f484c7b58396045387af946e
Alex Russell <slightlyoff@chromium.org>
2007-05-20 00:39:21 +0000

    adding tests for dojo.behavior. Fixing off-by-one re-appliation error. Fixing topic publication name/action swap. Fixing "found" mapping for singular. topic action values.
    Fixes #3001. New issues w/ dojo.behavior should be filed under separate cover.
    
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8657 560b804f-0ae3-0310-86f3-f6aa0a117693

--- behavior.js
+++ behavior.js
@@ -22,82 +22,88 @@ dojo.behavior = new function(){
 	}
 
 	// FIXME: need a better test so we don't exclude nightly Safari's!
-	this.behaviors = {};
+	this._behaviors = {};
 	this.add = function(behaviorObj){
-		/*	behavior objects are specified in the following format:
-		 *
-		 *	{ 
-		 *	 	"#id": {
-		 *			"found": function(element){
-		 *				// ...
-		 *			},
-		 *
-		 *			"onblah": {targetObj: foo, targetFunc: "bar"},
-		 *
-		 *			"onblarg": "/foo/bar/baz/blarg",
-		 *
-		 *			"onevent": function(evt){
-		 *			},
-		 *
-		 *			"onotherevent: function(evt){
-		 *				// ...
-		 *			}
-		 *		},
-		 *
-		 *		"#id2": {
-		 *			// ...
-		 *		},
-		 *
-		 *		"#id3": function(element){
-		 *			// ...
-		 *		},
-		 *
-		 *		// publish the match on a topic
-		 *		"#id4": "/found/topic/name",
-		 *
-		 *		// match all direct descendants
-		 *		"#id4 > *": function(element){
-		 *			// ...
-		 *		},
-		 *
-		 *		// match the first child node that's an element
-		 *		"#id4 > :first-child": { ... },
-		 *
-		 *		// match the last child node that's an element
-		 *		"#id4 > :last-child":  { ... },
-		 *
-		 *		// all elements of type tagname
-		 *		"tagname": {
-		 *			// ...
-		 *		},
-		 *
-		 *		"tagname1 tagname2 tagname3": {
-		 *			// ...
-		 *		},
-		 *
-		 *		".classname": {
-		 *			// ...
-		 *		},
-		 *
-		 *		"tagname.classname": {
-		 *			// ...
-		 *		},
-		 *	}
-		 *
-		 *	The "found" method is a generalized handler that's called as soon
-		 *	as the node matches the selector. Rules for values that follow also
-		 *	apply to the "found" key.
-		 *	
-		 *	The "on*" handlers are attached with dojo.connect(). 
-		 *
-		 *	If the value corresponding to the ID key is a function and not a
-		 *	list, it's treated as though it was the value of "found".
-		 *
-		 */
+		//	summary:
+		//		add the specified behavior to the list of behaviors which will
+		//		be applied the next time apply() is called. Calls to add() for
+		//		an already existing behavior do not replace the previous rules,
+		//		but are instead additive. New nodes which match the rule will
+		//		have all add()-ed behaviors applied to them when matched.
+		//
+		//	description:
+		//		behavior objects are specified in the following format(s):
+		//		
+		//			{ 
+		//			 	"#id": {
+		//					"found": function(element){
+		//						// ...
+		//					},
+		//			
+		//					"onblah": {targetObj: foo, targetFunc: "bar"},
+		//			
+		//					"onblarg": "/foo/bar/baz/blarg",
+		//			
+		//					"onevent": function(evt){
+		//					},
+		//			
+		//					"onotherevent: function(evt){
+		//						// ...
+		//					}
+		//				},
+		//			
+		//				"#id2": {
+		//					// ...
+		//				},
+		//			
+		//				"#id3": function(element){
+		//					// ...
+		//				},
+		//			
+		//				// publish the match on a topic
+		//				"#id4": "/found/topic/name",
+		//			
+		//				// match all direct descendants
+		//				"#id4 > *": function(element){
+		//					// ...
+		//				},
+		//			
+		//				// match the first child node that's an element
+		//				"#id4 > :first-child": { ... },
+		//			
+		//				// match the last child node that's an element
+		//				"#id4 > :last-child":  { ... },
+		//			
+		//				// all elements of type tagname
+		//				"tagname": {
+		//					// ...
+		//				},
+		//			
+		//				"tagname1 tagname2 tagname3": {
+		//					// ...
+		//				},
+		//			
+		//				".classname": {
+		//					// ...
+		//				},
+		//			
+		//				"tagname.classname": {
+		//					// ...
+		//				},
+		//			}
+		//		
+		//		The "found" method is a generalized handler that's called as soon
+		//		as the node matches the selector. Rules for values that follow also
+		//		apply to the "found" key.
+		//		
+		//		The "on*" handlers are attached with dojo.connect(). 
+		//		
+		//		If the value corresponding to the ID key is a function and not a
+		//		list, it's treated as though it was the value of "found".
 
 		var tmpObj = {};
 		forIn(behaviorObj, this, function(behavior, name){
-			var tBehavior = arrIn(this.behaviors, name);
+			var tBehavior = arrIn(this._behaviors, name);
 			if(typeof tBehavior["id"] != "number"){
 				tBehavior.id = _inc++;
 			}
@@ -112,42 +118,15 @@ dojo.behavior = new function(){
 		});
 	}
 
-	this.apply = function(){
-		forIn(this.behaviors, function(tBehavior, id){
-			var elems = dojo.query(id);
-			dojo.forEach(elems, 
-				function(elem){
-					var runFrom = 0;
-					var bid = "_dj_behavior_"+tBehavior.id;
-					if(typeof elem[bid] == "number"){
-						runFrom = elem[bid];
-						if(runFrom == (tBehavior.length-1)){
-							return;
-						}
-					}
-					// run through the versions, applying newer rules at each step
-
-					for(var x=runFrom, tver; tver = tBehavior[x]; x++){
-						forIn(tver, function(ruleSet, ruleSetName){
-							if(dojo.isArray(ruleSet)){
-								dojo.forEach(ruleSet, function(action){
-									dojo.behavior.applyToNode(elem, action, ruleSetName);
-								});
-							}
-						});
-					}
-
-					// ensure that re-application only adds new rules to the node
-					elem[bid] = tBehavior.length-1;
-				}
-			);
-		});
-	}
-
-
-	this.applyToNode = function(node, action, ruleSetName){
+	var _applyToNode = function(node, action, ruleSetName){
 		if(dojo.isString(action)){
-			// dojo.topic.registerPublisher(action, node, ruleSetName);
+			if(ruleSetName == "found"){
+				dojo.publish(action, [ node ]);
+			}else{
+				dojo.connect(node, ruleSetName, function(){
+					dojo.publish(action, arguments);
+				});
+			}
 		}else if(dojo.isFunction(action)){
 			if(ruleSetName == "found"){
 				action(node);
@@ -155,6 +134,47 @@ dojo.behavior = new function(){
 				dojo.connect(node, ruleSetName, action);
 			}
 		}
+	}
+
+	this.apply = function(){
+		// summary:
+		//		applies all currently registered behaviors to the document,
+		//		taking care to ensure that only incremental updates are made
+		//		since the last time add() or apply() were called. If new
+		//		matching nodes have been added, all rules in a behavior will be
+		//		applied to that node. For previously matched nodes, only
+		//		behaviors which have been added since the last call to apply()
+		//		will be added to the nodes.
+		forIn(this._behaviors, function(tBehavior, id){
+			dojo.query(id).forEach( 
+				function(elem){
+					var runFrom = 0;
+					var bid = "_dj_behavior_"+tBehavior.id;
+					if(typeof elem[bid] == "number"){
+						runFrom = elem[bid];
+						// console.debug(bid, runFrom);
+						if(runFrom == (tBehavior.length)){
+							return;
+						}
+					}
+					// run through the versions, applying newer rules at each step
+
+					for(var x=runFrom, tver; tver = tBehavior[x]; x++){
+						// console.debug(tver);
+						forIn(tver, function(ruleSet, ruleSetName){
+							if(dojo.isArray(ruleSet)){
+								dojo.forEach(ruleSet, function(action){
+									_applyToNode(elem, action, ruleSetName);
+								});
+							}
+						});
+					}
+
+					// ensure that re-application only adds new rules to the node
+					elem[bid] = tBehavior.length;
+				}
+			);
+		});
 	}
 }
 

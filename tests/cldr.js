f88f29c5d3aef7121bca2958de21d27b944a4f15
Adam Peller <github@peller.org>
2007-05-28 01:51:40 +0000

    added some tabs to make Alex happy :-P  dojo.cldr is mostly generated data and therefore really doesn't have any standalone functional tests; dojo.{date,number,currency} ought to include tests which use this data.  Refs #2758
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8773 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/cldr.js
+++ tests/cldr.js
@@ -5,11 +5,11 @@ dojo.require("dojo.cldr.monetary");
 
 tests.register("tests.cldr", 
 	[
-function test_date_getWeekend(t){
-	t.is(6, dojo.cldr.supplemental.getWeekend('en-us').start);
-	t.is(0, dojo.cldr.supplemental.getWeekend('en-us').end);
-	t.is(5, dojo.cldr.supplemental.getWeekend('he-il').start);
-	t.is(6, dojo.cldr.supplemental.getWeekend('he-il').end);
-}
+		function test_date_getWeekend(t){
+			t.is(6, dojo.cldr.supplemental.getWeekend('en-us').start);
+			t.is(0, dojo.cldr.supplemental.getWeekend('en-us').end);
+			t.is(5, dojo.cldr.supplemental.getWeekend('he-il').start);
+			t.is(6, dojo.cldr.supplemental.getWeekend('he-il').end);
+		}
 	]
 );

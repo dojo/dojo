f1f5ab85b84c036ad91dbd27f0ad5275d81932d6
Alex Russell <slightlyoff@chromium.org>
2007-06-04 05:17:45 +0000

    adding Google Gears detection support. Refs #3252
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8892 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/hostenv_browser.js
+++ _base/_loader/hostenv_browser.js
@@ -49,6 +49,7 @@ if(typeof window != 'undefined'){
 		d.isMozilla = d.isMoz = ((geckoPos >= 0)&&(!d.isKhtml)) ? tv : 0;
 		d.isFF = 0;
 		d.isIE = 0;
+		d.isGears = 0;
 		try{
 			if(d.isMoz){
 				d.isFF = parseFloat(dua.split("Firefox/")[1].split(" ")[0]);
@@ -57,6 +58,52 @@ if(typeof window != 'undefined'){
 				d.isIE = parseFloat(dav.split("MSIE ")[1].split(";")[0]);
 			}
 		}catch(e){}
+
+		d._gearsObject = function(){
+			// summary: 
+			//		factory method to get a Google Gears plugin instance to
+			//		expose in the browser runtime environment, if present
+			var factory;
+			var results;
+			
+			var gearsObj = d.getObject("google.gears");
+			if(gearsObj){ return gearsObj; } // already defined elsewhere
+			
+			if(typeof GearsFactory != "undefined"){ // Firefox
+				factory = new GearsFactory();
+			}else{
+				try{ // IE?
+					factory = new ActiveXObject("Gears.Factory");
+				}catch(exp){
+					// Safari?
+					if(navigator.mimeTypes["application/x-googlegears"]){
+						factory = document.createElement("object");
+						factory.setAttribute("type", "application/x-googlegears");
+						factory.setAttribute("width", 0);
+						factory.setAttribute("height", 0);
+						factory.style.display = "none";
+						document.documentElement.appendChild(factory);
+					}
+				}
+			}
+			
+			// still nothing?
+			if(!factory){ return null; }
+			
+			// define the global objects now; don't overwrite them though if they
+			// were somehow set internally by the Gears plugin, which is on their
+			// dev roadmap for the future
+			dojo.setObject("google.gears.factory", factory);
+			return dojo.getObject("google.gears");
+		}
+		
+		// see if we have Google Gears installed, and if
+		// so, make it available in the runtime environment
+		// and in the Google standard 'google.gears' global object
+		var gearsObj = d._gearsObject();
+		if(gearsObj){
+			d.isGears = parseFloat(gearsObj.factory.getBuildInfo());
+		}
 
 		var cm = document["compatMode"];
 		d.isQuirks = (cm == "BackCompat")||(cm == "QuirksMode")||(d.isIE < 6);

a898823065ded9cef8e8f7125a40e529510fdabd
Eugene Lazutkin <eugene@lazutkin.com>
2007-06-07 06:43:29 +0000

    Implements enhanced styles of drag detection, and customizable Mover. Includes box-constrained, and parent-constrained movers. A grid mover is implemented as an example of custom mover. Refs #3223. Refs #3224.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8956 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dnd/move.js
+++ dnd/move.js
@@ -3,21 +3,23 @@ dojo.provide("dojo.dnd.move");
 dojo.require("dojo.dnd.common");
 
 dojo.dnd.Mover = function(node, e){
-	// summary: an object, which makes a node follow the mouse
+	// summary: an object, which makes a node follow the mouse, 
+	//	used as a default mover, and as a base class for custom movers
 	// node: Node: a node (or node's id) to be moved
 	// e: Event: a mouse event, which started the move;
 	//	only pageX and pageY properties are used
 	this.node = dojo.byId(node);
-	var m = this.margin = dojo.marginBox(this.node), d = node.ownerDocument;
+	var m = this.marginBox = dojo.marginBox(this.node), d = node.ownerDocument;
 	m.l -= e.pageX;
 	m.t -= e.pageY;
-		this.firstEvent = dojo.connect(d, "onmousemove", this, "_makeAbsolute");
+	var firstEvent = dojo.connect(d, "onmousemove", this, "_makeAbsolute");
 	this.events = [
 		dojo.connect(d, "onmousemove", this, "onMouseMove"),
 		dojo.connect(d, "onmouseup",   this, "destroy"),
 		// cancel text selection and text dragging
 		dojo.connect(d, "ondragstart",   dojo, "stopEvent"),
-		dojo.connect(d, "onselectstart", dojo, "stopEvent")
+		dojo.connect(d, "onselectstart", dojo, "stopEvent"),
+		firstEvent
 	];
 };
 
@@ -26,35 +28,38 @@ dojo.extend(dojo.dnd.Mover, {
 	onMouseMove: function(e){
 		// summary: event processor for onmousemove
 		// e: Event: mouse event
-		var m = this.margin;
+		var m = this.marginBox;
 		dojo.marginBox(this.node, {l: m.l + e.pageX, t: m.t + e.pageY});
 	},
 	// utilities
 	_makeAbsolute: function(){
 		// summary: makes the node absolute; it is meant to be called only once
 		this.node.style.position = "absolute";	// enforcing the absolute mode
-		dojo.disconnect(this.firstEvent);
-		delete this.firstEvent;
+		dojo.disconnect(this.events.pop());
 	},
 	destroy: function(){
 		// summary: stops the move, deletes all references, so the object can be garbage-collected
 		dojo.forEach(this.events, dojo.disconnect);
-		if(this.firstEvent){
-			dojo.disconnect(this.firstEvent);
-		}
-		this.node = null;
+		this.events = this.node = null;
 	}
 });
 
-dojo.dnd.Moveable = function(node, handle){
+dojo.dnd.Moveable = function(node, opt){
 	// summary: an object, which makes a node moveable
 	// node: Node: a node (or node's id) to be moved
-	// handle: Node: a node (or node's id), which is used as a mouse handle;
-	//	if omitted, the node itself is used as a handle
-	if(!handle){ handle = node; }
+	// opt: Object: an optional object with additional parameters;
+	//	following parameters are recognized:
+	//		handle: Node: a node (or node's id), which is used as a mouse handle
+	//			if omitted, the node itself is used as a handle
+	//		delay: Number: delay move by this number of pixels
+	//		skip: Boolean: skip move of form elements
+	//		mover: Object: a constructor of custom Mover
 	this.node = dojo.byId(node);
-	this.handle = dojo.byId(handle);
+	this.handle = (opt && opt.handle) ? dojo.byId(opt.handle) : null;
 	if(!this.handle){ this.handle = this.node; }
+	this.delay = (opt && opt.delay > 0) ? opt.delay : 0;
+	this.skip  = opt && opt.skip;
+	this.mover = (opt && opt.mover) ? opt.mover : dojo.dnd.Mover;
 	this.events = [
 		dojo.connect(this.handle, "onmousedown", this, "onMouseDown"),
 		// cancel text selection and text dragging
@@ -68,13 +73,78 @@ dojo.extend(dojo.dnd.Moveable, {
 	onMouseDown: function(e){
 		// summary: event processor for onmousedown, creates a Mover for the node
 		// e: Event: mouse event
-		new dojo.dnd.Mover(this.node, e);
+		if(this.skip){
+			var t = e.target;
+			if(t.nodeType == 3 /*TEXT_NODE*/){
+				t = t.parentNode;
+			}
+			// do not trigger move if user interacts with form elements
+			if(" button textarea input select option ".indexOf(" " + t.tagName.toLowerCase() + " ") >= 0) {
+				return;
+			}
+		}
+		if(this.delay){
+			this.events.push(dojo.connect(this.handle, "onmousemove", this, "onMouseMove"));
+			this.events.push(dojo.connect(this.handle, "onmouseup", this, "onMouseUp"));
+			this._lastX = e.pageX;
+			this._lastY = e.pageY;
+		}else{
+			new this.mover(this.node, e);
+		}
 		dojo.stopEvent(e);
+	},
+	onMouseMove: function(e){
+		if(Math.abs(e.pageX - this._lastX) > this.delay || Math.abs(e.pageY - this._lastY) > this.delay){
+			this.onMouseUp(e);
+			new this.mover(this.node, e);
+		}
+		dojo.stopEvent(e);
+	},
+	onMouseUp: function(e){
+		dojo.disconnect(this.events.pop());
+		dojo.disconnect(this.events.pop());
 	},
 	// utilities
 	destroy: function(){
 		// summary: stops watching for possible move, deletes all references, so the object can be garbage-collected
 		dojo.forEach(this.events, dojo.disconnect);
-		this.node = this.handle = null;
+		this.events = this.node = this.handle = null;
 	}
 });
+
+dojo.dnd.constrainedMover = function(fun, within){
+	var mover = function(node, e){
+		dojo.dnd.Mover.call(this, node, e);
+		var c = this.constraintBox = fun.call(this), m = this.marginBox;
+		c.r = c.l + c.w - (within ? m.w : 0);
+		c.b = c.t + c.h - (within ? m.h : 0);
+	};
+	dojo.extend(mover, dojo.dnd.Mover.prototype);
+	dojo.extend(mover, {
+		onMouseMove: function(e){
+			// summary: event processor for onmousemove
+			// e: Event: mouse event
+			var m = this.marginBox, c = this.constraintBox,
+				l = m.l + e.pageX, t = m.t + e.pageY;
+			l = l < c.l ? c.l : c.r < l ? c.r : l;
+			t = t < c.t ? c.t : c.b < t ? c.b : t;
+			dojo.marginBox(this.node, {l: l, t: t});
+		}
+	});
+	return mover;
+};
+
+dojo.dnd.boxConstrainedMover = function(box, within){
+	return dojo.dnd.constrainedMover(function(){ return box; }, within);
+};
+
+dojo.dnd.parentConstrainedMover = function(within){
+	var fun = function(){
+		var n = this.node.parentNode,
+			s = dojo.getComputedStyle(n),
+			c = dojo._getContentBox(n, s),
+			p = dojo._getPadBounds(n, s);
+		return {l: 0, t: 0, w: c.w + p.w, h: c.h + p.h};
+	};
+	return dojo.dnd.constrainedMover(fun, within);
+};

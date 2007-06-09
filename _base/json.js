682b4a9b34cbc3cdd11c093f64472c6e9eb98f22
Dustin Machi <dmachi@dojotoolkit.org>
2007-06-09 05:07:29 +0000

    update json serializer to produce yaml compliant output. also fixed dependent xhr test. fixes #1889
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@8992 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/json.js
+++ _base/json.js
@@ -94,7 +94,7 @@ dojo.toJson = function(/*Object*/ it, /*Boolean?*/ prettyPrint, /*String?*/ _ind
 			}
 			res.push(newLine + nextIndent + val);
 		}
-		return "[" + res.join(",") + newLine + _indentStr + "]";
+		return "[" + res.join(", ") + newLine + _indentStr + "]";
 	}
 	/*
 	// look in the registry
@@ -129,7 +129,7 @@ dojo.toJson = function(/*Object*/ it, /*Boolean?*/ prettyPrint, /*String?*/ _ind
 		}
 		// FIXME: use += on Moz!!
 		//	 MOW NOTE: using += is a pain because you have to account for the dangling comma...
-		output.push(newLine + nextIndent + keyStr + ":" + val);
+		output.push(newLine + nextIndent + keyStr + ": " + val);
 	}
-	return "{" + output.join(",") + newLine + _indentStr + "}";
+	return "{" + output.join(", ") + newLine + _indentStr + "}";
 }

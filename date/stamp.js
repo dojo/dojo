bd5127cf7ee36535724a2f57871ecf5432fc6365
Adam Peller <github@peller.org>
2007-06-11 20:06:40 +0000

    Use faster regexp methods.  Refs #3007
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@9026 560b804f-0ae3-0310-86f3-f6aa0a117693

--- date/stamp.js
+++ date/stamp.js
@@ -2,84 +2,95 @@ dojo.provide("dojo.date.stamp");
 
 // Methods to convert dates to or from a wire (string) format using well-known conventions
 
-dojo.date.stamp.fromISOString = function(/*String*/formattedString, /*Number*/defaultTime){
+dojo.date.stamp.fromISOString = function(/*String*/formattedString, /*Number?*/defaultTime){
 	//	summary:
-	//		Returns a Date object given a string formatted according to a subset of the ISO-8601 standard
+	//		Returns a Date object given a string formatted according to a subset of the ISO-8601 standard.
 	//
 	//	description:
 	//		Accepts a string formatted according to a profile of ISO8601 as defined by
-	//		RFC3339 (http://www.ietf.org/rfc/rfc3339.txt).  Additionally, partial input is allowed.
+	//		RFC3339 (http://www.ietf.org/rfc/rfc3339.txt), except that partial input is allowed.
+	//		Can also process dates as specified by http://www.w3.org/TR/NOTE-datetime
 	//		The following combinations are valid:
-	//			yyyy-MM-dd
-	//			THH:mm:ss
-	//			yyyy-MM-ddTHH:mm:ss
-	//			THH:mm:ssTZD
-	//			yyyy-MM-ddTHH:mm:ssTZD
-	//		where TZD is Z or +/- followed by a time expression HH:mm
+	// 			* dates only
+	//				yyyy
+	//				yyyy-MM
+	//				yyyy-MM-dd
+	// 			* times only, with an optional time zone appended
+	//				THH:mm
+	//				THH:mm:ss
+	//				THH:mm:ss.SSS
+	// 			* and "datetimes" which could be any combination of the above
+	//		timezones may be specified as Z (for UTC) or +/- followed by a time expression HH:mm
 	//		Assumes the local time zone if not specified.  Does not validate.  Improperly formatted
-	//		input may return an invalid date object.  Arguments which are out of bounds will be handled
+	//		input may return null.  Arguments which are out of bounds will be handled
 	// 		by the Date constructor (e.g. January 32nd typically gets resolved to February 1st)
 	//
   	//	formattedString:
 	//		A string such as 2005-06-30T08:05:00-07:00 or 2005-06-30 or T08:05:00
 	//
 	//	defaultTime:
-	//		Used for defaults for fields omitted in the formattedString.  If omitted,
-	//		uses 1970-01-01T00:00:00.0Z for default.
+	//		Used for defaults for fields omitted in the formattedString.
+	//		Uses 1970-01-01T00:00:00.0Z by default.
 
-//TODO: this is frustratingly close to http://www.w3.org/TR/NOTE-datetime.  We could match this by making MM dd and TZD
-//	optional, and the date mandatory.
-//TODO: can a regexp be crafted to do this as efficiently?  Is it worth providing validation?
-	var result = new Date(defaultTime || 0);
-	var segments = formattedString.split("T");
-	if(segments[0]){
-		var dateSegments = segments[0].split("-");
-		result.setFullYear(dateSegments[0]);
-		result.setMonth(0); // need to do this so the date doesn't wrap around; reconsider defaultTime arg?
-		result.setDate(dateSegments[2]);
-		result.setMonth(dateSegments[1]-1);
-	}
-	if(segments[1]){
-		var timeSegments = segments[1].substring(0, 8).split(":");
-		result.setHours(timeSegments[0]);
-		result.setMinutes(timeSegments[1]);
-		result.setSeconds(timeSegments[2]);
-		var remainder = segments[1].substring(8);
-		if(remainder.charAt(0) === "."){
-			//TODO: millis?
-		}
-		if(remainder){
-			var offset = 0;
-			if(remainder.charAt(0) != 'Z'){
-				var gmtOffset = remainder.substring(1).split(":");
-				offset = (gmtOffset[0] * 60) + (Number(gmtOffset[1]) || 0);
-				if(remainder.charAt(0) != '-'){ offset *= -1; }
-			}
-			offset -= result.getTimezoneOffset();
-			if(offset){
-				result.setTime(result.getTime() + offset * 60000);
-			}
-		}
+	if(!dojo.date.stamp._isoRegExp){
+		dojo.date.stamp._isoRegExp =
+//TODO: could be more restrictive and check for 00-59, etc.
+			/^(?:(\d{4})(?:-(\d{2})(?:-(\d{2}))?)?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(.\d+)?)?((?:[+-](\d{2}):(\d{2}))|Z)?)?$/;
 	}
 
-	return result;
+	var match = dojo.date.stamp._isoRegExp.exec(formattedString);
+	var result = null;
+
+	if(match){
+		match.shift();
+		match[1] && match[1]--; // Javascript Date months are 0-based
+		match[6] && (match[6] *= 1000); // Javascript Date expects fractional seconds as milliseconds
+
+		if(defaultTime){
+			// mix in defaultTime.  Relatively expensive, so use || operators for the fast path of defaultTime === 0
+			defaultTime = new Date(defaultTime);
+			dojo.map(["FullYear", "Month", "Date", "Hours", "Minutes", "Seconds", "Milliseconds"], function(prop){
+				return defaultTime["get" + prop]();
+			}).forEach(function(value, index){
+				if(match[index] === undefined){
+					match[index] = value;
+				}
+			});
+		}
+
+		result = new Date(match[0]||1970, match[1]||0, match[2]||0, match[3]||0, match[4]||0, match[5]||0, match[6]||0);
+
+		var offset = 0;
+		var zoneSign = match[7] && match[7].charAt(0);
+		if(zoneSign != 'Z'){
+			offset = (match[8] * 60) + (Number(match[9]) || 0);
+			if(zoneSign != '-'){ offset *= -1; }
+		}
+		offset -= result.getTimezoneOffset();
+		if(offset){
+			result.setTime(result.getTime() + offset * 60000);
+		}
+	}
+
+	return result; // Date or null
 }
 
-dojo.date.stamp.toISOString = function(/*Date*/dateObject, /*Object*/options){
+dojo.date.stamp.toISOString = function(/*Date*/dateObject, /*Object?*/options){
 	//	summary:
 	//		Format a Date object as a string according a subset of the ISO-8601 standard
 	//
 	//	description:
-	//		When selector option is omitted, output follows RFC3339 ((http://www.ietf.org/rfc/rfc3339.txt)
+	//		When options.selector is omitted, output follows RFC3339 (http://www.ietf.org/rfc/rfc3339.txt)
 	//		Times are formatted using the local time zone.  Does not check bounds.
 	//
 	//	dateObject:
 	//		A Date object
 	//
-	//	object {selector: string, zulu: boolean}
+	//	object {selector: string, zulu: boolean, milliseconds: boolean}
 	//		selector- "date" or "time" for partial formatting of the Date object.
 	//			Both date and time will be formatted by default.
 	//		zulu- if true, UTC/GMT is used for a timezone
+	//		milliseconds- if true, output milliseconds
 
 	var _ = function(n){ return (n < 10) ? "0" + n : n; }
 	options = options || {};
@@ -92,10 +103,10 @@ dojo.date.stamp.toISOString = function(/*Date*/dateObject, /*Object*/options){
 	formattedDate.push(date);
 	if(options.selector != "date"){
 		var time = [_(dateObject[getter+"Hours"]()), _(dateObject[getter+"Minutes"]()), _(dateObject[getter+"Seconds"]())].join(':');
-//		var millis = dateObject[getter+"Milliseconds"]();
-//		if(options.milliseconds){
-//			time += "."+millis;
-//		}
+		var millis = dateObject[getter+"Milliseconds"]();
+		if(options.milliseconds){
+			time += "."+ (millis < 100 ? "0" : "") + _(millis);
+		}
 		if(options.zulu){
 			time += "Z";
 		}else{

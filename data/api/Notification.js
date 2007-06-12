767578a05983147d78b98c134295b7687413bd15
Brian Skinner <skinner@dojotoolkit.org>
2007-06-12 07:38:16 +0000

    Fixes #3369
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@9045 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/api/Notification.js
+++ data/api/Notification.js
@@ -11,9 +11,9 @@ dojo.declare("dojo.data.api.Notification",dojo.data.api.Read,null,{
 	//		This API defines a set of APIs that all datastores that conform to the
 	//		Notifications API must implement.  In general, most stores will implement 
 	//		these APIs as no-op functions for users who wish to monitor them to be able
-	//		to connect to then via dojo.event.connect().  For non-users of 
-	//		dojo.event.connect, they should be able to just replace the method on the 
-	//		store to obtain notifications.  Both read-only and read-write stores may implement
+	//		to connect to then via dojo.connect().  For non-users of dojo.connect, 
+	//		they should be able to just replace the method on the store to obtain
+	//		 notifications.  Both read-only and read-write stores may implement
 	//		this feature.  In the case of a read-only store, this feature makes sense if 
 	//		the store itself does internal polling to a back-end server and periodically updates
 	//		its cache of items (deletes, adds, and updates).
@@ -24,7 +24,7 @@ dojo.declare("dojo.data.api.Notification",dojo.data.api.Read,null,{
 	//			//Do something with the information...
 	//		};
 	//		var store = new some.newStore();
-	//		dojo.event.connect(store, "onSet", onUpdate);
+	//		dojo.connect(store, "onSet", onUpdate);
 
 	getFeatures: function(){
 		//	summary: 
@@ -36,15 +36,15 @@ dojo.declare("dojo.data.api.Notification",dojo.data.api.Read,null,{
 	},
 
 	onSet: function(/* item */ item, 
-					/*attribute | attribute-name-string*/ attribute, 
-					/*object | array*/ oldValue,
-					/*object | array*/ newValue){
+					/* attribute-name-string */ attribute, 
+					/* object | array */ oldValue,
+					/* object | array */ newValue){
 		//	summary:
 		//		This method is called any time an item is modified via setValue, setValues, unsetAttribute, etc.  
 		//	description:
 		//		This method is called any time an item is modified via setValue, setValues, unsetAttribute, etc.  
 		//		Its purpose is to provide a hook point for those who wish to monitor actions on items in the store 
-		//		in a simple manner.  The general expected usage is to dojo.event.connect() to the store's 
+		//		in a simple manner.  The general expected usage is to dojo.connect() to the store's 
 		//		implementation and be called after the store method is called.
 		//
 		//	item:

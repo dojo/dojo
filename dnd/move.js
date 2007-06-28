83977c2717ce202fbea277e355b1aaf6295e74b8
Eugene Lazutkin <eugene@lazutkin.com>
2007-06-28 06:48:41 +0000

    Added global and local classes, so programmers can change UI during the move. Added topics to indicated that the move has started and ended. Fixes #3371.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@9349 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dnd/move.js
+++ dnd/move.js
@@ -19,6 +19,10 @@ dojo.dnd.Mover = function(node, e){
 		dojo.connect(d, "onselectstart", dojo, "stopEvent"),
 		firstEvent
 	];
+	// set globals to indicate that move has started
+	dojo.publish("dndMoveStart", [this.node]);
+	dojo.addClass(dojo.body(), "dojoMove"); 
+	dojo.addClass(this.node, "dojoMoveItem"); 
 };
 
 dojo.extend(dojo.dnd.Mover, {
@@ -42,6 +46,11 @@ dojo.extend(dojo.dnd.Mover, {
 	destroy: function(){
 		// summary: stops the move, deletes all references, so the object can be garbage-collected
 		dojo.forEach(this.events, dojo.disconnect);
+		// undo global settings
+		dojo.publish("dndMoveStop", [this.node]);
+		dojo.removeClass(dojo.body(), "dojoMove");
+		dojo.removeClass(this.node, "dojoMoveItem");
+		// destroy objects
 		this.events = this.node = null;
 	}
 });

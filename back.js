7ac7d1cf5ca634948bb193517494295b415a13c2
Robert Coup <robert@coup.net.nz>
2007-07-29 05:02:22 +0000

    Add a warning instead of throwing an obscure exception if dojo.back.init() hasn't been called. Fixes #3861.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@9848 560b804f-0ae3-0310-86f3-f6aa0a117693

--- back.js
+++ back.js
@@ -100,7 +100,11 @@ dojo.provide("dojo.back");
 		//summary: private method. Do not call this directly.
 		var url = (djConfig["dojoIframeHistoryUrl"] || dojo.moduleUrl("dojo", "resources/iframe_history.html")) + "?" + (new Date()).getTime();
 		moveForward = true;
-		(dojo.isSafari) ? historyIframe.location = url : window.frames[historyIframe.name].location = url;
+        if (historyIframe) {
+		    (dojo.isSafari) ? historyIframe.location = url : window.frames[historyIframe.name].location = url;
+        } else {
+            console.warn("dojo.back: Not initialised. You need to call dojo.back.init() from a <script> block that lives inside the <body> tag.");
+        }
 		return url; //String
 	}
 

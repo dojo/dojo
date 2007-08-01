7ee3426baf44eb74ea4558bd07d2199decbe6e56
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2007-08-01 19:46:56 +0000

    Checking if fix to  non-tf8 char.  Byte emcodin check didn't catch the bad bytes.   Fooey, validator test failed.  Restoring to good file.  refs #3938
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@9910 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/number.js
+++ tests/number.js
@@ -562,7 +562,7 @@ function test_number_format_pad(){
 //	t.is(0,result);
 
 	/**************************************** tolerant parse *****************************************
-	 * refere to ICU4J's NumberFormatTest.TestStrictParse()£»
+	 * refers to ICU4J's NumberFormatTest.TestStrictParse()??
 	 * TODO: Seems dojo.number parses string in a tolerant way.  
 	 */
 	 var options = {locale:"en"};

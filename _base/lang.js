1e6153898b233b985a1c0f41be17fbdd37d78101
Adam Peller <github@peller.org>
2007-09-12 00:57:26 +0000

    Add comments for isObject plus style mods.  Fixes #4424, #3961
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@10475 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/lang.js
+++ _base/lang.js
@@ -4,43 +4,42 @@ dojo.provide("dojo._base.lang");
 
 dojo.isString = function(/*anything*/ it){
 	// summary:	Return true if it is a String.
-	return (typeof it == "string" || it instanceof String); // Boolean
+	return typeof it == "string" || it instanceof String; // Boolean
 }
 
 dojo.isArray = function(/*anything*/ it){
 	// summary: Return true of it is an Array
-	return (it && it instanceof Array || typeof it == "array" || ((typeof dojo["NodeList"] != "undefined") && (it instanceof dojo.NodeList))); // Boolean
+	return it && it instanceof Array || typeof it == "array" || ((typeof dojo.NodeList != "undefined") && (it instanceof dojo.NodeList)); // Boolean
 }
 
 if(dojo.isBrowser && dojo.isSafari){
 	// only slow this down w/ gratuitious casting in Safari since it's what's b0rken
 	dojo.isFunction = function(/*anything*/ it){
-		if((typeof(it) == "function") && (it == "[object NodeList]")){ return false; }
-		return (typeof it == "function" || it instanceof Function); // Boolean
+		if(typeof it == "function" && it == "[object NodeList]"){ return false; }
+		return typeof it == "function" || it instanceof Function; // Boolean
 	}
 }else{
 	dojo.isFunction = function(/*anything*/ it){
-		return (typeof it == "function" || it instanceof Function); // Boolean
+		return typeof it == "function" || it instanceof Function; // Boolean
 	}
 }
 
 dojo.isObject = function(/*anything*/ it){
-	if(typeof it == "undefined"){ return false; }
-	// FIXME: why true for null?
-	return (it === null || typeof it == "object" || dojo.isArray(it) || dojo.isFunction(it)); // Boolean
+	// summary: 
+	//		Returns true if it is a JavaScript object (or an Array, a Function or null)
+	return typeof it != "undefined" &&
+		(it === null || typeof it == "object" || dojo.isArray(it) || dojo.isFunction(it)); // Boolean
 }
 
 dojo.isArrayLike = function(/*anything*/ it){
 	// return:
 	//		If it walks like a duck and quicks like a duck, return true
 	var d = dojo;
-	if((!it)||(typeof it == "undefined")){ return false; }
-	if(d.isString(it)){ return false; }
 	// keep out built-in constructors (Number, String, ...) which have length
 	// properties
-	if(d.isFunction(it)){ return false; } 
+	if(!it || typeof it == "undefined" || d.isString(it) || d.isFunction(it)){ return false; } 
 	if(d.isArray(it)){ return true; }
-	if((it.tagName)&&(it.tagName.toLowerCase()=='form')){ return false; }
+	if(it.tagName && it.tagName.toLowerCase() == 'form'){ return false; }
 	if(isFinite(it.length)){ return true; }
 	return false; // Boolean
 }
@@ -49,8 +48,7 @@ dojo.isAlien = function(/*anything*/ it){
 	// summary: 
 	//		Returns true if it is a built-in function or some other kind of
 	//		oddball that *should* report as a function but doesn't
-	if(!it){ return false; }
-	return !dojo.isFunction(it) && /\{\s*\[native code\]\s*\}/.test(String(it)); // Boolean
+	return it && !dojo.isFunction(it) && /\{\s*\[native code\]\s*\}/.test(String(it)); // Boolean
 }
 
 dojo._mixin = function(/*Object*/ obj, /*Object*/ props){
@@ -71,10 +69,8 @@ dojo._mixin = function(/*Object*/ obj, /*Object*/ props){
 	// IE doesn't recognize custom toStrings in for..in
 	if(dojo.isIE){
 		var p = props.toString;
-		if((typeof(p) == "function") 
-			&& (p != obj.toString) 
-			&& (p != tobj.toString) 
-			&& (p != "\nfunction toString() {\n    [native code]\n}\n")){
+		if(typeof p == "function" && p != obj.toString && p != tobj.toString &&
+			p != "\nfunction toString() {\n    [native code]\n}\n"){
 				obj.toString = props.toString;
 		}
 	}
@@ -109,7 +105,7 @@ dojo._hitchArgs = function(scope, method /*,...*/){
 		// locate our method
 		var f = (named ? (scope||dojo.global)[method] : method);
 		// invoke with collected args
-		return (f)&&(f.apply(scope||this, pre.concat(args))); // Any
+		return f && f.apply(scope || this, pre.concat(args)); // Any
  	} // Function
 }
 
@@ -141,10 +137,10 @@ dojo.hitch = function(/*Object*/scope, /*Function|String*/method /*,...*/){
 	}
 	if(dojo.isString(method)){
 		scope = scope || dojo.global;
-		if (!scope[method]) throw(['dojo.hitch: scope["', method, '"] is null (scope="', scope, '")'].join(''))
+		if(!scope[method]){ throw(['dojo.hitch: scope["', method, '"] is null (scope="', scope, '")'].join('')); }
 		return function(){ return scope[method].apply(scope, arguments||[]); }
 	}else{
-		return (!scope ? method : function(){ return method.apply(scope, arguments||[]); });
+		return !scope ? method : function(){ return method.apply(scope, arguments||[]); };
 	}
 }
 
@@ -174,7 +170,7 @@ dojo._toArray = function(/*Object*/obj, /*Number?*/offset){
 	//		Converts an array-like object (i.e. arguments, DOMCollection)
 	//		to an array. Returns a new Array object.
 	var arr = [];
-	for(var x= offset||0; x<obj.length; x++){
+	for(var x= offset || 0; x < obj.length; x++){
 		arr.push(obj[x]);
 	}
 	return arr;

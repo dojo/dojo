5e2628397ab01cb1dc0e6beae7208646d53c077c
Alex Russell <slightlyoff@chromium.org>
2007-10-06 18:23:13 +0000

    merging patch from Mike Wilcox (mike@sitepen.com) that adds object inspection and a popup mode to Dojo's integrated Firebug Lite. Excellent patch, needed only the most minor style cleanups. Great work. Fixes #4589
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@10746 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _firebug/firebug.js
+++ _firebug/firebug.js
@@ -34,6 +34,31 @@ dojo.experimental = function(/* String */ moduleName, /* String? */ extra){
 	console.debug(message);
 }
 
+// FIREBUG LITE
+	// summary: Firebug Lite, the baby brother to Joe Hewitt's Firebug for Mozilla Firefox
+	// description:
+	//		Opens a console for logging, debugging, and error messages.
+	//		Contains partial functionality to Firebug. See function list below.
+	//
+	// 	Added functionality:
+	//		Supports inline objects in object inspector window (only simple trace of dom nodes, however)
+	//			example:
+	//			console.log("my object", {foo:"bar"})
+	//		Option for console to open in popup window
+	//			example:
+	//			var djConfig = {isDebug: true, popup:true };
+	//		Option for console height (ignored for popup)
+	//			example:
+	//			var djConfig = {isDebug: true, debugHeight:100 };
+	//
+	//	NOTE: 
+	//			Firebug is a Firefox extension created by Joe Hewitt (see license). You do not need Dojo to run Firebug.
+	//			Firebug Lite is included in Dojo by permission from Joe Hewitt
+	//			If you are new to Firebug, or used to the Dojo 0.4 dojo.debug, you can learn Firebug 
+	//				functionality by reading the function comments below or visiting http://www.getfirebug.com/docs.html
+	//	NOTE:
+	//		To test Firebug Lite in Firefox, set console = null;
+	
 if(
 	(
 		(!("console" in window)) || 
@@ -49,26 +74,39 @@ if(
 
 	window.console = {
 		log: function(){
+			// summary: 
+			//		Sends arguments to console.
 			logFormatted(arguments, "");
 		},
 		
 		debug: function(){
+			// summary: 
+			//		Sends arguments to console. Missing finctionality to show script line of trace.
 			logFormatted(arguments, "debug");
 		},
 		
 		info: function(){
+			// summary: 
+			//		Sends arguments to console, highlighted with (I) icon.
 			logFormatted(arguments, "info");
 		},
 		
 		warn: function(){
+			// summary: 
+			//		Sends warning arguments to console, highlighted with (!) icon and blue style.
 			logFormatted(arguments, "warning");
 		},
 		
 		error: function(){
+			// summary: 
+			//		Sends error arguments (object) to console, highlighted with (X) icon and yellow style
+			//			NEW: error object now displays in object inspector
 			logFormatted(arguments, "error");
 		},
 		
 		assert: function(truth, message){
+			// summary: 
+			//		Tests for true. Throws exception if false.
 			if(!truth){
 				var args = [];
 				for(var i = 1; i < arguments.length; ++i){
@@ -81,6 +119,8 @@ if(
 		},
 		
 		dir: function(object){
+			// summary: 
+			//		Traces object. Only partially implemented.
 			var html = [];
 						
 			var pairs = [];
@@ -112,6 +152,8 @@ if(
 		},
 		
 		dirxml: function(node){
+			// summary: 
+			//
 			var html = [];
 			
 			appendNode(node, html);
@@ -119,18 +161,32 @@ if(
 		},
 		
 		group: function(){
+			// summary: 
+			//		collects log messages into a group, starting with this call and ending with 
+			//			groupEnd(). Missing collapse functionality
 			logRow(arguments, "group", pushGroup);
 		},
 		
 		groupEnd: function(){
+			// summary: 
+			//		Closes group. See above
 			logRow(arguments, "", popGroup);
 		},
 		
 		time: function(name){
+			// summary: 
+			//		Starts timers assigned to name given in argument. Timer stops and displays on timeEnd(title);
+			//	example:
+			//		console.time("load");
+			//		console.time("myFunction");
+			//		console.timeEnd("load");
+			//		console.timeEnd("myFunction");
 			timeMap[name] = (new Date()).getTime();
 		},
 		
 		timeEnd: function(name){
+			// summary: 
+			//		See above.
 			if(name in timeMap){
 				var delta = (new Date()).getTime() - timeMap[name];
 				logFormatted([name+ ":", delta+"ms"]);
@@ -139,34 +195,61 @@ if(
 		},
 		
 		count: function(){
+			// summary: 
+			//		Not supported
 			this.warn(["count() not supported."]);
 		},
 		
 		trace: function(){
+			// summary: 
+			//		Not supported
 			this.warn(["trace() not supported."]);
 		},
 		
 		profile: function(){
+			// summary: 
+			//		Not supported
 			this.warn(["profile() not supported."]);
 		},
 		
 		profileEnd: function(){ },
 		
 		clear: function(){
+			// summary: 
+			//		Clears message console. Do not call this directly
 			consoleBody.innerHTML = "";
 		},
 
-		open: function(){ toggleConsole(true); },
+		open: function(){ 
+			// summary: 
+			//		Opens message console. Do not call this directly
+			toggleConsole(true); 
+		},
 		
 		close: function(){
+			// summary: 
+			//		Closes message console. Do not call this directly
 			if(frameVisible){
 				toggleConsole();
 			}
+		},
+		closeObjectInspector:function(){
+			// summary: 
+			//		Closes object inspector and opens message console. Do not call this directly
+			consoleObjectInspector.innerHTML = "";
+			consoleObjectInspector.style.display = "none";
+			consoleBody.style.display = "block";	
 		}
 	};
  
 	// ***************************************************************************
-	   
+	
+	// using global objects so they can be accessed
+	// most of the objects in this script are run anonomously
+	var _firebugDoc ;
+	var _firebugWin ;
+	var __consoleAnchorId__ = 0;
+	
 	var consoleFrame = null;
 	var consoleBody = null;
 	var commandLine = null;
@@ -193,24 +276,54 @@ if(
 			commandLine.focus();
 		}
 	}
-
+	
+	openWin = function(){
+		var win = window.open("","_firebug","status=0,menubar=0,resizable=1,width=640,height=480,scrollbars=1,addressbar=0");
+		var newDoc=win.document;
+		HTMLstring='<html><head><title>Firebug Lite</title></head>\n';
+		HTMLstring+='<body bgColor="#CCCCCC">\n';
+		//Testing access to dojo from the popup window
+		/*HTMLstring+='<button onclick="(function(){ console.log(dojo.version.toString()); })()">Test Parent Dojo</button>\n';*/
+		HTMLstring+='<div id="fb"></div>';
+		HTMLstring+='</body></html>';
+	
+	
+		newDoc.write(HTMLstring);
+		newDoc.close();
+		return win;
+	
+	}
+	
 	function createFrame(){
 		if(consoleFrame){
 			return;
 		}
 		
-		var doc = document;
+		if(djConfig.popup){
+			_firebugWin = openWin();
+			_firebugDoc = _firebugWin.document;
+			djConfig.debugContainerId = 'fb';
+			var containerHeight = "100%";
+			
+			// connecting popup
+			_firebugWin.console = window.console;
+			_firebugWin.dojo = window.dojo;
+
+		}else{
+			_firebugDoc = document;	
+			var containerHeight = (djConfig.debugHeight) ? djConfig.debugHeight + "px" :"300px";
+		}
 		
-		var styleElement = doc.createElement("link");
+		var styleElement = _firebugDoc.createElement("link");
 		styleElement.href = dojo.moduleUrl("dojo._firebug", "firebug.css");
 		styleElement.rel = "stylesheet";
 		styleElement.type = "text/css";
-		var styleParent = doc.getElementsByTagName("head");
+		var styleParent = _firebugDoc.getElementsByTagName("head");
 		if(styleParent){
 			styleParent = styleParent[0];
 		}
 		if(!styleParent){
-			styleParent = doc.getElementsByTagName("html")[0];
+			styleParent = _firebugDoc.getElementsByTagName("html")[0];
 		}
 		if(dojo.isIE){
 			window.setTimeout(function(){ styleParent.appendChild(styleElement); }, 0);
@@ -219,36 +332,40 @@ if(
 		}
 		
 		if(typeof djConfig != "undefined" && djConfig["debugContainerId"]){
-			consoleFrame = doc.getElementById(djConfig.debugContainerId);
+			consoleFrame = _firebugDoc.getElementById(djConfig.debugContainerId);
 		}
 		if(!consoleFrame){
-			consoleFrame = doc.createElement("div");
-			doc.body.appendChild(consoleFrame);
+			consoleFrame = _firebugDoc.createElement("div");
+			_firebugDoc.body.appendChild(consoleFrame);
 		}
 		consoleFrame.className += " firebug";
-		consoleFrame.style.height = "200px";
+		consoleFrame.style.height = containerHeight;
 		consoleFrame.style.display = (frameVisible ? "block" : "none");	  
 		
+		var closeStr = (djConfig.popup) ? "" : '    <a href="#" onclick="console.close(); return false;">Close</a>';
 		consoleFrame.innerHTML = 
 			  '<div id="firebugToolbar">'
 			+ '  <a href="#" onclick="console.clear(); return false;">Clear</a>'
 			+ '  <span class="firebugToolbarRight">'
-			+ '    <a href="#" onclick="console.close(); return false;">Close</a>'
+			+ closeStr
 			+ '  </span>'
 			+ '</div>'
 			+ '<input type="text" id="firebugCommandLine">'
-			+ '<div id="firebugLog"></div>';
+			+ '<div id="firebugLog"></div>'
+			+ '<div id="objectLog" style="display:none;"></div>';
 
 
-		var toolbar = doc.getElementById("firebugToolbar");
+		var toolbar = _firebugDoc.getElementById("firebugToolbar");
 		toolbar.onmousedown = onSplitterMouseDown;
 
-		commandLine = doc.getElementById("firebugCommandLine");
+		commandLine = _firebugDoc.getElementById("firebugCommandLine");
 		addEvent(commandLine, "keydown", onCommandLineKeyDown);
 
-		addEvent(doc, dojo.isIE || dojo.isSafari ? "keydown" : "keypress", onKeyDown);
+		addEvent(_firebugDoc, dojo.isIE || dojo.isSafari ? "keydown" : "keypress", onKeyDown);
 		
-		consoleBody = doc.getElementById("firebugLog");
+		consoleBody = _firebugDoc.getElementById("firebugLog");
+		consoleObjectInspector = _firebugDoc.getElementById("objectLog");
+
 		layout();
 		flush();
 	}
@@ -342,7 +459,7 @@ if(
 
 	function logFormatted(objects, className){
 		var html = [];
-
+		
 		var format = objects[0];
 		var objIndex = 0;
 
@@ -352,6 +469,7 @@ if(
 		}
 
 		var parts = parseFormat(format);
+		
 		for(var i = 0; i < parts.length; ++i){
 			var part = parts[i];
 			if(part && typeof(part) == "object"){
@@ -361,19 +479,55 @@ if(
 				appendText(part, html);
 			}
 		}
-
+		
+		
+		var ids = [];
+		var obs = [];
 		for(var i = objIndex+1; i < objects.length; ++i){
 			appendText(" ", html);
 			
 			var object = objects[i];
 			if(typeof(object) == "string"){
 				appendText(object, html);
+				
+			}else if(object.nodeType == 1){
+				// simple tracing of dom nodes
+				appendText("< "+object.tagName+" id=\""+ object.id+"\" />", html);
+				
 			}else{
-				appendObject(object, html);
+				// Create link for object inspector
+				// need to create an ID for this link, since it is currently text
+				var id = "_a" + __consoleAnchorId__++;
+				ids.push(id);
+				// need to save the object, so the arrays line up
+				obs.push(object)
+				var str = '<a id="'+id+'" href="javascript:void(0);">'+getObjectAbbr(object)+'</a>';
+				
+				appendLink( str , html);
 			}
 		}
 		
 		logRow(html, className);
+		
+		// Now that the row is inserted in the DOM, loop through all of the links that were just created
+		for(var i=0; i<ids.length; i++){
+			var btn = _firebugDoc.getElementById(ids[i]);
+	
+			// store the object in the dom btn for reference later
+			// avoid parsing these objects unless necessary
+			btn.obj = obs[i];
+	
+			dojo.connect(btn, "onclick", function(){
+				// hide rows
+				consoleBody.style.display = "none";
+				consoleObjectInspector.style.display = "block";
+				// create a back button
+				var bkBtn = '<a href="javascript:console.closeObjectInspector();">&nbsp;<<&nbsp;Back</a>';
+				consoleObjectInspector.innerHTML = bkBtn + "<pre>" + printObject( this.obj ) + "</pre>";
+													  
+			})
+
+		}
 	}
 
 	function parseFormat(format){
@@ -426,7 +580,11 @@ if(
 	}
 
 	// ***************************************************************************
-
+	function appendLink(object, html){
+		// needed for object links - no HTML escaping
+		html.push( objectToString(object) );
+	}
+	
 	function appendText(object, html){
 		html.push(escapeHTML(objectToString(object)));
 	}
@@ -644,6 +802,78 @@ if(
 			commandLine.value = "";
 		}
 	}
+	//***************************************************************************************************
+	// Print Object Helpers
+	getAtts = function(o){
+			//Get amount of items in an object
+			if(dojo.isArray(o)) { 
+				return "[array with " + o.length + " slots]"; 
+			}else{
+				var i = 0;
+				for(var nm in o){
+					i++;
+				}
+				return "{object with " + i + " items}";
+			}
+		}
+		printObject = function(o, i, txt){
+			
+			// Recursively trace object, indenting to represent depth for display in object inspector
+			// TODO: counter to prevent overly complex or looped objects (will probably help with dom nodes)
+			var br = "\n"; // using a <pre>... otherwise we'd need a <br />
+			var ind = "  ";
+			txt = (txt) ? txt : "";
+			i = (i) ? i : ind;
+			for(var nm in o){
+				if(typeof(o[nm]) == "object"){
+					txt += i+nm +" -> " + getAtts(o[nm]) + br;
+					txt += printObject(o[nm], i+ind);
+				}else{
+					txt += i+nm +" : "+o[nm] + br;
+				}
+			}
+			return txt;
+		}
+		
+		
+		getObjectAbbr = function(obj){
+			// Gets an abbreviation of an object for display in log
+			// X items in object, including id
+			// X items in an array
+			// TODO: Firebug Sr. actually goes by char count
+			var nm = obj.id || obj.name || obj.ObjectID || obj.widgetId;
+			if(nm){ return "{"+nm+"}";	}
+
+			var obCnt = 2;
+			var arCnt = 4;
+			var cnt = 0;
+			
+			
+			if(dojo.isArray(obj)){
+				nm ="[";
+				for(var i=0;i<obj.length;i++){
+					nm+=obj[i]+","
+					if(i>arCnt){
+						nm+=" ... ("+obj.length+" items)";
+						break;
+					}
+				}
+				nm+="]";
+
+			}else{
+				nm = "{";
+				for ( var i in obj){
+					cnt++
+					if(cnt > obCnt) break;
+					nm += i+"="+obj[i]+"  ";
+				}
+				nm+="}"
+			}
+			
+			return nm;
+		}
+		
+	//*************************************************************************************
 	
 	window.onerror = onError;
 	addEvent(document, dojo.isIE || dojo.isSafari ? "keydown" : "keypress", onKeyDown);

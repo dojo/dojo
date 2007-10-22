75719ee4df84cfe9335a4d0350ec6f2352aade86
Adam Peller <github@peller.org>
2007-10-22 02:44:35 +0000

    In a build, an exact match is required for locale.  Fixes #4814
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@11037 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/number.js
+++ tests/number.js
@@ -571,7 +571,7 @@ function test_number_format_pad(){
 	 * refers to ICU4J's NumberFormatTest.TestStrictParse()??
 	 * TODO: Seems dojo.number parses string in a tolerant way.  
 	 */
-	 var options = {locale:"en"};
+	 var options = {locale:"en-us"};
 	/*
 	 * TODO: !!Failed case,Should all pass,
 	 * but the following elements failed (all parsed to NaN):

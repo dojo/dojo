04d0c61d0e5db759318d44782c6ad9a0823ed47e
Alex Russell <slightlyoff@chromium.org>
2007-10-27 06:49:08 +0000

    adding first extension file for dojo.NodeList. Refs #4480
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@11145 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ NodeList-ext.js
@@ -0,0 +1,86 @@
+dojo.provide("dojo.NodeList-ext");
+dojo.require("dojo.fx");
+
+dojo.extend(dojo.NodeList, {
+	_anim: function(obj, method, args){
+		var anims = [];
+		args = args||{};
+		this.forEach(function(item){
+			var tmpArgs = { node: item };
+			dojo.mixin(tmpArgs, args);
+			anims.push(obj[method](tmpArgs));
+		});
+		// FIXME: combine isn't in Base!!
+		return dojo.fx.combine(anims); // dojo._Animation
+	},
+
+	wipeIn: function(args){
+		//	summary:
+		//		wipe in all elements of this NodeList. Returns an instance of dojo._Animation
+		//	example:
+		//		// fade in all tables with class "blah"
+		//		dojo.query("table.blah").wipeIn().play();
+		return this._anim(dojo.fx, "wipeIn", args); // dojo._Animation
+	},
+
+	wipeOut: function(args){
+		//	summary:
+		//		wipe out all elements of this NodeList. Returns an instance of dojo._Animation
+		//	example:
+		//		// wipe out all tables with class "blah"
+		//		dojo.query("table.blah").wipeOut().play();
+		return this._anim(dojo.fx, "wipeOut", args); // dojo._Animation
+	},
+
+	slideTo: function(args){
+		//	summary:
+		//		slide all elements of the node list to the specified place.
+		//		Returns an instance of dojo._Animation
+		//	example:
+		//		// move all tables with class "blah" to 300/300
+		//		dojo.query("table.blah").slideTo({
+		//			left: 40,
+		//			top: 50
+		//		}).play();
+		return this._anim(dojo.fx, "slideTo", args); // dojo._Animation
+	},
+
+
+	fadeIn: function(args){
+		//	summary:
+		//		fade in all elements of this NodeList. Returns an instance of dojo._Animation
+		//	example:
+		//		// fade in all tables with class "blah"
+		//		dojo.query("table.blah").fadeIn().play();
+		return this._anim(dojo, "fadeIn", args); // dojo._Animation
+	},
+
+	fadeOut: function(args){
+		//	summary:
+		//		fade out all elements of this NodeList. Returns an instance of dojo._Animation
+		//	example:
+		//		// fade out all elements with class "zork"
+		//		dojo.query(".zork").fadeOut().play();
+		//
+		//		// fade them on a delay and do something at the end
+		//		var fo = dojo.query(".zork").fadeOut();
+		//		dojo.connect(fo, "onEnd", function(){ /*...*/ });
+		//		fo.play();
+		return this._anim(dojo, "fadeOut", args); // dojo._Animation
+	},
+
+	animateProperty: function(args){
+		//	summary:
+		//		see dojo.animateProperty(). Animate all elements of this
+		//		NodeList across the properties specified.
+		//	example:
+		//		dojo.query(".zork").animateProperty({
+		//			duration: 500,
+		//			properties: { 
+		//				color:		{ start: "black", end: "white" },
+		//				left:		{ end: 300 } 
+		//			} 
+		//		}).play();
+		return this._anim(dojo, "animateProperty", args); // dojo._Animation
+	}
+});

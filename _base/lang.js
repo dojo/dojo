772dc1f490a78d67aa641f3a1524cd25892b75ac
Alex Russell <slightlyoff@chromium.org>
2007-10-30 10:30:35 +0000

    make dojo.delegate public and add docs. Refs #4820
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@11216 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/lang.js
+++ _base/lang.js
@@ -40,7 +40,15 @@ dojo.isObject = function(/*anything*/ it){
 }
 
 dojo.isArrayLike = function(/*anything*/ it){
-	// return:
+	//	summary:
+	//		similar to dojo.isArray() but more permissive
+	//	description:
+	//		Doesn't strongly test for "arrayness".  Instead, settles for "isn't
+	//		a string or number and has a length property". Arguments objects
+	//		and DOM collections will return true when passed to
+	//		dojo.isArrayLike(), but will return false when passed to
+	//		dojo.isArray().
+	//	return:
 	//		If it walks like a duck and quicks like a duck, return true
 	var d = dojo;
 	return it && it !== undefined &&
@@ -119,7 +127,40 @@ dojo.hitch = function(/*Object*/scope, /*Function|String*/method /*,...*/){
 	return !scope ? method : function(){ return method.apply(scope, arguments || []); }; // Function
 }
 
-dojo._delegate = function(obj, props){
+/*=====
+dojo.delegate = function(obj, props){
+	//	summary:
+	//		returns a new object which "looks" to obj for properties which it
+	//		does not have a value for. Optionally takes a bag of properties to
+	//		seed the returned object with initially. 
+	//	description:
+	//		This is a small implementaton of the Boodman/Crockford delegation
+	//		pattern in JavaScript. An intermediate object constructor mediates
+	//		the prototype chain for the returned object, using it to delegate
+	//		down to obj for property lookup when object-local lookup fails.
+	//		This can be thought of similarly to ES4's "wrap", save that it does
+	//		not act on types but rather on pure objects.
+	//	obj:
+	//		The object to delegate to for properties not found directly on the
+	//		return object or in props.
+	//	props:
+	//		an object containing properties to assign to the returned object
+	//	returns:
+	//		an Object of anonymous type
+	//	example:
+	//	|	var foo = { bar: "baz" };
+	//	|	var thinger = dojo.delegate(foo, { thud: "xyzzy"});
+	//	|	thinger.bar == "baz"; // delegated to foo
+	//	|	foo.xyzzy == undefined; // by definition
+	//	|	thinger.xyzzy == "xyzzy"; // mixed in from props
+	//	|	foo.bar = "thonk";
+	//	|	thinger.bar == "thonk"; // still delegated to foo's bar
+}
+=====*/
+
+
+dojo.delegate = dojo._delegate = function(obj, props){
+
 	// boodman/crockford delegation
 	function TMP(){};
 	TMP.prototype = obj;

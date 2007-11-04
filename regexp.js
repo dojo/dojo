782bcd44b812dacfc18adea2d5574e7d58d12bd9
Alex Russell <slightlyoff@chromium.org>
2007-11-04 23:18:54 +0000

    clean up doc formatting. Refs #4820
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@11383 560b804f-0ae3-0310-86f3-f6aa0a117693

--- regexp.js
+++ regexp.js
@@ -1,9 +1,10 @@
 dojo.provide("dojo.regexp");
 
 dojo.regexp.escapeString = function(/*String*/str, /*String?*/except){
-//summary:
-//	Adds escape sequences for special characters in regular expressions
-// except: a String with special characters to be left unescaped
+	//	summary:
+	//		Adds escape sequences for special characters in regular expressions
+	// except:
+	//		a String with special characters to be left unescaped
 
 //	return str.replace(/([\f\b\n\t\r[\^$|?*+(){}])/gm, "\\$1"); // string
 	return str.replace(/([\.$?*!=:|{}\(\)\[\]\\\/^])/g, function(ch){
@@ -14,28 +15,34 @@ dojo.regexp.escapeString = function(/*String*/str, /*String?*/except){
 	}); // String
 }
 
-dojo.regexp.buildGroupRE = function(/*value or Array of values*/a, /*Function(x) returns a regular expression as a String*/re,
-	/*Boolean?*/nonCapture){
-	// summary: Builds a regular expression that groups subexpressions
-	// description: A utility function used by some of the RE generators.
-	//  The subexpressions are constructed by the function, re, in the second parameter.
-	//  re builds one subexpression for each elem in the array a, in the first parameter.
-	//  Returns a string for a regular expression that groups all the subexpressions.
-	//
-	// a:  A single value or an array of values.
-	// re:  A function.  Takes one parameter and converts it to a regular expression. 
-	// nonCapture: If true, uses non-capturing match, otherwise matches are retained by regular expression. 
+dojo.regexp.buildGroupRE = function(/*Object|Array*/arr, /*Function*/re, /*Boolean?*/nonCapture){
+	//	summary:
+	//		Builds a regular expression that groups subexpressions
+	//	description:
+	//		A utility function used by some of the RE generators. The
+	//		subexpressions are constructed by the function, re, in the second
+	//		parameter.  re builds one subexpression for each elem in the array
+	//		a, in the first parameter. Returns a string for a regular
+	//		expression that groups all the subexpressions.
+	// arr:
+	//		A single value or an array of values.
+	// re:
+	//		A function. Takes one parameter and converts it to a regular
+	//		expression. 
+	// nonCapture:
+	//		If true, uses non-capturing match, otherwise matches are retained
+	//		by regular expression. Defaults to false
 
 	// case 1: a is a single value.
-	if(!(a instanceof Array)){
-		return re(a); // String
+	if(!(arr instanceof Array)){
+		return re(arr); // String
 	}
 
 	// case 2: a is an array
 	var b = [];
-	for (var i = 0; i < a.length; i++){
+	for(var i = 0; i < arr.length; i++){
 		// convert each elem to a RE
-		b.push(re(a[i]));
+		b.push(re(arr[i]));
 	}
 
 	 // join the REs as alternatives in a RE group.
@@ -43,7 +50,10 @@ dojo.regexp.buildGroupRE = function(/*value or Array of values*/a, /*Function(x)
 }
 
 dojo.regexp.group = function(/*String*/expression, /*Boolean?*/nonCapture){
-	// summary: adds group match to expression
-	// nonCapture: If true, uses non-capturing match, otherwise matches are retained by regular expression. 
+	// summary:
+	//		adds group match to expression
+	// nonCapture:
+	//		If true, uses non-capturing match, otherwise matches are retained
+	//		by regular expression. 
 	return "(" + (nonCapture ? "?:":"") + expression + ")"; // String
 }

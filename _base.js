19326680d0c0efd37d1c327ae489c4d3feb6530c
James Burke <jrburke@gmail.com>
2008-02-23 23:12:54 +0000

    Refs #5887. Gets opera to work after page load in xd situation. Opera was downloading modules as part of the dojo.config.require section, which happened before dojo.i18n was downloaded in the xd case. Switch to using nifty conditional include build comments so we can load dojo.i18n for dojo.xd.js before the dojo.config.require section runs in _base/browser.js \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@12660 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base.js
+++ _base.js
@@ -1,4 +1,7 @@
 dojo.provide("dojo._base");
+//>>includeStart("i18nXdInclude", fileName.indexOf("dojo.xd.js") != -1 && kwArgs.loader == "xdomain");
+dojo.require("dojo.i18n");
+//>>includeEnd("i18nXdInclude");
 dojo.require("dojo._base.lang");
 dojo.require("dojo._base.declare");
 dojo.require("dojo._base.connect");

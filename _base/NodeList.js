7f40250595af7b300b08d35b223151b8ad8713b2
Alex Russell <slightlyoff@chromium.org>
2008-03-03 03:52:30 +0000

    shorten NodeList code, add toggleClass(), and update documentation. Refs #4205. Refs #3961. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@12818 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/NodeList.js
+++ _base/NodeList.js
@@ -7,12 +7,18 @@ dojo.require("dojo._base.array");
 	var d = dojo;
 
 	var tnl = function(arr){
+		// decorate an array to make it look like a NodeList
 		arr.constructor = dojo.NodeList;
 		dojo._mixin(arr, dojo.NodeList.prototype);
 		return arr;
 	}
 
-	var _mapIntoDojo = function(func){
+	var _mapIntoDojo = function(func, alwaysThis){
+		// returns a function which, when executed in the scope of its caller,
+		// applies the passed arguments to a particular dojo.* function (named
+		// in func) and aggregates the returns. if alwaysThis is true, it
+		// always returns the scope object and not the collected returns from
+		// the Dojo method
 		return function(){
 			var _a = arguments;
 			var aa = d._toArray(_a, 0, [null]);
@@ -20,7 +26,7 @@ dojo.require("dojo._base.array");
 				aa[0] = i;
 				return d[func].apply(d, aa);
 			});
-			return ( (_a.length > 1) || !d.isString(_a[0]) ) ? this : s; // String||dojo.NodeList
+			return (alwaysThis || ( (_a.length > 1) || !d.isString(_a[0]) )) ? this : s; // String||dojo.NodeList
 		}
 	};
 
@@ -81,7 +87,7 @@ dojo.require("dojo._base.array");
 			//		This method behaves exactly like the Array.splice method
 			//		with the caveat that it returns a dojo.NodeList and not a
 			//		raw Array. For more details, see:
-			//			http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:splice
+			//			<http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:splice>
 			// index: Integer
 			//		begin can be a positive or negative integer, with positive
 			//		integers noting the offset to begin at, and negative
@@ -108,7 +114,7 @@ dojo.require("dojo._base.array");
 			//		This method behaves exactly like the Array.concat method
 			//		with the caveat that it returns a dojo.NodeList and not a
 			//		raw Array. For more details, see:
-			//			http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:concat
+			//			<http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:concat>
 			// item: Object...?
 			//		Any number of optional parameters may be passed in to be
 			//		spliced into the NodeList
@@ -128,7 +134,7 @@ dojo.require("dojo._base.array");
 			//		The loction to start searching from. Optional. Defaults to 0.
 			//	description:
 			//		For more details on the behavior of indexOf, see:
-			//			http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:indexOf
+			//			<http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:indexOf>
 			//	returns:
 			//		Positive Integer or 0 for a match, -1 of not found.
 			return d.indexOf(this, value, fromIndex); // Integer
@@ -140,7 +146,7 @@ dojo.require("dojo._base.array");
 			//		acted-on array is implicitly this NodeList
 			//	description:
 			//		For more details on the behavior of lastIndexOf, see:
-			//			http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:lastIndexOf
+			//			<http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:lastIndexOf>
 			// value: Object
 			//		The value to search for.
 			// fromIndex: Integer?
@@ -152,8 +158,8 @@ dojo.require("dojo._base.array");
 
 		every: function(/*Function*/callback, /*Object?*/thisObject){
 			//	summary:
-			//		see dojo.every() and:
-			//			http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:every
+			//		see `dojo.every()` and:
+			//			<http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Objects:Array:every>
 			//		Takes the same structure of arguments and returns as
 			//		dojo.every() with the caveat that the passed array is
 			//		implicitly this NodeList
@@ -185,7 +191,8 @@ dojo.require("dojo._base.array");
 			//		array is implicitly this NodeList
 
 			d.forEach(this, callback, thisObj);
-			return this; // dojo.NodeList non-standard return to allow easier chaining
+			// non-standard return to allow easier chaining
+			return this; // dojo.NodeList 
 		},
 
 		// custom methods
@@ -224,24 +231,71 @@ dojo.require("dojo._base.array");
 			//		if no value is passed, the result is an array of strings.
 			//		If a value is passed, the return is this NodeList
 		},
+
+		addClass: function(className){
+			//	summary:
+			//		adds the specified class to every node in the list
+			//	className: String
+			//		the CSS class to add
+			//	return:
+			//		dojo.NodeList, this list
+		},
+
+		removeClass: function(className){
+			//	summary:
+			//		removes the specified class from every node in the list
+			//	className: String
+			//		the CSS class to add
+			//	return:
+			//		dojo.NodeList, this list
+		},
+
+		toggleClass: function(className, condition){
+			//	summary:
+			//		Adds a class to node if not present, or removes if present.
+			//		Pass a boolean condition if you want to explicitly add or remove.
+			//	condition: Boolean?
+			//		If passed, true means to add the class, false means to remove.
+			//	className: String
+			//		the CSS class to add
+			//	return: dojo.NodeList
+			//		this list
+		},
+
+		connect: function(methodName, objOrFunc, funcName){
+			//	summary:
+			//		attach event handlers to every item of the NodeList. Uses dojo.connect()
+			//		so event properties are normalized
+			//	methodName: String
+			//		the name of the method to attach to. For DOM events, this should be
+			//		the lower-case name of the event
+			//	objOrFunc: Object|Function|String
+			//		if 2 arguments are passed (methodName, objOrFunc), objOrFunc should
+			//		reference a function or be the name of the function in the global
+			//		namespace to attach. If 3 arguments are provided
+			//		(methodName, objOrFunc, funcName), objOrFunc must be the scope to 
+			//		locate the bound function in
+			//	funcName: String?
+			//		optional. A string naming the function in objOrFunc to bind to the
+			//		event. May also be a function reference.
+			//	example:
+			//		add an onclick handler to every button on the page
+			//		|	dojo.query("div:nth-child(odd)").connect("onclick", function(e){
+			//		|		console.debug("clicked!");
+			//		|	});
+			// example:
+			//		attach foo.bar() to every odd div's onmouseover
+			//		|	dojo.query("div:nth-child(odd)").connect("onmouseover", foo, "bar");
+		},
 		=====*/
 		attr: _mapIntoDojo("attr"),
 		style: _mapIntoDojo("style"),
+		addClass: _mapIntoDojo("addClass", true),
+		removeClass: _mapIntoDojo("removeClass", true),
+		toggleClass: _mapIntoDojo("toggleClass", true),
+		connect: _mapIntoDojo("connect", true),
 
-		addClass: function(/*String*/ className){
-			// summary:
-			//		adds the specified class to every node in the list
-			//
-			this.forEach(function(i){ d.addClass(i, className); });
-			return this;
-		},
-
-		removeClass: function(/*String*/ className){
-			this.forEach(function(i){ d.removeClass(i, className); });
-			return this;
-		},
-
-		// FIXME: toggleClass()? connectPublisher()? connectRunOnce()?
+		// FIXME: connectPublisher()? connectRunOnce()?
 
 		place: function(/*String||Node*/ queryOrNode, /*String*/ position){
 			//	summary:
@@ -253,50 +307,13 @@ dojo.require("dojo._base.array");
 			//		for relative positioning.
 			//	position:
 			//		can be one of:
-			//			"last"||"end" (default)
-			//			"first||"start"
-			//			"before"
-			//			"after"
+			//			* "last"||"end" (default)
+			//			* "first||"start"
+			//			* "before"
+			//			* "after"
 			// 		or an offset in the childNodes property
 			var item = d.query(queryOrNode)[0];
-			position = position || "last";
-
-			for(var x=0; x<this.length; x++){
-				d.place(this[x], item, position);
-			}
-			return this; // dojo.NodeList
-		},
-
-		connect: function(/*String*/ methodName, /*Object||Function||String*/ objOrFunc, /*String?*/ funcName){
-			//	summary:
-			//		attach event handlers to every item of the NodeList. Uses dojo.connect()
-			//		so event properties are normalized
-			//	methodName:
-			//		the name of the method to attach to. For DOM events, this should be
-			//		the lower-case name of the event
-			//	objOrFunc:
-			//		if 2 arguments are passed (methodName, objOrFunc), objOrFunc should
-			//		reference a function or be the name of the function in the global
-			//		namespace to attach. If 3 arguments are provided
-			//		(methodName, objOrFunc, funcName), objOrFunc must be the scope to 
-			//		locate the bound function in
-			//	funcName:
-			//		optional. A string naming the function in objOrFunc to bind to the
-			//		event. May also be a function reference.
-			//	example:
-			//		add an onclick handler to every button on the page
-			//		|	dojo.query("div:nth-child(odd)").connect("onclick", function(e){
-			//		|		console.debug("clicked!");
-			//		|	});
-			// example:
-			//		attach foo.bar() to every odd div's onmouseover
-			//		|	dojo.query("div:nth-child(odd)").onclick("onmouseover", foo, "bar");
-
-			// FIXME: if we only get a function, should the scope be the node?
-			this.forEach(function(item){
-				d.connect(item, methodName, objOrFunc, funcName);
-			});
-			return this; // dojo.NodeList
+			return this.forEach(function(i){ d.place(i, item, (position||"last")); }); // dojo.NodeList
 		},
 
 		orphan: function(/*String?*/ simpleFilter){
@@ -304,8 +321,10 @@ dojo.require("dojo._base.array");
 			//		removes elements in this list that match the simple
 			//		filter from their parents and returns them as a new
 			//		NodeList.
-			//	simpleFilter: single-expression CSS filter
-			//	return: a dojo.NodeList of all of the elements orpahned
+			//	simpleFilter:
+			//		single-expression CSS filter
+			//	return:
+			//		`dojo.NodeList` the orpahned elements 
 			var orphans = simpleFilter ? d._filterQueryResult(this, simpleFilter) : this;
 			orphans.forEach(function(item){
 				if(item.parentNode){
@@ -325,11 +344,11 @@ dojo.require("dojo._base.array");
 			//		Represents the nodes to be adopted relative to the
 			//		first element of this NodeList.
 			//	position:
-			//		optional. One of:
-			//			"last"||"end" (default)
-			//			"first||"start"
-			//			"before"
-			//			"after"
+			//		can be one of:
+			//			* "last"||"end" (default)
+			//			* "first||"start"
+			//			* "before"
+			//			* "after"
 			// 		or an offset in the childNodes property
 			var item = this[0];
 			return d.query(queryOrListOrNode).forEach(function(ai){ d.place(ai, item, position || "last"); }); // dojo.NodeList
@@ -412,6 +431,15 @@ dojo.require("dojo._base.array");
 			//		list, with an optional position argument. If no position
 			//		argument is provided, the content is appended to the end of
 			//		each item.
+			//	content:
+			//		the HTML in string format to add at position to every item
+			//	position:
+			//		can be one of:
+			//			* "last"||"end" (default)
+			//			* "first||"start"
+			//			* "before"
+			//			* "after"
+			// 		or an offset in the childNodes property
 			//	example:
 			//		appends content to the end if the position is ommitted
 			//	|	dojo.query("h3 > p").addContent("hey there!");
@@ -421,15 +449,6 @@ dojo.require("dojo._base.array");
 			//	example:
 			//		adds a header before each element of the list
 			//	|	dojo.query(".note").addContent("<h4>NOTE:</h4>", "before");
-			//	content:
-			//		the HTML in string format to add at position to every item
-			//	position:
-			//		One of:
-			//			"last"||"end" (default)
-			//			"first||"start"
-			//			"before"
-			//			"after"
-			//		or an integer offset in the childNodes property
 			var ta = d.doc.createElement("span");
 			if(d.isString(content)){
 				ta.innerHTML = content;
@@ -448,17 +467,25 @@ dojo.require("dojo._base.array");
 			});
 			return this; // dojo.NodeList
 		},
+
+		empty: function(){
+			//	summary:
+			//		clears all content from each node in the list
+			return this.forEach("item.innerHTML='';"); // dojo.NodeList
+
+			// FIXME: should we be checking for and/or disposing of widgets below these nodes?
+		},
 		
 		instantiate: function(/*String|Object*/ declaredClass, /*Object?*/ properties){
-			// summary:
-			//		Create a new instance of a specified class, using the specified properties
-			//		and each node in the nodeList as a srcNodeRef
+			//	summary:
+			//		Create a new instance of a specified class, using the
+			//		specified properties and each node in the nodeList as a
+			//		srcNodeRef
 			//
 			var c = d.isFunction(declaredClass) ? declaredClass : d.getObject(declaredClass);
-			this.forEach(function(i){
+			return this.forEach(function(i){
 				new c(properties||{},i);
-			})
-			return this; // dojo.NodeList
+			}) // dojo.NodeList
 		}
 
 	});

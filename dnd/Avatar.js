36fe6d06ff504990d2fa5b3cce7d11c83538417c
Eugene Lazutkin <eugene@lazutkin.com>
2008-06-13 19:22:30 +0000

    dnd: adding the missing query restriction in the avatar, thx Bryan Forbes! Refs #5687. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@13996 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dnd/Avatar.js
+++ dnd/Avatar.js
@@ -67,7 +67,7 @@ dojo.declare("dojo.dnd.Avatar", null, {
 		// summary: updates the avatar to reflect the current DnD state
 		dojo[(this.manager.canDropFlag ? "add" : "remove") + "Class"](this.node, "dojoDndAvatarCanDrop");
 		// replace text
-		dojo.query("tr.dojoDndAvatarHeader td").forEach(function(node){
+		dojo.query("tr.dojoDndAvatarHeader td", this.node).forEach(function(node){
 			node.innerHTML = this._generateText();
 		}, this);
 	},

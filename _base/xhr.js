92bb577421dc3b5c0fb352321f5749f8360d6729
Dylan Schiemann <dylan@dojotoolkit.org>
2008-07-06 04:39:42 +0000

    refs #6793, add information about what an error case is for the error handler for xhr
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@14288 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/xhr.js
+++ _base/xhr.js
@@ -284,7 +284,9 @@ dojo.require("dojo._base.query");
 		//	error: Function?
 		//		function(response, ioArgs){}. response is an Object, ioArgs
 		//		is of type dojo.__IoCallbackArgs. The error function will
-		//		be called in an error case. 
+		//		be called in an error case.  The error case is when the 
+		//		send or receive of a request fails, the url is invalid, the
+		//		connection times out, etc. 
 		//	handle: Function?
 		//		function(response, ioArgs){}. response is an Object, ioArgs
 		//		is of type dojo.__IoCallbackArgs. The handle function will

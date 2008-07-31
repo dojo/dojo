69a4f3e58c00cf8805e44caee0ec40995a69801d
Nathan Toone <nathan@toonetown.com>
2008-07-31 13:51:42 +0000

    Fixes #7326 - account for padding and margin on the body when calculating position for onFirstMove
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@14645 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dnd/Mover.js
+++ dnd/Mover.js
@@ -60,8 +60,20 @@ dojo.declare("dojo.dnd.Mover", null, {
 			default:
 				s.position = "absolute";	// enforcing the absolute mode
 				var m = dojo.marginBox(this.node);
-				l = m.l;
-				t = m.t;
+				// event.pageX/pageY (which we used to generate the initial
+				// margin box) includes padding and margin set on the body.
+				// However, setting the node's position to absolute and then
+				// doing dojo.marginBox on it *doesn't* take that additional
+				// space into account - so we need to subtract the combined
+				// padding and margin.  We use getComputedStyle and
+				// _getMarginBox/_getContentBox to avoid the extra lookup of
+				// the computed style. 
+				var b = dojo.doc.body;
+				var bs = dojo.getComputedStyle(b);
+				var bm = dojo._getMarginBox(b, bs);
+				var bc = dojo._getContentBox(b, bs);
+				l = m.l - (bc.l - bm.l);
+				t = m.t - (bc.t - bm.t);
 				break;
 		}
 		this.marginBox.l = l - this.marginBox.l;

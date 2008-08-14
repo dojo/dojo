91e1db0d7c474879bd9d861e359718c7f05a6139
Douglas Hays <doughays@dojotoolkit.org>
2008-08-14 23:44:19 +0000

    References #7441.  Proxy commit for haysmark. Remove security function. Add delay parameter to doh.robot.scrollIntoView so its properly sequenced.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@14852 560b804f-0ae3-0310-86f3-f6aa0a117693

--- robot.js
+++ robot.js
@@ -8,14 +8,36 @@ dojo.require("doh.robot");
 dojo.mixin(doh.robot,{
 
 	// TODO: point to dojo.scrollIntoView post 1.2
-	_scrollIntoView : function(/*DOMNode*/ node){
+	_scrollIntoView : function(/*String||DOMNode||Function*/ node){
 		// summary:
 		//		Scroll the passed node into view, if it is not.
 		// 		Stub to be replaced dijit.robot.
+		if(typeof node == "function"){
+			// if the user passed a function returning a node, evaluate it
+			node = node();
+		}
 		node.scrollIntoView(false);
 	},
 
-	mouseMoveAt : function(/*Function*/ sec, /*String||DOMNode*/ node, /*Number*/ delay, /*Number*/ offsetX, /*Number*/ offsetY){
+	scrollIntoView : function(/*String||DOMNode||Function*/ node, /*Number, optional*/ delay){
+		// summary:
+		//		Scroll the passed node into view, if it is not.
+		//
+		// node:
+		//		The id of the node, or the node itself, to move the mouse to.
+		//		If you pass an id or a function that returns a node, the node will not be evaluated until the movement executes.
+		//		This is useful if you need to move the mouse to an node that is not yet present.
+		//
+		// delay:
+		//		Delay, in milliseconds, to wait before firing.
+		//		The delay is a delta with respect to the previous automation call.
+		//
+		doh.robot.sequence(function(){
+			doh.robot._scrollIntoView(node);
+		}, delay);
+	},
+
+	mouseMoveAt : function(/*String||DOMNode||Function*/ node, /*Integer, optional*/ delay, /*Number, optional*/ offsetX, /*Number, optional*/ offsetY, /*Integer, optional*/ duration){
 		// summary:
 		//		Moves the mouse over the specified node at the specified relative x,y offset.
 		//
@@ -23,23 +45,19 @@ dojo.mixin(doh.robot,{
 		// 		Moves the mouse over the specified node at the specified relative x,y offset.
 		// 		You should manually scroll off-screen nodes into view; use dijit.robot for automatic scrolling support.
 		// 		If you do not specify an offset, mouseMove will default to move to the middle of the node.
-		// 		Example: to move the mouse over a ComboBox's down arrow node, call doh.mouseMove(sec, dijit.byId('setvaluetest').downArrowNode);
-		//
-		// sec:
-		//		new Function('return window')
-		//		Public key that verifies that the calling window is allowed to automate user input.
+		// 		Example: to move the mouse over a ComboBox's down arrow node, call doh.mouseMoveAt(dijit.byId('setvaluetest').downArrowNode);
 		//
 		// node:
 		//		The id of the node, or the node itself, to move the mouse to.
-		//		If you pass an id, the node will not be evaluated until the movement executes.
+		//		If you pass an id or a function that returns a node, the node will not be evaluated until the movement executes.
 		//		This is useful if you need to move the mouse to an node that is not yet present.
 		//
 		// delay:
 		//		Delay, in milliseconds, to wait before firing.
 		//		The delay is a delta with respect to the previous automation call.
 		//		For example, the following code ends after 600ms:
-		//			doh.mouseClick(sec,{left:true},100) // first call; wait 100ms
-		//			doh.typeKeys(sec,"dij",500) // 500ms AFTER previous call; 600ms in all
+		//			doh.mouseClick({left:true}, 100) // first call; wait 100ms
+		//			doh.typeKeys("dij", 500) // 500ms AFTER previous call; 600ms in all
 		//
 		// offsetX:
 		//		x offset relative to the node, in pixels, to move the mouse. The default is half the node's width.
@@ -47,10 +65,19 @@ dojo.mixin(doh.robot,{
 		// offsetY:
 		//		y offset relative to the node, in pixels, to move the mouse. The default is half the node's height.
 		//
+		// duration:
+		//		Approximate time Robot will spend moving the mouse
+		//		The default is 100ms.
+		//
 
 		doh.robot._assertRobot();
+		duration = duration||100;
+		this.sequence(function(){
+		if(typeof node == "function"){
+			// if the user passed a function returning a node, evaluate it
+			node = node();
+		}
 		if(!node) return;
-		this._sequence(function(){
 		node=dojo.byId(node);
 		if(offsetY === undefined){
 			var box=dojo.contentBox(node);
@@ -64,8 +91,8 @@ dojo.mixin(doh.robot,{
 		var c = dojo.coords(node);
 		x += c.x;
 		y += c.y;
-		doh.robot._mouseMove(sec, x, y);
-		},delay);
+		doh.robot._mouseMove(x, y, false, duration);
+		},delay,duration);
 	}
 });
 

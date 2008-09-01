e612bce433d22360e23675148e102227a6c48783
Adam Peller <github@peller.org>
2008-09-01 03:46:52 +0000

    Use arbitrary subset of currencies by default. Refs #7188
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@15049 560b804f-0ae3-0310-86f3-f6aa0a117693

--- cldr/nls/currency.js
+++ cldr/nls/currency.js
@@ -1,15 +1,9 @@
 // generated from ldml/main/*.xml, xpath: ldml/numbers/currencies
 ({
-	BRL_displayName:"BRL",
-	BRL_symbol:"R$",
 	EUR_displayName:"EUR",
 	EUR_symbol:"€",
 	GBP_displayName:"GBP",
 	GBP_symbol:"UK£",
-	INR_displayName:"INR",
-	INR_symbol:"0≤Rs.|1≤Re.|1<Rs.",
-	ITL_displayName:"ITL",
-	ITL_symbol:"IT₤",
 	JPY_displayName:"JPY",
 	JPY_symbol:"JP¥",
 	USD_displayName:"USD",

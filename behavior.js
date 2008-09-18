83cce07bb6c76502eaf90ebd8b5335c0bb995ab2
Peter E Higgins <dante@dojotoolkit.org>
2008-09-18 13:55:19 +0000

    fixes #6291 - adding package-level docs and fixing syntax of dojo.behavior API docs.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@15274 560b804f-0ae3-0310-86f3-f6aa0a117693

--- behavior.js
+++ behavior.js
@@ -1,6 +1,22 @@
 dojo.provide("dojo.behavior");
 
 dojo.behavior = new function(){
+	// summary: 
+	//		Utility for unobtrusive/progressive event binding, DOM traversal,
+	//		and manipulation.
+	//
+	// description:
+	//		
+	//		A very simple, lightweight mechanism for applying code to 
+	//		existing documents, based around `dojo.query` (CSS3 selectors) for node selection, 
+	//		and a simple two-command API: `dojo.behavior.add()` and `dojo.behavior.apply()`;
+	//	
+	//		Behaviors apply to a given page, and are registered following the syntax 
+	//		options described by `dojo.behavior.add` to match nodes to actions, or "behaviors".
+	//		
+	//		Added behaviors are applied to the current DOM when .apply() is called,
+	//		matching only new nodes found since .apply() was last called. 
+	//		
 	function arrIn(obj, name){
 		if(!obj[name]){ obj[name] = []; }
 		return obj[name];
@@ -23,83 +39,113 @@ dojo.behavior = new function(){
 
 	// FIXME: need a better test so we don't exclude nightly Safari's!
 	this._behaviors = {};
-	this.add = function(behaviorObj){
+	this.add = function(/* Object */behaviorObj){
 		//	summary:
-		//		add the specified behavior to the list of behaviors which will
+		//		Add the specified behavior to the list of behaviors, ignoring existing
+		//		matches. 
+		//
+		//	description:
+		//		Add the specified behavior to the list of behaviors which will
 		//		be applied the next time apply() is called. Calls to add() for
 		//		an already existing behavior do not replace the previous rules,
 		//		but are instead additive. New nodes which match the rule will
 		//		have all add()-ed behaviors applied to them when matched.
-		//
-		//	description:
-		//		behavior objects are specified in the following format(s):
-		//		
-		//			{ 
-		//			 	"#id": {
-		//					"found": function(element){
-		//						// ...
-		//					},
-		//			
-		//					"onblah": {targetObj: foo, targetFunc: "bar"},
-		//			
-		//					"onblarg": "/foo/bar/baz/blarg",
-		//			
-		//					"onevent": function(evt){
-		//					},
-		//			
-		//					"onotherevent: function(evt){
-		//						// ...
-		//					}
-		//				},
-		//			
-		//				"#id2": {
-		//					// ...
-		//				},
-		//			
-		//				"#id3": function(element){
-		//					// ...
-		//				},
-		//			
-		//				// publish the match on a topic
-		//				"#id4": "/found/topic/name",
-		//			
-		//				// match all direct descendants
-		//				"#id4 > *": function(element){
-		//					// ...
-		//				},
-		//			
-		//				// match the first child node that's an element
-		//				"#id4 > :first-child": { ... },
-		//			
-		//				// match the last child node that's an element
-		//				"#id4 > :last-child":  { ... },
-		//			
-		//				// all elements of type tagname
-		//				"tagname": {
-		//					// ...
-		//				},
-		//			
-		//				"tagname1 tagname2 tagname3": {
-		//					// ...
-		//				},
-		//			
-		//				".classname": {
-		//					// ...
-		//				},
-		//			
-		//				"tagname.classname": {
-		//					// ...
-		//				},
-		//			}
 		//		
 		//		The "found" method is a generalized handler that's called as soon
 		//		as the node matches the selector. Rules for values that follow also
 		//		apply to the "found" key.
 		//		
-		//		The "on*" handlers are attached with dojo.connect(). 
+		//		The "on*" handlers are attached with `dojo.connect()`, using the 
+		//		matching node
 		//		
 		//		If the value corresponding to the ID key is a function and not a
 		//		list, it's treated as though it was the value of "found".
+		//
+		// 		dojo.behavior.add() can be called any number of times before 
+		//		the DOM is ready. `dojo.behavior.apply()` is called automatically
+		//		by `dojo.addOnLoad`, though can be called to re-apply previously added
+		//		behaviors anytime the DOM changes.
+		//
+		//		There are a variety of formats permitted in the behaviorObject
+		//	
+		//	example:
+		//		Simple list of properties. "found" is special. "Found" is assumed if 
+		//		no property object for a given selector, and property is a function.
+		//
+		//	|	dojo.behavior.add({
+		//	|		"#id": {
+		//	|			"found": function(element){
+		//	|				// node match found
+		//	|			},
+		//	|			"onclick": function(evt){
+		//	|				// register onclick handler for found node
+		//	|			}
+		//	|		},
+		// 	|		"#otherid": function(element){
+		//	|			// assumes "found" with this syntax
+		//	|		}
+		//	|	});
+		//
+		//	example: 
+		//		 If property is a string, a dojo.publish will be issued on the channel:
+		//
+		//	|	dojo.behavior.add({
+		//	|		// dojo.publish() whenever class="noclick" found on anchors
+		//	|		"a.noclick": "/got/newAnchor",
+		//	|		"div.wrapper": {
+		//	|			"onclick": "/node/wasClicked"
+		//	|		}
+		//	|	});
+		//	|	dojo.subscribe("/got/newAnchor", function(node){
+		//	|		// handle node finding when dojo.behavior.apply() is called, 
+		//	|		// provided a newly matched node is found.
+		//	|	});
+		//
+		//	example:
+		//		Scoping can be accomplished by passing an object as a property to 
+		//		a connection handle (on*):
+		//	
+		//	|	dojo.behavior.add({ 
+		//	|		 	"#id": {
+		//	|				// like calling dojo.hitch(foo,"bar"). execute foo.bar() in scope of foo
+		//	|				"onmouseenter": { targetObj: foo, targetFunc: "bar" },
+		//	|				"onmouseleave": { targetObj: foo, targetFunc: "baz" }
+		//	|			}
+		//	|	});
+		//
+		//	example: 
+		//		Bahaviors match on CSS3 Selectors, powered by dojo.query. Example selectors:
+		//
+		//	|	dojo.behavior.add({
+		//	|		// match all direct descendants
+		//	|		"#id4 > *": function(element){
+		//	|			// ...
+		//	|		},
+		//	|		
+		//	|		// match the first child node that's an element
+		//	|		"#id4 > :first-child": { ... },
+		//	|		
+		//	|		// match the last child node that's an element
+		//	|		"#id4 > :last-child":  { ... },
+		//	|		
+		//	|		// all elements of type tagname
+		//	|		"tagname": {
+		//	|			// ...
+		//	|		},
+		//	|		
+		//	|		"tagname1 tagname2 tagname3": {
+		//	|			// ...
+		//	|		},
+		//	|		
+		//	|		".classname": {
+		//	|			// ...
+		//	|		},
+		//	|		
+		//	|		"tagname.classname": {
+		//	|			// ...
+		//	|		}
+		//	|	});
+		//   
 
 		var tmpObj = {};
 		forIn(behaviorObj, this, function(behavior, name){
@@ -138,13 +184,27 @@ dojo.behavior = new function(){
 
 	this.apply = function(){
 		// summary:
-		//		applies all currently registered behaviors to the document,
+		//		Applies all currently registered behaviors to the document.
+		// 
+		// description:
+		//		Applies all currently registered behaviors to the document,
 		//		taking care to ensure that only incremental updates are made
-		//		since the last time add() or apply() were called. If new
-		//		matching nodes have been added, all rules in a behavior will be
+		//		since the last time add() or apply() were called. 
+		//	
+		//		If new matching nodes have been added, all rules in a behavior will be
 		//		applied to that node. For previously matched nodes, only
 		//		behaviors which have been added since the last call to apply()
 		//		will be added to the nodes.
+		//
+		//		apply() is called once automatically by `dojo.addOnLoad`, so 
+		//		registering behaviors with `dojo.behavior.add` before the DOM is
+		//		ready is acceptable, provided the dojo.behavior module is ready.
+		//		
+		//		Calling appy() manually after manipulating the DOM is required 
+		//		to rescan the DOM and apply newly .add()ed behaviors, or to match
+		//		nodes that match existing behaviors when those nodes are added to 
+		//		the DOM.
+		//		
 		forIn(this._behaviors, function(tBehavior, id){
 			dojo.query(id).forEach( 
 				function(elem){

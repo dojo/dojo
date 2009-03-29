f33464d65a6c49caafe30e6cb7d8fe9ac7334224
Peter E Higgins <dante@dojotoolkit.org>
2009-03-29 13:34:57 +0000

    fixes #9007 - trunk is reopen "officially". 1.3 branch in place.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@17146 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -208,7 +208,7 @@ dojo.global = {
 		//		Descriptor flag. If total version is "1.2.0beta1", will be "beta1"
 		//	revision: Number
 		//		The SVN rev from which dojo was pulled
-		major: 1, minor: 3, patch: 0, flag: "dev",
+		major: 1, minor: 4, patch: 0, flag: "dev",
 		revision: rev ? +rev[0] : NaN,
 		toString: function(){
 			with(d.version){

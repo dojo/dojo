40853e40ea5f1dbc1301f8c0560fa37ffd02c0f9
Nathan Toone <nathan@toonetown.com>
2009-04-15 23:21:01 +0000

    Refs #9131 - remove extraneous boolean checks for dojo.isString()
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@17293 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/lang.js
+++ _base/lang.js
@@ -5,7 +5,7 @@ dojo.provide("dojo._base.lang");
 dojo.isString = function(/*anything*/ it){
 	//	summary:
 	//		Return true if it is a String
-	return !!arguments.length && it != null && (typeof it == "string" || it instanceof String); // Boolean
+	return it && (typeof it == "string" || it instanceof String); // Boolean
 }
 
 dojo.isArray = function(/*anything*/ it){

190bb29c69bd0717eafc7105c44fe90915d66451
James Burke <jrburke@gmail.com>
2009-04-17 18:12:00 +0000

    Fixes #9001: clarify docs on the purpose of dojo.exists.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@17325 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -375,7 +375,10 @@ dojo.global = {
 		//		determine if an object supports a given method
 		//	description: 
 		//		useful for longer api chains where you have to test each object in
-		//		the chain
+		//		the chain. Useful only for object and method detection.
+		//		Not useful for testing generic properties on an object.
+		//		In particular, dojo.exists("foo.bar") when foo.bar = ""
+		//		will return false. Use ("bar" in foo) to test for those cases.
 		//	name: 	
 		//		Path to an object, in the form "A.B.C".
 		//	obj:

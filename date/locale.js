7c5af9e571665ec2435508186a765293af7e7125
Adam Peller <github@peller.org>
2009-06-05 03:51:18 +0000

    Merge [17730] to correct comment.  Refs #9366
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@17731 560b804f-0ae3-0310-86f3-f6aa0a117693

--- date/locale.js
+++ date/locale.js
@@ -435,7 +435,9 @@ dojo.date.locale.parse = function(/*String*/value, /*dojo.date.locale.__FormatOp
 		return null;
 	}
 
-	// Check for underflow, due to DST shifts.  See #9633
+	// Check for underflow, due to DST shifts.  See #9366
+	// This assumes a 1 hour dst shift correction at midnight
+	// We could compare the timezone offset after the shift and add the difference instead.
 	if(dateToken && dateObject.getDate() < result[2]){
 		dateObject = dojo.date.add(dateObject, "hour", 1);
 	}

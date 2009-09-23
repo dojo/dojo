d15d7f9b5a7a5f10fe2b1356051c0cce1d331edc
James Burke <jrburke@gmail.com>
2009-09-23 22:43:16 +0000

    Fixes #9925, dojo.ready as an alias for dojo.addOnLoad. Docs, release notes updated too.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@20216 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/loader.js
+++ _base/_loader/loader.js
@@ -196,7 +196,7 @@
 		}
 	}
 
-	dojo.addOnLoad = function(/*Object?*/obj, /*String|Function*/functionName){
+	dojo.ready = dojo.addOnLoad = function(/*Object?*/obj, /*String|Function*/functionName){
 		// summary:
 		//		Registers a function to be triggered after the DOM has finished
 		//		loading and widgets declared in markup have been instantiated.

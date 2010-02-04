2a74cacdb73b9a05261f3ae68db2a59a556c9383
Adam Peller <github@peller.org>
2010-02-04 15:42:56 +0000

    more description of dojo.currency.  Refs #8606
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@21304 560b804f-0ae3-0310-86f3-f6aa0a117693

--- currency.js
+++ currency.js
@@ -8,6 +8,14 @@ dojo.require("dojo.cldr.monetary");
 /*=====
 dojo.currency = {
 	// summary: localized formatting and parsing routines for currencies
+	//
+	// description: extends dojo.number to provide culturally-appropriate formatting of values
+	//	in various world currencies, including use of a currency symbol.  The currencies are specified
+	//	by a three-letter international symbol in all uppercase, and support for the currencies is
+	//	provided by the data in `dojo.cldr`.  The scripts generating dojo.cldr specify which
+	//	currency support is included.  A fixed number of decimal places is determined based
+	//	on the currency type and is not determined by the 'pattern' argument.  The fractional
+	//	portion is optional, by default, and variable length decimals are not supported.
 }
 =====*/
 

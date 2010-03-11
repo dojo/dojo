2ccdb5a1fe23fc5cccc189f36c41a8ba38cacbfa
James Burke <jrburke@gmail.com>
2010-03-11 19:24:37 +0000

    Updating dojo version for the tag. \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.0.3/dojo@21556 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -80,7 +80,7 @@
 	//TODOC:  HOW TO DOC THIS?
 	dojo.version = {
 		// summary: version number of this instance of dojo.
-		major: 0, minor: 9, patch: 0, flag: "dev",
+		major: 1, minor: 0, patch: 3, flag: "",
 		revision: Number("$Rev$".match(/[0-9]+/)[0]),
 		toString: function(){
 			with(d.version){

2c12d8a2c9c09896f85738fbe166e71c1002735c
Adam Peller <github@peller.org>
2010-03-11 05:29:16 +0000

    Updating dojo version for the tag.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.2.4/dojo@21540 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -200,7 +200,7 @@ dojo.global = {
 		//		Descriptor flag. If total version is "1.2.0beta1", will be "beta1"
 		//	revision: Number
 		//		The SVN rev from which dojo was pulled
-		major: 1, minor: 1, patch: 0, flag: "dev",
+		major: 1, minor: 2, patch: 4, flag: "",
 		revision: rev ? +rev[0] : 999999, //FIXME: use NaN?
 		toString: function(){
 			with(d.version){

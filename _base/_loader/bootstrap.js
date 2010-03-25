d557dcf27bd09e13888b5864c3b823158cc87453
Adam Peller <github@peller.org>
2010-03-25 03:15:20 +0000

    Updating dojo version for the tag. \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.4.3/dojo@21629 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -242,7 +242,7 @@ dojo.global = {
 	}
 =====*/
 	dojo.version = {
-		major: 1, minor: 4, patch: 0, flag: "dev",
+		major: 1, minor: 4, patch: 3, flag: "",
 		revision: rev ? +rev[0] : NaN,
 		toString: function(){
 			with(d.version){

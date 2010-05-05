1595397b8b64cdb896078a11d7d3a58f9bb35080
Eugene Lazutkin <eugene@lazutkin.com>
2010-05-05 19:24:14 +0000

    gfx: bugfix - added missing shape elements to the list, thx Jared!, !strict, refs #10491.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.4/dojo@22077 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/_loader/hostenv_browser.js
+++ _base/_loader/hostenv_browser.js
@@ -412,7 +412,7 @@ if(typeof window != 'undefined'){
 
 		try{
 			document.namespaces.add("v","urn:schemas-microsoft-com:vml");
-			var vmlElems = ["*", "group", "roundrect", "oval", "shape", "rect", "imagedata"],
+			var vmlElems = ["*", "group", "roundrect", "oval", "shape", "rect", "imagedata", "path", "textpath", "text"],
 				i = 0, l = 1, s = document.createStyleSheet();
 			if(dojo.isIE >= 8){
 				i = 1;

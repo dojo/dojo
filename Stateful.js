4661962f7338a312628a1cbff24988bdcc323f9a
Kris Zyp <kriszyp@gmail.com>
2010-05-11 18:23:30 +0000

    Fixes |this| for callbacks, refs #10839
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@22138 560b804f-0ae3-0310-86f3-f6aa0a117693

--- Stateful.js
+++ Stateful.js
@@ -87,11 +87,12 @@ dojo.declare("dojo.Stateful", null, {
 		
 		var callbacks = this._watchCallbacks;
 		if(!callbacks){
+			var self = this;
 			callbacks = this._watchCallbacks = function(name, oldValue, value, ignoreCatchall){
 				var notify = function(propertyCallbacks){
 					for(var i = 0, l = propertyCallbacks && propertyCallbacks.length; i < l; i++){
 						try{
-							propertyCallbacks[i].call(this, name, oldValue, value);
+							propertyCallbacks[i].call(self, name, oldValue, value);
 						}catch(e){
 							console.error(e);
 						}

1fc63ecab799de5dd72403474243b7c9d7f8a7cb
Kris Zyp <kriszyp@gmail.com>
2010-09-14 20:49:05 +0000

    Properly handle multiple watch listeners (without incurring GC problems), refs #11537
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@22873 560b804f-0ae3-0310-86f3-f6aa0a117693

--- store/Watchable.js
+++ store/Watchable.js
@@ -4,49 +4,62 @@ dojo.store.Watchable = function(store){
 	//	summary:
 	//		The Watch store wrapper takes a store and sets a watch method on query()
 	// 		results that can be used to monitor results for changes
-	var callbacks = [];
+	var queryUpdaters = [], revision = 0;
 	// a Comet driven store could directly call notify to notify watchers when data has
 	// changed on the backend
 	var notifyAll = store.notify = function(object, existingId){
-		for(var i = 0, l = callbacks.length; i < l; i++){
-			callbacks[i](object, existingId);
+		revision++;
+		for(var i = 0, l = queryUpdaters.length; i < l; i++){
+			queryUpdaters[i](object, existingId);
 		}
 	}
 	var originalQuery = store.query;
 	store.query = function(query, options){
 		var results = originalQuery.apply(this, arguments);
-		var queryExecutor = store.queryEngine && store.queryEngine(query, options);
 		if(results && results.forEach){
+			var queryExecutor = store.queryEngine && store.queryEngine(query, options);
+			var queryRevision = revision;
+			var listeners = [], queryUpdater;
 			results.watch = function(listener){
-				var callback = function(changed, existingId){
-					if(queryExecutor){
-						if(existingId){
-							// remove the old one
-							results.forEach(function(object, i){
-								if(store.getIdentity(object) == existingId){
-									results.splice(i, 1);
-									listener(i, existingId);
-								}
-							});
+				if(listeners.push(listener) == 1){
+					// first listener was added, create the query checker and updater
+					queryUpdaters.push(queryUpdater = function(changed, existingId){
+						if(++queryRevision != revision){
+							throw new Error("Query is out of date, you must watch() the query prior to any data modifications");
 						}
-						// add the new one
-						if(changed && 
-								// if a matches function exists, use that (probably more efficient)
-								(queryExecutor.matches ? queryExecutor.matches(changed) : queryExecutor([changed]).length)){ 
-							// TODO: handle paging correctly
-							results.push(changed);
-							results = queryExecutor(results);
-							listener(results.indexOf(changed), undefined, changed);
+						if(queryExecutor){
+							if(existingId){
+								// remove the old one
+								results.forEach(function(object, i){
+									if(store.getIdentity(object) == existingId){
+										results.splice(i, 1);
+										listener(i, existingId);
+									}
+								});
+							}
+							// add the new one
+							if(changed && 
+									// if a matches function exists, use that (probably more efficient)
+									(queryExecutor.matches ? queryExecutor.matches(changed) : queryExecutor([changed]).length)){ 
+								// TODO: handle paging correctly
+								results.push(changed);
+								results = queryExecutor(results);
+								listener(results.indexOf(changed), undefined, changed);
+							}
+						}else{
+							// we don't have a queryEngine, so we don't provide any index information or updates to result sets 
+							listener(undefined, existingId, changed); 
 						}
-					}else{
-						// we don't have a queryEngine, so we don't provide any index information or updates to result sets 
-						listener(undefined, existingId, changed); 
-					}
-				};
-				callbacks.push(callback);
+					});
+				}
 				return {
 					unwatch: function(){
-						callbacks.splice(dojo.indexOf(callbacks, callback), 1);
+						// remove this listener
+						listeners.splice(dojo.indexOf(listeners, listener), 1);
+						if(!listeners.length){
+							// no more listeners, remove the query updater too
+							queryUpdaters.splice(dojo.indexOf(queryUpdaters, queryUpdater), 1);
+						}
 					}
 				}
 			};

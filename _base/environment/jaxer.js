d969f9efc16a749fa8d3534cf57089eca50f0a6c
Kris Zyp <kriszyp@gmail.com>
2010-10-25 21:06:24 +0000

    Removing asynchronous loader files that should not be included yet, refs #11869
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@23048 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/environment/jaxer.js
+++
@@ -1,3 +0,0 @@
-define("dojo/_base/environment/jaxer", ["dojo"], function(dojo) {
-});
-// Copyright (c) 2008-2010, Rawld Gill and ALTOVISO LLC (www.altoviso.com). Use, modification, and distribution subject to terms of license.
--- _base/environment/rhino.js
+++
@@ -1,5 +0,0 @@
-define("dojo/_base/environment/rhino", ["dojo"], function(dojo) {
-  dojo.host= dojo._name= "rhino";
-  //TODO: dojo.host has identical semantics to dojo._name; consider deprecating dojo._name.
-});
-// Copyright (c) 2008-2010, Rawld Gill and ALTOVISO LLC (www.altoviso.com). Use, modification, and distribution subject to terms of license.

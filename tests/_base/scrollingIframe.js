a0962da51427bca19e5ad9c9e582b381960978f1
Douglas Hays <doughays@dojotoolkit.org>
2010-12-13 17:05:05 +0000

    Fixes #12057.  Tweak scrolling/iframe test to not call the native scrollIntoView since Opera was off by 1 pixel.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@23342 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/_base/scrollingIframe.js
+++ tests/_base/scrollingIframe.js
@@ -14,11 +14,11 @@ function runScrollingTest(resultNode){
 	var control = document.getElementById("control");
 	var clientWidth = document.getElementById("clientWidth");
 	var abs1 = document.getElementById("abs1");
-	clientWidth.scrollIntoView(true);
+	window.scrollTo(0, 0); // start with standarized placement
 	setTimeout(function(){
 		var cw = dojo.hitch(dojo, "withGlobal")(window, "position", dojo, [clientWidth, false]);
-		if(cw.x < 0){
-			scrollBy(cw.x, 0); // webkit bug where the whole element is not fully scrolled into view
+		if(cw.x != 0){
+			scrollBy(cw.x, 0); // scroll width:100% control element fully into view
 		}
 		var p = dojo.hitch(dojo, "withGlobal")(window, "position", dojo, [control, true]);
 		abs1.style.left = p.x + "px";
@@ -45,7 +45,7 @@ function runScrollingTest(resultNode){
 
 function genScrollingTestNodes(hScroll, vScroll, large){
 	document.write(
-		'<DIV id="clientWidth" style="background-color:yellow;">&nbsp;</DIV>' +
+		'<DIV id="clientWidth" style="background-color:black;">&nbsp;</DIV>' +
 		'<DIV id="abs1" style="position:absolute;background-color:red;left:0;top:0;width:1em;font-family:monospace;font-size:16px;">&nbsp;</DIV>' +
 		'<CENTER id="mode"></CENTER>' +
 		'<DIV id="control" style="width:2em;height:2em;font-family:monospace;font-size:16px;background-color:cyan;margin:0 1em;border:0;padding:0;">&nbsp;&nbsp;</DIV>' +
@@ -91,7 +91,7 @@ function genScrollingTestBody(){
 	}else if(!isQuirks && !options.large){
 		html.style.overflowY = scroll;
 	}
-	document.write('<BODY style="margin:0;padding:0;border:0;background-color:white;overflow-x:' + (options.horz ? (isQuirks ? scroll : '') : 'hidden') + ';overflow-y:' + (isQuirks ? (options.vert ? scroll : 'hidden') : '') + ';">');
+	document.write('<BODY style="position:relative;margin:0;padding:0;border:0;background-color:white;overflow-x:' + (options.horz ? (isQuirks ? scroll : '') : 'hidden') + ';overflow-y:' + (isQuirks ? (options.vert ? scroll : 'hidden') : '') + ';">');
 	genScrollingTestNodes(options.horz, options.vert, options.large);
 	document.write('</BODY>');
 }

ce5b2b5e960c3a50594d0641a2e300d5cdc48644
Kris Zyp <kriszyp@gmail.com>
2010-12-22 19:40:33 +0000

    Use delegate instead of mixin, a little lighter, refs #11537
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@23425 560b804f-0ae3-0310-86f3-f6aa0a117693

--- store/Cache.js
+++ store/Cache.js
@@ -6,43 +6,43 @@ dojo.store.Cache = function(masterStore, cachingStore, options){
 	// 		caches data from the master into the caching store for faster
 	//		lookup. Normally one would use a memory store for the caching
 	// 		store and a server store like JsonRest for the master store.
-	var store = dojo.mixin({}, masterStore);
 	options = options || {};
-	store.query = function(query, directives){
-		var results = masterStore.query(query, directives);
-		results.forEach(function(object){
-			if(!options.isLoaded || options.isLoaded(object)){
-				cachingStore.put(object);
-			}
-		});
-		return results;
-	};
-	store.get = function(id, directives){
-		return dojo.when(cachingStore.get(id), function(result){
-			return result || dojo.when(masterStore.get(id, directives), function(result){
-				if(result){
-					cachingStore.put(result, {id: id});
+	return dojo.delegate(masterStore, {
+		query: function(query, directives){
+			var results = masterStore.query(query, directives);
+			results.forEach(function(object){
+				if(!options.isLoaded || options.isLoaded(object)){
+					cachingStore.put(object);
 				}
-				return result;
 			});
-		});
-	};
-	store.add = function(object, directives){
-		cachingStore.add(object, directives);
-		return masterStore.add(object, directives);
-	};
-	store.put = function(object, directives){
-		cachingStore.put(object, directives);
-		return masterStore.put(object, directives);
-	};
-	store.remove = function(id, directives){
-		cachingStore.remove(id, directives);
-		return masterStore.remove(id, directives);
-	};
-	store.evict = function(id){
-		cachingStore.remove(id);
-	}
-	return store;
+			return results;
+		},
+		get: function(id, directives){
+			return dojo.when(cachingStore.get(id), function(result){
+				return result || dojo.when(masterStore.get(id, directives), function(result){
+					if(result){
+						cachingStore.put(result, {id: id});
+					}
+					return result;
+				});
+			});
+		},
+		add: function(object, directives){
+			cachingStore.add(object, directives);
+			return masterStore.add(object, directives);
+		},
+		put: function(object, directives){
+			cachingStore.put(object, directives);
+			return masterStore.put(object, directives);
+		},
+		remove: function(id, directives){
+			cachingStore.remove(id, directives);
+			return masterStore.remove(id, directives);
+		},
+		evict: function(id){
+			cachingStore.remove(id);
+		}
+	});
 };
 return dojo.store.Cache;
 });

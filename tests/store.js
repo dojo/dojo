91a4688ab00a3e4c722bcde5af91d989ae89aa54
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2011-01-23 00:44:32 +0000

    Adding missing store.js so that store tests (and everything after it in the requires of module.js) can run.  Fixes #12193
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@23614 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tests/store.js
@@ -0,0 +1,7 @@
+dojo.provide("dojo.tests.store");
+
+dojo.require("dojo.tests.store.Cache");
+dojo.require("dojo.tests.store.DataStore");
+dojo.require("dojo.tests.store.JsonRest");
+dojo.require("dojo.tests.store.Memory");
+dojo.require("dojo.tests.store.Observable");

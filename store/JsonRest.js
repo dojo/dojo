01ed4b43e05546a666a4ada42edaa23e0f53a0e4
Kris Zyp <kriszyp@gmail.com>
2011-02-10 05:09:49 +0000

    Switch to dojo.declare refs #11537 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@23761 560b804f-0ae3-0310-86f3-f6aa0a117693

--- store/JsonRest.js
+++ store/JsonRest.js
@@ -1,156 +1,47 @@
 define("dojo/store/JsonRest", ["dojo", "dojo/store/util/QueryResults"], function(dojo) {
 
-dojo.store.JsonRest = function(/*dojo.store.JsonRest*/ options){
-	// summary:
-	//		This is a basic store for RESTful communicating with a server through JSON
-	//		formatted data.
-	// options:
-	//		This provides any configuration information that will be mixed into the store
-	var store = {
-		// target: String
-		//		The target base URL to use for all requests to the server. This string will be
-		//		prepended to the id to generate the URL (relative or absolute) for requests
-		//		sent to the server
-		target: "",
-		// idProperty: String
-		//		Indicates the property to use as the identity property. The values of this
-		//		property should be unique.
-		idProperty: "id",
-		get: function(id, options){
-			// summary:
-			//		Retrieves an object by it's identity. This will trigger a GET request to the server
-			// id:
-			//		The identity to use to lookup the object
-
-			var headers = options || {};
-			headers.Accept = "application/javascript, application/json";
-			return dojo.xhrGet({
-				url:this.target + id,
-				handleAs: "json",
-				headers: headers
-			});
-		},
-		getIdentity: function(object){
-			// summary:
-			//		Returns an object's identity
-			// object:
-			//		The object to get the identity from
-			return object[this.idProperty];
-		},
-		put: function(object, options){
-			// summary:
-			//		Stores an object. This will trigger a PUT request to the server
-			//		if the object has an id, otherwise it will trigger a POST request
-			// object:
-			//		The object to store.
-			// options:
-			//		Additional metadata for storing the data
-			// options.id:
-			//		The identity to use for storing the data
-			options = options || {};
-			var id = ("id" in options) ? options.id : this.getIdentity(object);
-			var hasId = typeof id != "undefined";
-			return dojo.xhr(hasId && !options.incremental ? "PUT" : "POST", {
-					url: hasId ? this.target + id : this.target,
-					postData: dojo.toJson(object),
-					handleAs: "json",
-					headers:{
-						"Content-Type": "application/json",
-						"If-Match": options.overwrite === true ? "*" : null,
-						"If-None-Match": options.overwrite === false ? "*" : null
-					}
-				});
-		},
-		add: function(object, options){
-			// summary:
-			//		Adds an object. This will trigger a PUT request to the server
-			//		if the object has an id, otherwise it will trigger a POST request
-			// object:
-			//		The object to store.
-			// options:
-			//		Additional metadata for storing the data
-			// options.id:
-			//		The identity to use for storing the data
-			options = options || {};
-			options.overwrite = false;
-			return this.put(object, options);
-		},
-		remove: function(id){
-			// summary:
-			//		Deletes an object by it's identity. This will trigger a DELETE request to the server
-			// id:
-			//		The identity to use to delete the object
-			return dojo.xhrDelete({
-				url:this.target + id
-			});
-		},
-		query: function(query, options){
-			// summary:
-			//		Queries the store for objects. This will trigger a GET request to the server, with the query added as a query string
-			// query:
-			//		The query to use for retrieving objects from the store
-			var headers = {Accept: "application/javascript, application/json"};
-			options = options || {};
-
-			if(options.start >= 0 || options.count >= 0){
-				headers.Range = "items=" + (options.start || '0') + '-' +
-					(("count" in options && options.count != Infinity) ?
-						(options.count + (options.start || 0) - 1) : '');
-			}
-			if(dojo.isObject(query)){
-				query = dojo.objectToQuery(query);
-				query = query ? "?" + query: "";
-			}
-			if(options && options.sort){
-				query += (query ? "&" : "?") + "sort(";
-				for(var i = 0; i<options.sort.length; i++){
-					var sort = options.sort[i];
-					query += (i > 0 ? "," : "") + (sort.descending ? '-' : '+') + encodeURIComponent(sort.attribute);
-				}
-				query += ")";
-			}
-			var results = dojo.xhrGet({
-				url: this.target + (query || ""),
-				handleAs: "json",
-				headers: headers
-			});
-			results.total = results.then(function(){
-				var range = results.ioArgs.xhr.getResponseHeader("Content-Range");
-				return range && (range=range.match(/\/(.*)/)) && +range[1];
-			});
-			return dojo.store.util.QueryResults(results);
-		}
-	};
-	dojo.mixin(store, options);
-	return store;
-};
-/*=====
-dojo.declare("dojo.store.JsonRest", null, {
+return dojo.declare("dojo.store.JsonRest", null, {
+	constructor: function(/*dojo.store.JsonRest*/ options){
+		// summary:
+		//		This is a basic store for RESTful communicating with a server through JSON
+		//		formatted data.
+		// options:
+		//		This provides any configuration information that will be mixed into the store
+		dojo.mixin(this, options);
+	},
 	// target: String
-	//		The target base URL to use for all requests to the server. This string will be
-	//		prepended to the id to generate the URL (relative or absolute) for requests
-	//		sent to the server
+	//		The target base URL to use for all requests to the server. This string will be 
+	// 	prepended to the id to generate the URL (relative or absolute) for requests 
+	// 	sent to the server 
 	target: "",
 	// idProperty: String
 	//		Indicates the property to use as the identity property. The values of this
 	//		property should be unique.
 	idProperty: "id",
 
-	get: function(id){
-		// summary:
+	get: function(id, options){
+		//	summary:
 		//		Retrieves an object by its identity. This will trigger a GET request to the server using
 		//		the url `this.target + id`.
-		// id: Number
+		//	id: Number
 		//		The identity to use to lookup the object
-		// returns: Object
+		//	returns: Object
 		//		The object in the store that matches the given id.
+		var headers = options || {};
+		headers.Accept = "application/javascript, application/json";
+		return dojo.xhrGet({
+			url:this.target + id,
+			handleAs: "json",
+			headers: headers
+		});
 	},
 	getIdentity: function(object){
 		// summary:
 		//		Returns an object's identity
 		// object: Object
 		//		The object to get the identity from
-		// returns: Number
+		//	returns: Number
+		return object[this.idProperty];
 	},
 	put: function(object, options){
 		// summary:
@@ -161,7 +52,20 @@ dojo.declare("dojo.store.JsonRest", null, {
 		// options: Object?
 		//		Additional metadata for storing the data.  Includes an "id"
 		//		property if a specific id is to be used.
-		// returns: Number
+		//	returns: Number
+		options = options || {};
+		var id = ("id" in options) ? options.id : this.getIdentity(object);
+		var hasId = typeof id != "undefined";
+		return dojo.xhr(hasId && !options.incremental ? "PUT" : "POST", {
+				url: hasId ? this.target + id : this.target,
+				postData: dojo.toJson(object),
+				handleAs: "json",
+				headers:{
+					"Content-Type": "application/json",
+					"If-Match": options.overwrite === true ? "*" : null,
+					"If-None-Match": options.overwrite === false ? "*" : null
+				}
+			});
 	},
 	add: function(object, options){
 		// summary:
@@ -172,27 +76,59 @@ dojo.declare("dojo.store.JsonRest", null, {
 		// options: Object?
 		//		Additional metadata for storing the data.  Includes an "id"
 		//		property if a specific id is to be used.
-		// returns: Number
+		options = options || {};
+		options.overwrite = false;
+		return this.put(object, options);
 	},
 	remove: function(id){
 		// summary:
 		//		Deletes an object by its identity. This will trigger a DELETE request to the server.
 		// id: Number
 		//		The identity to use to delete the object
+		return dojo.xhrDelete({
+			url:this.target + id
+		});
 	},
 	query: function(query, options){
 		// summary:
-		//		Queries the store for objects. This will trigger a GET request to the server, with the
+		//		Queries the store for objects. This will trigger a GET request to the server, with the 
 		//		query added as a query string.
 		// query: Object
 		//		The query to use for retrieving objects from the store.
-		// options: dojo.store.util.SimpleQueryEngine.__queryOptions?
+		//	options: dojo.store.util.SimpleQueryEngine.__queryOptions?
 		//		The optional arguments to apply to the resultset.
-		// returns: dojo.store.util.QueryResults
+		//	returns: dojo.store.util.QueryResults
 		//		The results of the query, extended with iterative methods.
+		var headers = {Accept: "application/javascript, application/json"};
+		options = options || {};
+
+		if(options.start >= 0 || options.count >= 0){
+			headers.Range = "items=" + (options.start || '0') + '-' +
+				(("count" in options && options.count != Infinity) ?
+					(options.count + (options.start || 0) - 1) : '');
+		}
+		if(dojo.isObject(query)){
+			query = dojo.objectToQuery(query);
+			query = query ? "?" + query: "";
+		}
+		if(options && options.sort){
+			query += (query ? "&" : "?") + "sort(";
+			for(var i = 0; i<options.sort.length; i++){
+				var sort = options.sort[i];
+				query += (i > 0 ? "," : "") + (sort.descending ? '-' : '+') + encodeURIComponent(sort.attribute);
+			}
+			query += ")";
+		}
+		var results = dojo.xhrGet({
+			url: this.target + (query || ""),
+			handleAs: "json",
+			headers: headers
+		});
+		results.total = results.then(function(){
+			var range = results.ioArgs.xhr.getResponseHeader("Content-Range");
+			return range && (range=range.match(/\/(.*)/)) && +range[1];
+		});
+		return dojo.store.util.QueryResults(results);
 	}
 });
-=====*/
-
-return dojo.store.JsonRest;
 });
--- store/Memory.js
+++ store/Memory.js
@@ -1,80 +1,19 @@
 define("dojo/store/Memory", ["dojo", "dojo/store/util/QueryResults", "dojo/store/util/SimpleQueryEngine"], function(dojo) {
 
-dojo.store.Memory = function(/*dojo.store.Memory*/ options){
-	// summary:
-	//		This is a basic in-memory object store.
-	// options:
-	//		This provides any configuration information that will be mixed into the store.
-	//		This should generally include the data property to provide the starting set of data.
-	var store = {
-		data:[],
-		idProperty: "id",
-		index:{},
-		queryEngine: dojo.store.util.SimpleQueryEngine,
-		get: function(id){
-			return this.index[id];
-		},
-		getIdentity: function(object){
-			return object[this.idProperty];
-		},
-		put: function(object, options){
-			var id = options && options.id || object[this.idProperty] || Math.random();
-			this.index[id] = object;
-			var data = this.data,
-				idProperty = this.idProperty;
-			for(var i = 0, l = data.length; i < l; i++){
-				if(data[i][idProperty] == id){
-					data[i] = object;
-					return id;
-				}
-			}
-			this.data.push(object);
-			return id;
-		},
-		add: function(object, options){
-			if(this.index[options && options.id || object[this.idProperty]]){
-				throw new Error("Object already exists");
-			}
-			return this.put(object, options);
-		},
-		remove: function(id){
-			delete this.index[id];
-			var data = this.data,
-				idProperty = this.idProperty;
-			for(var i = 0, l = data.length; i < l; i++){
-				if(data[i][idProperty] == id){
-					data.splice(i, 1);
-					return;
-				}
-			}
-		},
-		query: function(query, options){
-			return dojo.store.util.QueryResults(this.queryEngine(query, options)(this.data));
-		},
-		setData: function(data){
-			if(data.items){
-				// just for convenience with the data format IFRS expects
-				this.idProperty = data.identifier;
-				data = this.data = data.items;
-			}else{
-				this.data = data;
-			}
-
-			for(var i = 0, l = data.length; i < l; i++){
-				var object = data[i];
-				this.index[object[this.idProperty]] = object;
-			}
-		}
-	};
-	dojo.mixin(store, options);
-	store.setData(store.data);
-	return store;
-};
-/*=====
-dojo.declare("dojo.store.Memory", null, {
+return dojo.declare("dojo.store.Memory", null, {
+	constructor: function(/*dojo.store.Memory*/ options){
+		// summary:
+		//		This is a basic in-memory object store.
+		// options:
+		//		This provides any configuration information that will be mixed into the store. 
+		// 		This should generally include the data property to provide the starting set of data.
+		this.index = {};
+		dojo.mixin(this, options);
+		this.setData(this.data || []);
+	},
 	// data: Array
 	//		The array of all the objects in the memory store
-	data:[],
+	data:null,
 
 	// idProperty: String
 	//		Indicates the property to use as the identity property. The values of this
@@ -83,68 +22,95 @@ dojo.declare("dojo.store.Memory", null, {
 
 	// index: Object
 	//		An index of data by id
-	index:{},
+	index:null,
 
 	// queryEngine: Function
 	//		Defines the query engine to use for querying the data store
 	queryEngine: dojo.store.util.SimpleQueryEngine,
 	get: function(id){
-		// summary:
+		//	summary:
 		//		Retrieves an object by its identity
-		// id: Number
+		//	id: Number
 		//		The identity to use to lookup the object
-		// returns: Object
+		//	returns: Object
 		//		The object in the store that matches the given id.
+		return this.index[id];
 	},
 	getIdentity: function(object){
-		// summary:
+		// 	summary:
 		//		Returns an object's identity
-		// object: Object
+		// 	object: Object
 		//		The object to get the identity from
-		// returns: Number
+		//	returns: Number
+		return object[this.idProperty];
 	},
 	put: function(object, options){
-		// summary:
+		// 	summary:
 		//		Stores an object
-		// object: Object
+		// 	object: Object
 		//		The object to store.
-		// options: Object?
+		// 	options: Object?
 		//		Additional metadata for storing the data.  Includes an "id"
 		//		property if a specific id is to be used.
-		// returns: Number
+		//	returns: Number
+		var id = options && options.id || object[this.idProperty] || Math.random();
+		this.index[id] = object;
+		var data = this.data,
+			idProperty = this.idProperty;
+		for(var i = 0, l = data.length; i < l; i++){
+			if(data[i][idProperty] == id){
+				data[i] = object;
+				return id;
+			}
+		}
+		this.data.push(object);
+		return id;
 	},
 	add: function(object, options){
-		// summary:
+		// 	summary:
 		//		Creates an object, throws an error if the object already exists
-		// object: Object
+		// 	object: Object
 		//		The object to store.
-		// options: Object?
+		// 	options: Object?
 		//		Additional metadata for storing the data.  Includes an "id"
 		//		property if a specific id is to be used.
-		// returns: Number
+		//	returns: Number
+		if(this.index[options && options.id || object[this.idProperty]]){
+			throw new Error("Object already exists");
+		}
+		return this.put(object, options);
 	},
 	remove: function(id){
-		// summary:
+		// 	summary:
 		//		Deletes an object by its identity
-		// id: Number
+		// 	id: Number
 		//		The identity to use to delete the object
+		delete this.index[id];
+		var data = this.data,
+			idProperty = this.idProperty;
+		for(var i = 0, l = data.length; i < l; i++){
+			if(data[i][idProperty] == id){
+				data.splice(i, 1);
+				return;
+			}
+		}
 	},
 	query: function(query, options){
-		// summary:
+		// 	summary:
 		//		Queries the store for objects.
-		// query: Object
+		// 	query: Object
 		//		The query to use for retrieving objects from the store.
-		// options: dojo.store.util.SimpleQueryEngine.__queryOptions?
+		//	options: dojo.store.util.SimpleQueryEngine.__queryOptions?
 		//		The optional arguments to apply to the resultset.
-		// returns: dojo.store.util.QueryResults
+		//	returns: dojo.store.util.QueryResults
 		//		The results of the query, extended with iterative methods.
 		//
-		// example:
-		//		Given the following store:
+		// 	example:
+		// 		Given the following store:
 		//
-		//	|	var store = new dojo.store.Memory({
-		//	|		data: [
-		//	|			{id: 1, name: "one", prime: false },
+		// 	|	var store = new dojo.store.Memory({
+		// 	|		data: [
+		// 	|			{id: 1, name: "one", prime: false },
 		//	|			{id: 2, name: "two", even: true, prime: true},
 		//	|			{id: 3, name: "three", prime: true},
 		//	|			{id: 4, name: "four", even: true, prime: false},
@@ -159,14 +125,25 @@ dojo.declare("dojo.store.Memory", null, {
 		//	...or find all items where "even" is true:
 		//
 		//	|	var results = store.query({ even: true });
+		return dojo.store.util.QueryResults(this.queryEngine(query, options)(this.data));
 	},
 	setData: function(data){
-		// summary:
+		// 	summary:
 		//		Sets the given data as the source for this store, and indexes it
-		// data: Object[]
+		//	data: Object[]
 		//		An array of objects to use as the source of data.
+		if(data.items){
+			// just for convenience with the data format IFRS expects
+			this.idProperty = data.identifier;
+			data = this.data = data.items;
+		}else{
+			this.data = data;
+		}
+
+		for(var i = 0, l = data.length; i < l; i++){
+			var object = data[i];
+			this.index[object[this.idProperty]] = object;
+		}
 	}
 });
-=====*/
-return dojo.store.Memory;
 });

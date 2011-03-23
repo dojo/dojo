c93dd31b0e38e736a7b8a92b059920c3deb9e4a1
Bill Keese <bill@dojotoolkit.org>
2011-03-23 06:14:27 +0000

    On IE9, generate faux keypress-event for ENTER key, since it doesn't generate one natively.  Fixes #12508 on 1.6/ branch, !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.6/dojo@24087 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/event.js
+++ _base/event.js
@@ -459,7 +459,8 @@ define("dojo/_base/event", ["dojo/lib/kernel", "dojo/_base/connect"], function(d
 				var k=evt.keyCode;
 				// These are Windows Virtual Key Codes
 				// http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/WinUI/WindowsUserInterface/UserInput/VirtualKeyCodes.asp
-				var unprintable = k!=13 && k!=32 && k!=27 && (k<48||k>90) && (k<96||k>111) && (k<186||k>192) && (k<219||k>222);
+				var unprintable = (k!=13 || (dojo.isIE >= 9 && !dojo.isQuirks)) && k!=32 && k!=27 && (k<48||k>90) && (k<96||k>111) && (k<186||k>192) && (k<219||k>222);
+
 				// synthesize keypress for most unprintables and CTRL-keys
 				if(unprintable||evt.ctrlKey){
 					var c = unprintable ? 0 : k;

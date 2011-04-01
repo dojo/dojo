5ee728f4fd7e84f410786cfd360120465528749c
Bill Keese <bill@dojotoolkit.org>
2011-04-01 09:14:41 +0000

    Refactor parser to allow attributes (for a single node) to be partly specified in data-dojo-props, and partly specified directly ex: value=123.   Uses node.attributes to detect which attributes are specified on a node, or for older versions of IE calls cloneNode(false) followed by some regex's on clone.outerHTML.
    Due to lowercase/uppercase issues (ex: tabIndex, onClick), and for type conversion, the code still introspects each widget to get it's attribute metadata.   In the future, would like to defer/avoid that in the common case.
      
    Fixes #10153, #12423, #12476, #10150, #9823,  refs #11490 !strict.
    
    Also fixes the problem on IE8 where a button without type=... defaults to type=submit rather than whatever the widget defines the default as, fixes #10163, refs #9334, #8946.
    
    Future updates will be attached to ticket #12476.
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@24185 560b804f-0ae3-0310-86f3-f6aa0a117693

--- parser.js
+++ parser.js
@@ -2,25 +2,21 @@ define("dojo/parser", ["dojo", "dojo/date/stamp"], function(dojo) {
 
 new Date("X"); // workaround for #11279, new Date("") == NaN
 
+var features = {
+	// Feature detection for when node.attributes only lists the attributes specified in the markup
+	// rather than old IE/quirks behavior where it lists every default value too
+	"dom-attributes-explicit": document.createElement("div").attributes.length < 40
+};
+function has(feature){
+	return features[feature];
+}
+
+
 dojo.parser = new function(){
 	// summary:
 	//		The Dom/Widget parsing package
 
 	var d = dojo;
-
-	function val2type(/*Object*/ value){
-		// summary:
-		//		Returns name of type of given value.
-
-		if(d.isString(value)){ return "string"; }
-		if(typeof value == "number"){ return "number"; }
-		if(typeof value == "boolean"){ return "boolean"; }
-		if(d.isFunction(value)){ return "function"; }
-		if(d.isArray(value)){ return "array"; } // typeof [] == "object"
-		if(value instanceof Date) { return "date"; } // assume timestamp
-		if(value instanceof d._Url){ return "url"; }
-		return "object";
-	}
 
 	function str2obj(/*String*/ value, /*String*/ type){
 		// summary:
@@ -67,17 +63,33 @@ dojo.parser = new function(){
 	}
 
 	var dummyClass = {}, instanceClasses = {
-		// map from fully qualified name (like "dijit.Button") to structure like
-		// { cls: dijit.Button, params: {label: "string", disabled: "boolean"} }
+		// Map from fully qualified name (like "dijit.Button") to structure like:
+		// {
+		//		cls: dijit.Button,
+		//		params: {
+		//			label: {name: "label", type: "string"},
+		//			disabled: {name: "disabled", type: "boolean"},
+		//			onclick: {name: "onClick", type: "function"}
+		//		}
+		// }
+		// Note that the params key is lowercased, onclick rather than onClick.
 	};
 
 	// Widgets like BorderContainer add properties to _Widget via dojo.extend().
 	// If BorderContainer is loaded after _Widget's parameter list has been cached,
 	// we need to refresh that parameter list (for _Widget and all widgets that extend _Widget).
-	// TODO: remove this in 2.0, when we stop caching parameters.
 	d.connect(d, "extend", function(){
 		instanceClasses = {};
 	});
+
+	// These are the data types where we can simply do "typeof value" to get the type of the value
+	var _nativeTypes = {"string": "string", "number": "number", "boolean": "boolean", "function": "function"};
+
+	// TODO: defer getProtoInfo() code until needed.
+	//   1. We can avoid using the lowerCase --> mixedCase
+	//		mapping for obvious parameters (lowercase values like "name", or commmon mixed case like onChange
+	//		onClick, tabIndex)
+	//	 2. Only need to compute types of parameters that the user specifies
 
 	function getProtoInfo(cls, params){
 		// cls: A prototype
@@ -87,42 +99,39 @@ dojo.parser = new function(){
 		for(var name in cls){
 			if(name.charAt(0)=="_"){ continue; }	// skip internal properties
 			if(name in dummyClass){ continue; }		// skip "constructor" and "toString"
-			params[name] = val2type(cls[name]);
+
+			var value = cls[name];
+			params[name.toLowerCase()] = {
+				name: name,
+				type:
+					_nativeTypes[typeof value] ||
+					((value && "length" in value) ? "array" :
+					(value instanceof Date) ? "date" :
+					(value instanceof d._Url) ? "url" :
+					"object")
+			};
 		}
 		return params;
 	}
 
-	function getClassInfo(/*String*/ className, /*Boolean*/ skipParamsLookup){
+	function getClassInfo(/*String*/ className){
 		// summary:
 		//		Maps a widget name string like "dijit.form.Button" to the widget constructor itself,
 		//		and a list of that widget's parameters and their types
 		// className:
 		//		fully qualified name (like "dijit.form.Button")
 		// returns:
-		//		structure like
-		//			{
-		//				cls: dijit.Button,
-		//				params: { label: "string", disabled: "boolean"}
-		//			}
+		//		structure like instanceClasses, see above
 
 		var c = instanceClasses[className];
 		if(!c){
-			// get pointer to widget class
-			var cls = d.getObject(className), params = null;
+			var cls = d.getObject(className);
 			if(!cls){ return null; }		// class not defined [yet]
-			if(!skipParamsLookup){ // from fastpath, we don't need to lookup the attrs on the proto because they are explicit
-				params = getProtoInfo(cls.prototype, {})
-			}
-			c = { cls: cls, params: params };
-			
-		}else if(!skipParamsLookup && !c.params){
-			// if we're calling getClassInfo and have a cls proto, but no params info, scan that cls for params now
-			// and update the pointer in instanceClasses[className]. This happens when a widget appears in another
-			// widget's template which still uses dojoType, but an instance of the widget appears prior with a data-dojo-type,
-			// skipping this lookup the first time.
-			c.params = getProtoInfo(c.cls.prototype, {});
+			var params = getProtoInfo(cls.prototype, {})
+			c = (instanceClasses[className] = { cls: cls, params: params });
+
 		}
-		
+
 		return c;
 	}
 
@@ -153,7 +162,7 @@ dojo.parser = new function(){
 		return new Function(preamble+script.innerHTML+suffix);
 	};
 
-	this.instantiate = function(/* Array */nodes, /* Object? */mixin, /* Object? */args){
+	this.instantiate = function(nodes, mixin, args){
 		// summary:
 		//		Takes array of nodes, and turns them into class instances and
 		//		potentially calls a startup method to allow them to connect with
@@ -178,32 +187,24 @@ dojo.parser = new function(){
 		mixin = mixin||{};
 		args = args||{};
 
+		// Precompute names of special attributes we are looking for
 		// TODO: for 2.0 default to data-dojo- regardless of scopeName (or maybe scopeName won't exist in 2.0)
-		var attrName = (args.scope || d._scopeName) + "Type",	// typically "dojoType"
-			attrData = "data-" + (args.scope || d._scopeName) + "-";	// typically "data-dojo-"
+		var dojoType = (args.scope || d._scopeName) + "Type",		// typically "dojoType"
+			dojotype = dojoType.toLowerCase(),						// typically "dojotype" (intentionally lowercase)
+			attrData = "data-" + (args.scope || d._scopeName) + "-",// typically "data-dojo-"
+			dataDojoType = attrData + "type",						// typically "data-dojo-type"
+			dataDojoProps = attrData + "props",						// typically "data-dojo-props"
+			dataDojoAttachPoint = attrData + "attach-point",
+			dataDojoAttachEvent = attrData + "attach-event",
+			dataDojoId = attrData + "id";
 
 		d.forEach(nodes, function(obj){
 			if(!obj){ return; }
 
-			// Get pointers to DOMNode, dojoType string, and clsInfo (metadata about the dojoType), etc.
-			var node, type, clsInfo, clazz, scripts, fastpath;
-			if(obj.node){
-				// new format of nodes[] array, object w/lots of properties pre-computed for me
-				node = obj.node;
-				type = obj.type;
-				fastpath = obj.fastpath;
-				clsInfo = obj.clsInfo || (type && getClassInfo(type, fastpath));
+			var node = obj.node || obj,
+				type = dojoType in mixin ? mixin[dojoType] : obj.node ? obj.type : (node.getAttribute(dataDojoType) || node.getAttribute(dojoType)),
+				clsInfo = obj.clsInfo || (type && getClassInfo(type)),
 				clazz = clsInfo && clsInfo.cls;
-				scripts = obj.scripts;
-			}else{
-				// old (backwards compatible) format of nodes[] array, simple array of DOMNodes. no fastpath/data-dojo-type support here.
-				node = obj;
-				type = attrName in mixin ? mixin[attrName] : node.getAttribute(attrName);
-				clsInfo = type && getClassInfo(type);
-				clazz = clsInfo && clsInfo.cls;
-				scripts = (clazz && (clazz._noScript || clazz.prototype._noScript) ? [] :
-							d.query("> script[type^='dojo/']", node));
-			}
 			if(!clsInfo){
 				throw new Error("Could not load class '" + type);
 			}
@@ -221,59 +222,103 @@ dojo.parser = new function(){
 				// settings from dir=rtl or lang=... on a node above this node
 				d._mixin(params, obj.inherited);
 			}
-			
-			// mix things found in data-dojo-props into the params
-			if(fastpath){
-				var extra = node.getAttribute(attrData + "props");
-				if(extra && extra.length){
-					try{
-						extra = d.fromJson.call(args.propsThis, "{" + extra + "}");
-						d._mixin(params, extra);
-					}catch(e){
-						// give the user a pointer to their invalid parameters. FIXME: can we kill this in production?
-						throw new Error(e.toString() + " in data-dojo-props='" + extra + "'");
-					}
-				}
 
-				// For the benefit of _Templated, check if node has data-dojo-attach-point/data-dojo-attach-event
-				// and mix those in as though they were parameters
-				var attachPoint = node.getAttribute(attrData + "attach-point");
-				if(attachPoint){
-					params.dojoAttachPoint = attachPoint;
-				}
-				var attachEvent = node.getAttribute(attrData + "attach-event");
-				if(attachEvent){
-					params.dojoAttachEvent = attachEvent;
-				}
-				dojo.mixin(params, mixin);
+			// Get list of attributes explicitly listed in the markup
+			var attributes;
+			if(has("dom-attributes-explicit")){
+				// Standard path to get list of user specified attributes
+				attributes = node.attributes;
 			}else{
-				// FIXME: we need something like "deprecateOnce()" to throw dojo.deprecation for something.
-				// remove this logic in 2.0
-				// read parameters (ie, attributes) specified on DOMNode
+				// Special path for IE, avoid (sometimes >100) bogus entries in node.attributes
+				var clone = /^input$|^img$/i.test(node.nodeName) ? node : node.cloneNode(false),
+					attrs = clone.outerHTML.replace(/=[^\s"']+|="[^"]*"|='[^']*'/g, "").replace(/^\s*<[a-zA-Z]*/, "").replace(/>.*$/, "");
 
-				var attributes = node.attributes;
+				attributes = dojo.map(attrs.split(/\s+/), function(name){
+					var lcName = name.toLowerCase();
+					return {
+						name: name,
+						// getAttribute() doesn't work for button.value, returns innerHTML of button.
+						// but getAttributeNode().value doesn't work for the form.encType or li.value
+						value: node.nodeName.match(/^BUTTON$|^TEXTAREA$/) && name == "value" ?
+								node.getAttributeNode(lcName).value : node.getAttribute(lcName),
+						specified: true
+					};
+				});
+			}
 
-				// clsInfo.params lists expected params like {"checked": "boolean", "n": "number"}
-				for(var name in clsInfo.params){
-					var item = name in mixin ? { value:mixin[name], specified:true } : attributes.getNamedItem(name);
-					if(!item || (!item.specified && (!dojo.isIE || name.toLowerCase()!="value"))){ continue; }
-					var value = item.value;
-					// Deal with IE quirks for 'class' and 'style'
-					switch(name){
-					case "class":
-						value = "className" in mixin ? mixin.className : node.className;
-						break;
-					case "style":
-						value = "style" in mixin ? mixin.style : (node.style && node.style.cssText); // FIXME: Opera?
-					}
-					var _type = clsInfo.params[name];
-					if(typeof value == "string"){
-						params[name] = str2obj(value, _type);
+			// Read in attributes and process them, including data-dojo-props, data-dojo-type,
+			// dojoAttachPoint, etc., as well as normal foo=bar attributes.
+			var i=0, item;
+			while(item = attributes[i++]){
+				if(!item || !item.specified){
+					continue;
+				}
+				var lcName = item.name.toLowerCase(),
+					paramMeta = clsInfo.params[lcName],
+					name =  paramMeta ? paramMeta.name : item.name,
+					value = item.value;
+
+				switch(lcName){
+
+				// Data-dojo-props.   Save for later to make sure it overrides direct foo=bar settings
+				case dataDojoProps:		// data-dojo-props
+					var extra = value;
+					break;
+
+				// The data-dojo-type=... setting itself, already processed
+				case dataDojoType:
+				case dojotype:
+					break;
+
+				// data-dojo-id or jsId. TODO: drop jsId in 2.0
+				case dataDojoId:
+				case "jsid":
+					var jsname = value;
+					break;
+
+				// For the benefit of _Templated
+				case dataDojoAttachPoint:
+					params.dojoAttachPoint = value;
+					break;
+				case dataDojoAttachEvent:
+					params.dojoAttachEvent = value;
+					break;
+
+				// Special parameter handling needed for IE
+				case "class":
+					params["class"] = node.className;
+					break;
+				case "style":
+					params["style"] = node.style && node.style.cssText;
+					break;
+
+				// Normal attribute, ex: value="123"
+				default:
+					if(typeof value == "string" && paramMeta){
+						params[name] = str2obj(value, paramMeta.type);
 					}else{
 						params[name] = value;
 					}
 				}
 			}
+
+			// Mix things found in data-dojo-props into the params, overriding any direct settings
+			if(extra){
+				try{
+					extra = d.fromJson.call(args.propsThis, "{" + extra + "}");
+					d._mixin(params, extra);
+				}catch(e){
+					// give the user a pointer to their invalid parameters. FIXME: can we kill this in production?
+					throw new Error(e.toString() + " in data-dojo-props='" + extra + "'");
+				}
+			}
+
+			// Any parameters specified in "mixin" override everything else.
+			d._mixin(params, mixin);
+
+			var scripts = obj.node ? obj.scripts : (clazz && (clazz._noScript || clazz.prototype._noScript) ? [] :
+						d.query("> script[type^='dojo/']", node));
+
 
 			// Process <script type="dojo/*"> script tags
 			// <script type="dojo/method" event="foo"> tags are added to params, and passed to
@@ -307,8 +352,6 @@ dojo.parser = new function(){
 			thelist.push(instance);
 
 			// map it to the JS namespace if that makes sense
-			// FIXME: in 2.0, drop jsId support. use data-dojo-id instead
-			var jsname = (node.getAttribute(attrData + "id") || node.getAttribute("jsId"));
 			if(jsname){
 				d.setObject(jsname, instance);
 			}
@@ -425,7 +468,7 @@ dojo.parser = new function(){
 		}
 		args = args || {};
 
-		var attrName = (args.scope || d._scopeName) + "Type",		// typically "dojoType"
+		var dojoType = (args.scope || d._scopeName) + "Type",		// typically "dojoType"
 			attrData = "data-" + (args.scope || d._scopeName) + "-",	// typically "data-dojo-"
 			dataDojoType = attrData + "type",						// typically "data-dojo-type"
 			dataDojoTextDir = attrData + "textdir";					// typically "data-dojo-textdir"
@@ -504,15 +547,8 @@ dojo.parser = new function(){
 				continue;
 			}
 
-			// Check for data-dojo-type attributes
-			var type, html5 = node.getAttribute(dataDojoType);
-			if(html5){
-				type = html5;
-			}else{
-				// fallback to backward compatible mode, using dojoType. remove in 2.0
-				type = node.getAttribute(attrName);
-			}
-			var fastpath = html5 == type;
+			// Check for data-dojo-type attribute, fallback to backward compatible dojoType
+			var type = node.getAttribute(dataDojoType) || node.getAttribute(dojoType);
 
 			// Short circuit for leaf nodes containing nothing [but text]
 			var firstChild = node.firstChild;
@@ -529,12 +565,11 @@ dojo.parser = new function(){
 			};
 
 			// If dojoType/data-dojo-type specified, add to output array of nodes to instantiate
-			var clsInfo = type && getClassInfo(type, fastpath), // note: won't find classes declared via dojo.Declaration
+			var clsInfo = type && getClassInfo(type), // note: won't find classes declared via dojo.Declaration
 				childScripts = clsInfo && !clsInfo.cls.prototype._noScript ? [] : null; // <script> nodes that are parent's children
 			if(type){
 				list.push({
 					"type": type,
-					fastpath: fastpath,
 					clsInfo: clsInfo,
 					node: node,
 					scripts: childScripts,

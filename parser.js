d8ea90e387827381ed75825536f96c2aa6612087
Bill Keese <bill@dojotoolkit.org>
2011-05-02 10:46:01 +0000

    Fix bug in IE attribute finding regex, on IE it was getting confused on <H3> thinking that "3" was an attribute, thus causing the error/hang in the !InlineEditBox test.   Refs #12476 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@24582 560b804f-0ae3-0310-86f3-f6aa0a117693

--- parser.js
+++ parser.js
@@ -152,7 +152,7 @@ dojo.parser = new function(){
 			}else{
 				// Special path for IE, avoid (sometimes >100) bogus entries in node.attributes
 				var clone = /^input$|^img$/i.test(node.nodeName) ? node : node.cloneNode(false),
-					attrs = clone.outerHTML.replace(/=[^\s"']+|="[^"]*"|='[^']*'/g, "").replace(/^\s*<[a-zA-Z]*/, "").replace(/>.*$/, "");
+					attrs = clone.outerHTML.replace(/=[^\s"']+|="[^"]*"|='[^']*'/g, "").replace(/^\s*<[a-zA-Z0-9]*/, "").replace(/>.*$/, "");
 
 				attributes = dojo.map(attrs.split(/\s+/), function(name){
 					var lcName = name.toLowerCase();

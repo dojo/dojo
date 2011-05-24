51a0534a380464192c9c977a4c7c5dc7182b4ea5
Bill Keese <bill@dojotoolkit.org>
2011-05-24 00:36:43 +0000

    fix path in dojo.provide(), plus async loading logic, refs #12380 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@25148 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/date/locale.js
+++ tests/date/locale.js
@@ -1,4 +1,4 @@
-dojo.provide("tests.date.locale");
+dojo.provide("dojo.tests.date.locale");
 
 dojo.require("dojo.date.locale");
 
@@ -12,22 +12,20 @@ tests.register("tests.date.locale",
 			name: "date.locale",
 			runTest: function(t){
 				var partLocaleList = ["en-us", "fr-fr", "es", "de-at", "ja-jp", "zh-cn"];
-        if(dojo.isAsync){
-            var
-              def = new doh.Deferred(),
-              deps = [];
-            dojo.forEach(partLocaleList, function(locale){
-              deps.push(dojo.getL10nName("dojo/cldr", "gregorian", locale));
-            });
-            define(deps, function(){
-							def.callback(true);
-            });
-            return def;
-        }else{ // tests for the v1.x loader/i18n machinery
-  				dojo.forEach(partLocaleList, function(locale){
-	  				dojo.requireLocalization("dojo.cldr", "gregorian", locale);
-		  		});
-        }
+				if(dojo.isAsync){
+					var def = new doh.Deferred(),
+						deps = dojo.map(partLocaleList, function(locale){
+							return dojo.getL10nName("dojo/cldr", "gregorian", locale)
+						});
+					require(deps, function(){
+						def.callback(true);
+					});
+					return def;
+				}else{ // tests for the v1.x loader/i18n machinery
+					dojo.forEach(partLocaleList, function(locale){
+						dojo.requireLocalization("dojo.cldr", "gregorian", locale);
+					});
+				}
 			},
 			tearDown: function(){
 				//Clean up bundles that should not exist if

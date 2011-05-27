31b9226d816ebed7e623a1d325950de1f1bd9dc0
Eugene Lazutkin <eugene@lazutkin.com>
2011-05-27 19:56:53 +0000

    declare: fixed a bug in the debug version of inherited(), thx Bill for noticing!, !strict, fixes #12888.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@25222 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/declare.js
+++ _base/declare.js
@@ -200,9 +200,9 @@ define(["./kernel", "../has", "./lang", "./array"], function(dojo, has){
 		return this.__inherited(name, true);
 	}
 
-	function inherited__debug(args, a){
-		var f = this.getInherited(args, a);
-		if(f){ return f.apply(this, a || args); }
+	function inherited__debug(args, a1, a2){
+		var f = this.getInherited(args, a1);
+		if(f){ return f.apply(this, a2 || a1 || args); }
 		// intentionally no return if a super method was not found
 	}
 

90d889f3850b256d67bf168f834a073769d86d28
Kris Zyp <kriszyp@gmail.com>
2011-05-29 03:40:11 +0000

    Make when() available on dojo/_base/Deferred export... (are all _base modules supposed to return dojo?), refs #12672 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@25244 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/Deferred.js
+++ _base/Deferred.js
@@ -306,7 +306,7 @@ define(["./kernel", "./lang"], function(dojo){
 		fired: -1
 	});
 
-	dojo.when = function(promiseOrValue, /*Function?*/callback, /*Function?*/errback, /*Function?*/progressHandler){
+	dojo.Deferred.when = dojo.when = function(promiseOrValue, /*Function?*/callback, /*Function?*/errback, /*Function?*/progressHandler){
 		// summary:
 		//		This provides normalization between normal synchronous values and
 		//		asynchronous promises, so you can interact with them in a common way

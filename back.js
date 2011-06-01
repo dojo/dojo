21ba7b3faf7d8257279e6de6bbdc47e41d970a93
Peter E Higgins <dante@dojotoolkit.org>
2011-06-01 23:46:21 +0000

    refs #13101 - expose dojo.back docs again.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@25325 560b804f-0ae3-0310-86f3-f6aa0a117693

--- back.js
+++ back.js
@@ -4,16 +4,14 @@ define(["./main", "require"], function(dojo, require) {
 	// summary:
 	//		TODOC
 
-dojo.getObject("back", true, dojo);
+	dojo.getObject("back", true, dojo);
 
 /*=====
 dojo.back = {
 	// summary: Browser history management resources
-}
+};
 =====*/
 
-
-(function(){
 	var back = dojo.back,
 
 	// everyone deals with encoding the hash slightly differently
@@ -103,11 +101,11 @@ dojo.back = {
 		//summary: private method. Do not call this directly.
 		var url = (dojo.config["dojoIframeHistoryUrl"] || require.toUrl("./resources/iframe_history.html")) + "?" + (new Date()).getTime();
 		moveForward = true;
-        if(historyIframe){
-		    dojo.isWebKit ? historyIframe.location = url : window.frames[historyIframe.name].location = url;
-        }else{
-            //console.warn("dojo.back: Not initialised. You need to call dojo.back.init() from a <script> block that lives inside the <body> tag.");
-        }
+		if(historyIframe){
+			dojo.isWebKit ? historyIframe.location = url : window.frames[historyIframe.name].location = url;
+		}else{
+			//console.warn("dojo.back: Not initialised. You need to call dojo.back.init() from a <script> block that lives inside the <body> tag.");
+		}
 		return url; //String
 	}
 
@@ -145,17 +143,17 @@ dojo.back = {
 
 	back.init = function(){
 		//summary: Initializes the undo stack. This must be called from a <script>
-		//         block that lives inside the <body> tag to prevent bugs on IE.
+		//		   block that lives inside the <body> tag to prevent bugs on IE.
 		// description:
-		// 		Only call this method before the page's DOM is finished loading. Otherwise
-		// 		it will not work. Be careful with xdomain loading or djConfig.debugAtAllCosts scenarios,
-		// 		in order for this method to work, dojo.back will need to be part of a build layer.
+		//		Only call this method before the page's DOM is finished loading. Otherwise
+		//		it will not work. Be careful with xdomain loading or djConfig.debugAtAllCosts scenarios,
+		//		in order for this method to work, dojo.back will need to be part of a build layer.
 		if(dojo.byId("dj_history")){ return; } // prevent reinit
 		var src = dojo.config["dojoIframeHistoryUrl"] || require.toUrl("./resources/iframe_history.html");
 		if (dojo._postLoad) {
 			console.error("dojo.back.init() must be called before the DOM has loaded. "
-			            + "If using xdomain loading or djConfig.debugAtAllCosts, include dojo.back "
-			            + "in a build layer.");
+						+ "If using xdomain loading or djConfig.debugAtAllCosts, include dojo.back "
+						+ "in a build layer.");
 		} else {
 			document.write('<iframe style="border:0;width:1px;height:1px;position:absolute;visibility:hidden;bottom:0;right:0;" name="dj_history" id="dj_history" src="' + src + '"></iframe>');
 		}
@@ -176,7 +174,7 @@ dojo.back = {
 	//FIXME: Make these doc comments not be awful. At least they're not wrong.
 	//FIXME: Would like to support arbitrary back/forward jumps. Have to rework iframeLoaded among other things.
 	//FIXME: is there a slight race condition in moz using change URL with the timer check and when
-	//       the hash gets set? I think I have seen a back/forward call in quick succession, but not consistent.
+	//		 the hash gets set? I think I have seen a back/forward call in quick succession, but not consistent.
 
 
 	/*=====
@@ -222,7 +220,7 @@ dojo.back = {
 		//		for example). If you want to detect hash changes using semantic fragment IDs, then
 		//		consider using dojo.hash instead (in Dojo 1.4+).
 		//
-	 	//	example:
+		//	example:
 		//		|	dojo.back.addToHistory({
 		//		|		back: function(){ console.log('back pressed'); },
 		//		|		forward: function(){ console.log('forward pressed'); },
@@ -230,7 +228,7 @@ dojo.back = {
 		//		|	});
 
 		//	BROWSER NOTES:
-		//  Safari 1.2:
+		//	Safari 1.2:
 		//	back button "works" fine, however it's not possible to actually
 		//	DETECT that you've moved backwards by inspecting window.location.
 		//	Unless there is some other means of locating.
@@ -388,7 +386,7 @@ dojo.back = {
 			handleForwardButton();
 		}
 	};
- })();
 
-return dojo.back;
+	return dojo.back;
+	
 });

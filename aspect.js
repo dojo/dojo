e3da8a0ce6c1e87fe61f13fdffa4dd6569e3b007
Peter E Higgins <dante@dojotoolkit.org>
2011-06-01 23:26:01 +0000

    refs #13101 - make aspect.js exposed as dojo.aspect \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@25324 560b804f-0ae3-0310-86f3-f6aa0a117693

--- aspect.js
+++ aspect.js
@@ -1,63 +1,94 @@
 define([], function(){
-// summary:
-//		dojo/aspect provides aspect oriented programming functionality, allowing for
-//		one to add before, around, or after advice on existing methods.
-//	description:
-//		For example:
-//		|	define(["dojo/aspect"], function(aspect){
-//		|		var signal = aspect.after(targetObject, "methodName", function(someArgument){
-// 		|			this will be called when targetObject.methodName() is called, after the original function is called
-//		|		});
-//		The returned signal object can be used to cancel the advice.
-//		|	signal.remove(); // this will stop the advice from being executed anymore
-//		|	aspect.before(targetObject, "methodName", function(someArgument){
-//		|		// this will be called when targetObject.methodName() is called, before the original function is called
-//		|	 });
-//	after(target, methodName, advice, receiveArguments):
-//		The "after" export of the aspect module is a function that can be used to attach
-//		"after" advice to a method. This function will be executed after the original method
-//		is executed. By default the function will be called with a single argument, the return
-//		value of the original method, or the the return value of the last executed advice (if a previous one exists).
-//		The fourth (optional) argument can be set to true to so the function receives the original
-// 		arguments (from when the original method was called) rather than the return value.
-//		If there are multiple "after" advisors, they are executed in the order they were registered.
-//	before(target, methodName, advice);
-//		The "before" export of the aspect module is a function that can be used to attach
-//		"before" advice to a method. This function will be executed before the original method
-//		is executed. This function will be called with the arguments used to call the method.
-//		This function may optionally return an array as the new arguments to use to call
-//		the original method (or the previous, next-to-execute before advice, if one exists).
-//		If the before method doesn't return anything (returns undefined) the original arguments
-//		will be preserved.
-//		If there are multiple "before" advisors, they are executed in the reverse order they were registered.
-//	around(target, methodName, advisor);
-//		The "around" export of the aspect module is a function that can be used to attach
-//		"around" advice to a method. The advisor function is immediately executed when
-// 		the around() is called, is passed a single argument that is a function that can be
-// 		called to continue execution of the original method (or the next around advisor).
-// 		The advisor function should return a function, and this function will be called whenever
-// 		the method is called. It will be called with the arguments used to call the method.
-//		Whatever this function returns will be returned as the result of the method call (unless after advise changes it).
-//		If there are multiple "around" advisors, the most recent one is executed first,
-//		which can then delegate to the next one and so on. For example:
-//		|	around(obj, "foo", function(originalFoo){
-//		|		return function(){
-//		|			var start = new Date().getTime();
-// 		|			var results = originalFoo.apply(this, arguments); // call the original
-// 		|			var end = new Date().getTime();
-// 		|			console.log("foo execution took " + (end - start) + " ms");
-//		|			return results;
-//		|		};
-//  	|	});
-//	All the advisor functions take these arguments:
-//	target:
-//		This is the target object
-//	methodName:
-//		This is the name of the method to attach to.
-//	advice:
-//		This is function to be called before, after, or around the original method
-//
- 	"use strict";
+
+// TODOC: after/before/around return object
+// TODOC: after/before/around param types. 
+
+/*=====
+	dojo.aspect = {
+		// summary: provides aspect oriented programming functionality, allowing for
+		//		one to add before, around, or after advice on existing methods.
+		//
+		// example:
+		//	|	define(["dojo/aspect"], function(aspect){
+		//	|		var signal = aspect.after(targetObject, "methodName", function(someArgument){
+		//	|			this will be called when targetObject.methodName() is called, after the original function is called
+		//	|		});
+		//
+		// example:
+		//	The returned signal object can be used to cancel the advice.
+		//	|	signal.remove(); // this will stop the advice from being executed anymore
+		//	|	aspect.before(targetObject, "methodName", function(someArgument){
+		//	|		// this will be called when targetObject.methodName() is called, before the original function is called
+		//	|	 });
+		
+		after: function(target, methodName, advice, receiveArguments){
+			// summary: The "after" export of the aspect module is a function that can be used to attach
+			//		"after" advice to a method. This function will be executed after the original method
+			//		is executed. By default the function will be called with a single argument, the return
+			//		value of the original method, or the the return value of the last executed advice (if a previous one exists).
+			//		The fourth (optional) argument can be set to true to so the function receives the original
+			//		arguments (from when the original method was called) rather than the return value.
+			//		If there are multiple "after" advisors, they are executed in the order they were registered.
+			// target:
+			//		This is the target object
+			// methodName:
+			//		This is the name of the method to attach to.
+			// advice:
+			//		This is function to be called before, after, or around the original method
+		},
+		
+		before: function(target, methodName, advice){
+			// summary: The "before" export of the aspect module is a function that can be used to attach
+			//		"before" advice to a method. This function will be executed before the original method
+			//		is executed. This function will be called with the arguments used to call the method.
+			//		This function may optionally return an array as the new arguments to use to call
+			//		the original method (or the previous, next-to-execute before advice, if one exists).
+			//		If the before method doesn't return anything (returns undefined) the original arguments
+			//		will be preserved.
+			//		If there are multiple "before" advisors, they are executed in the reverse order they were registered.
+			//
+			// target:
+			//		This is the target object
+			// methodName:
+			//		This is the name of the method to attach to.
+			// advice:
+			//		This is function to be called before, after, or around the original method	 
+		},
+
+		around: function(target, methodName, advice){
+			// summary: The "around" export of the aspect module is a function that can be used to attach
+			//		"around" advice to a method. The advisor function is immediately executed when
+			//		the around() is called, is passed a single argument that is a function that can be
+			//		called to continue execution of the original method (or the next around advisor).
+			//		The advisor function should return a function, and this function will be called whenever
+			//		the method is called. It will be called with the arguments used to call the method.
+			//		Whatever this function returns will be returned as the result of the method call (unless after advise changes it).
+			//
+			// example:
+			//		If there are multiple "around" advisors, the most recent one is executed first,
+			//		which can then delegate to the next one and so on. For example:
+			//		|	around(obj, "foo", function(originalFoo){
+			//		|		return function(){
+			//		|			var start = new Date().getTime();
+			//		|			var results = originalFoo.apply(this, arguments); // call the original
+			//		|			var end = new Date().getTime();
+			//		|			console.log("foo execution took " + (end - start) + " ms");
+			//		|			return results;
+			//		|		};
+			//		|	});
+			//
+			// target:
+			//		This is the target object
+			// methodName:
+			//		This is the name of the method to attach to.
+			// advice:
+			//		This is function to be called before, after, or around the original method
+		}
+
+	};
+=====*/
+
+	"use strict";
 	function advise(dispatcher, type, advice, receiveArguments){
 		var previous = dispatcher[type];
 		var around = type == "around";

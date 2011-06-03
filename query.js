06ecb05a6259ce10b0b758b5667600d69c6285be
Peter E Higgins <dante@dojotoolkit.org>
2011-06-03 16:05:20 +0000

    refs #13101 - makes dojo.query docs show up in api tool \!strict ironically
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@25398 560b804f-0ae3-0310-86f3-f6aa0a117693

--- query.js
+++ query.js
@@ -1,55 +1,76 @@
 define(["./_base/kernel", "./_base/NodeList", "./has","./selector/_loader", "./selector/_loader!default"], 
 		function(dojo, NodeList, has, loader, defaultEngine){
-// summary:
-// 		This modules provides DOM querying functionality. The module export is a function
-// 		that can be used to query for DOM nodes by CSS selector and returns a dojo.NodeList
-// 		representing the matching nodes.
-//	example:
-//		add an onclick handler to every submit button in the document
-//		which causes the form to be sent via Ajax instead:
-//	|	define("dojo/query", function(query){
-// 	|	query("input[type='submit']").on("click", function(e){
-//	|		dojo.stopEvent(e); // prevent sending the form
-//	|		var btn = e.target;
-//	|		dojo.xhrPost({
-//	|			form: btn.form,
-//	|			load: function(data){
-//	|				// replace the form with the response
-//	|				var div = dojo.doc.createElement("div");
-//	|				dojo.place(div, btn.form, "after");
-//	|				div.innerHTML = data;
-//	|				dojo.style(btn.form, "display", "none");
-//	|			}
-//	|		});
-//	|	}); 
-// 	description:
-// 		dojo/query is responsible for loading the appropriate query engine and wrapping 
-// 		its results with a NodeList. You can use dojo/query with a specific selector engine
-//		by using it as a plugin. For example, if you installed the sizzle package, you could
-// 		use it as the selector engine with:
-//		|	define("dojo/query!sizzle", function(query){
-//		|		query("div")...
-//		The id after the ! can be a module id of the selector engine or one of the following values:
-//		* acme: This is the default engine used by Dojo base, and will ensure that the full
-// 		Acme engine is always loaded. 
-//		* css2: If the browser has a native selector engine, this will be used, otherwise a
-// 		very minimal lightweight selector engine will be loaded that can do simple CSS2 selectors
-// 		(by #id, .class, tag, and [name=value] attributes, with standard child or descendant (>)
-// 		operators) and nothing more.
-//		* css2.1: If the browser has a native selector engine, this will be used, otherwise the
-// 		full Acme engine will be loaded. 
-//		* css3: If the browser has a native selector engine with support for CSS3 pseudo
-// 		selectors (most modern browsers except IE8), this will be used, otherwise the
-// 		full Acme engine will be loaded.
-//		* Or the module id of a selector engine can be used to explicitly choose the selector engine
-//		For example, if you are using CSS3 pseudo selectors in module, you can specify that
-// 		you will need support them with
-// 		|	define("dojo/query!css3", function(query){
-// 		|		query('#t > h3:nth-child(odd)')...
-//		You can also choose the selector engine/load configuration by setting the <FIXME:what is the configuration setting?>.
-//		For example:
-//		<script data-dojo-config="query-selector:'css3'" src="dojo.js"></script>
-//		
+/*===== 
+dojo.query = function(selector, context){
+	// summary:
+	//		This modules provides DOM querying functionality. The module export is a function
+	//		that can be used to query for DOM nodes by CSS selector and returns a dojo.NodeList
+	//		representing the matching nodes.
+	//
+	// selector: String
+	//		A CSS selector to search for.
+	// context: String|DomNode?
+	//		An optional context to limit the searching scope. Only nodes under `context` will be 
+	//		scanned. 
+	// 
+	//	example:
+	//		add an onclick handler to every submit button in the document
+	//		which causes the form to be sent via Ajax instead:
+	//	|	define(["dojo/query"], function(query){
+	// 	|	query("input[type='submit']").on("click", function(e){
+	//	|		dojo.stopEvent(e); // prevent sending the form
+	//	|		var btn = e.target;
+	//	|		dojo.xhrPost({
+	//	|			form: btn.form,
+	//	|			load: function(data){
+	//	|				// replace the form with the response
+	//	|				var div = dojo.doc.createElement("div");
+	//	|				dojo.place(div, btn.form, "after");
+	//	|				div.innerHTML = data;
+	//	|				dojo.style(btn.form, "display", "none");
+	//	|			}
+	//	|		});
+	//	|	}); 
+	//
+	// description:
+	//		dojo/query is responsible for loading the appropriate query engine and wrapping 
+	//		its results with a `dojo.NodeList`. You can use dojo/query with a specific selector engine
+	//		by using it as a plugin. For example, if you installed the sizzle package, you could
+	//		use it as the selector engine with:
+	//		|	define("dojo/query!sizzle", function(query){
+	//		|		query("div")...
+	//
+	//		The id after the ! can be a module id of the selector engine or one of the following values:
+	//		|	+ acme: This is the default engine used by Dojo base, and will ensure that the full
+	//		|	Acme engine is always loaded. 
+	//		|	
+	//		|	+ css2: If the browser has a native selector engine, this will be used, otherwise a
+	//		|	very minimal lightweight selector engine will be loaded that can do simple CSS2 selectors
+	//		|	(by #id, .class, tag, and [name=value] attributes, with standard child or descendant (>)
+	//		|	operators) and nothing more.
+	//		|
+	//		|	+ css2.1: If the browser has a native selector engine, this will be used, otherwise the
+	//		|	full Acme engine will be loaded. 
+	//		|	
+	//		|	+ css3: If the browser has a native selector engine with support for CSS3 pseudo
+	//		|	selectors (most modern browsers except IE8), this will be used, otherwise the
+	//		|	full Acme engine will be loaded.
+	//		|	
+	//		|	+ Or the module id of a selector engine can be used to explicitly choose the selector engine
+	//		
+	//		For example, if you are using CSS3 pseudo selectors in module, you can specify that
+	//		you will need support them with:
+	//		|	define("dojo/query!css3", function(query){
+	//		|		query('#t > h3:nth-child(odd)')...
+	//
+	//		You can also choose the selector engine/load configuration by setting the <FIXME:what is the configuration setting?>.
+	//		For example:
+	//		|	<script data-dojo-config="query-selector:'css3'" src="dojo.js"></script>
+	//		
+	return new dojo.NodeList(); // dojo.NodeList
+};
+=====*/
+
 "use strict";
 function queryForEngine(engine){
 	var query = function(/*String*/ query, /*String|DOMNode?*/ root){
@@ -101,12 +122,18 @@ function queryForEngine(engine){
 }
 var query = dojo.query = queryForEngine(defaultEngine);
 
-query.load = function(id, parentRequire, loaded, config){
-	// can be used a AMD plugin to conditionally load new query engine
+query.load = /*===== dojo.query.load= ======*/ function(id, parentRequire, loaded, config){
+	// summary: can be used as AMD plugin to conditionally load new query engine
+	// example:
+	//	|	define(["dojo/query!custom"], function(qsa){ 
+	//	|		// loaded selector/custom.js as engine
+	//	|		qsa("#foobar").forEach(...);
+	//	|	});
 	loader.load(id, parentRequire, function(engine){
 		loaded(queryForEngine(engine));
 	});
 };
+
 dojo._filterQueryResult = function(nodes, selector, root){
 	return NodeList._wrap(query.filter(nodes, selector, root));
 };

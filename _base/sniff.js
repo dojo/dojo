bff2ac504739a27a35d73eacb8fa3d1915404e85
Kris Zyp <kriszyp@gmail.com>
2011-06-24 20:34:52 +0000

    Remove erroneous spaces in feature names, refs #12431 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@25602 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/sniff.js
+++ _base/sniff.js
@@ -173,9 +173,9 @@ define(["./kernel", "../has"], function(dojo, has){
 	has.add("khtml", dojo.isKhtml= isKhtml);
 	has.add("webKit", dojo.isWebKit= isWebKit);
 	has.add("chrome", dojo.isChrome= isChrome);
-	has.add("mac ", dojo.isMac = isMac );
+	has.add("mac", dojo.isMac = isMac );
 	has.add("safari", dojo.isSafari= isSafari);
-	has.add("mozilla ", dojo.isMozilla = dojo.isMoz = isMozilla );
+	has.add("mozilla", dojo.isMozilla = dojo.isMoz = isMozilla );
 	has.add("ie", dojo.isIE = isIE );
 	has.add("ff", dojo.isFF= isFF);
 	has.add("quirks", dojo.isQuirks= isQuirks);

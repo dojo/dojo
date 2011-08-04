aca740e265cd9fd8bea4786138ec2548e7daa675
Chris Mitchell <ccmitchellusa@gmail.com>
2011-08-04 05:28:31 +0000

    fixes #13574 api doc update for NodeList-html.  Thanks Stephen
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@26013 560b804f-0ae3-0310-86f3-f6aa0a117693

--- NodeList-html.js
+++ NodeList-html.js
@@ -15,11 +15,17 @@ var lang = dojo, NodeList = dojo.NodeList;
 =====*/
 
 lang.extend(NodeList, {
-	html: function(content, /* Object? */params){
+	html: function(/* String|DomNode|NodeList? */ content, /* Object? */params){
 		//	summary:
 		//		see `dojo.html.set()`. Set the content of all elements of this NodeList
 		//
-		// description:
+		//	content:
+		//		An html string, node or enumerable list of nodes for insertion into the dom
+		//
+		//	params:
+		//		Optional flags/properties to configure the content-setting. See dojo.html._ContentSetter
+		//
+		//	description:
 		//		Based around `dojo.html.set()`, set the content of the Elements in a
 		//		NodeList to the given content (string/node/nodelist), with optional arguments
 		//		to further tune the set content behavior.

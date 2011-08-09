3416c7a5485f7792ca7860398f25f2f02b0e0bcd
Ben Hockey <neonstalwart@gmail.com>
2011-08-09 20:39:59 +0000

    refer to lang rather than dojo in dojo/i18n.js.  refs #13484 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@26131 560b804f-0ae3-0310-86f3-f6aa0a117693

--- i18n.js
+++ i18n.js
@@ -70,16 +70,16 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/lang", "
 			// get the root bundle which instructs which other bundles are required to contruct the localized bundle
 			require([bundlePathAndName], function(root){
 				var
-					current= cache[bundlePathAndName + "/"]= dojo.clone(root.root),
+					current= cache[bundlePathAndName + "/"]= lang.clone(root.root),
 					availableLocales= getAvailableLocales(!root._v1x && root, locale, bundlePath, bundleName);
 				require(availableLocales, function(){
 					for (var i= 1; i<availableLocales.length; i++){
-						cache[availableLocales[i]]= current= lang.mixin(dojo.clone(current), arguments[i]);
+						cache[availableLocales[i]]= current= lang.mixin(lang.clone(current), arguments[i]);
 					}
 					// target may not have been resolve (e.g., maybe only "fr" exists when "fr-ca" was requested)
 					var target= bundlePathAndName + "/" + locale;
 					cache[target]= current;
-					load && load(dojo.delegate(current));
+					load && load(lang.delegate(current));
 				});
 			});
 		},

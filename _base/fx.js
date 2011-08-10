77b0a7144d53dca9e0f1b55c151afa0c20cd1297
Bill Keese <bill@dojotoolkit.org>
2011-08-10 11:50:30 +0000

    fix dojo/_base/fx to be baseless, patch from Stephen Zhang (thanks!), fixes #13645 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@26146 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/fx.js
+++ _base/fx.js
@@ -1,4 +1,4 @@
-define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sniff"], function(dojo, lang){
+define(["./kernel", "./lang", "./Color", "./connect", "./sniff", "../dom", "../dom-style"], function(dojo, lang, Color, connect, has, dom, style){
 	// module:
 	//		dojo/_base/fx
 	// summary:
@@ -43,7 +43,7 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 		//		animation instance.
 
 		_mixin(this, args);
-		if(dojo.isArray(this.curve)){
+		if(lang.isArray(this.curve)){
 			this.curve = new dojo._Line(this.curve[0], this.curve[1]);
 		}
 
@@ -52,7 +52,7 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 	// Alias to drop come 2.0:
 	dojo._Animation = dojo.Animation;
 
-	dojo.extend(dojo.Animation, {
+	lang.extend(dojo.Animation, {
 		// duration: Integer
 		//		The time in milliseonds the animation will take to run
 		duration: 350,
@@ -181,7 +181,7 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 			_t._fire("beforeBegin", [_t.node]);
 
 			var de = delay || _t.delay,
-				_p = dojo.hitch(_t, "_play", gotoStart);
+				_p = lang.hitch(_t, "_play", gotoStart);
 
 			if(de > 0){
 				_t._delayTimer = setTimeout(_p, de);
@@ -323,21 +323,21 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 			run: function(){}
 		};
 
-	dojo.extend(dojo.Animation, {
+	lang.extend(dojo.Animation, {
 
 		_startTimer: function(){
 			if(!this._timer){
-				this._timer = dojo.connect(runner, "run", this, "_cycle");
+				this._timer = connect.connect(runner, "run", this, "_cycle");
 				ctr++;
 			}
 			if(!timer){
-				timer = setInterval(dojo.hitch(runner, "run"), this.rate);
+				timer = setInterval(lang.hitch(runner, "run"), this.rate);
 			}
 		},
 
 		_stopTimer: function(){
 			if(this._timer){
-				dojo.disconnect(this._timer);
+				connect.disconnect(this._timer);
 				this._timer = null;
 				ctr--;
 			}
@@ -352,13 +352,13 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 
 	var _makeFadeable =
 		//>>excludeStart("webkitMobile", kwArgs.webkitMobile);
-		dojo.isIE ? function(node){
+		has("ie") ? function(node){
 			// only set the zoom if the "tickle" value would be the same as the
 			// default
 			var ns = node.style;
 			// don't set the width to auto if it didn't already cascade that way.
 			// We don't want to f anyones designs
-			if(!ns.width.length && dojo.style(node, "width") == "auto"){
+			if(!ns.width.length && style.get(node, "width") == "auto"){
 				ns.width = "auto";
 			}
 		} :
@@ -371,18 +371,18 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 		//		args.node from the start to end values passed (args.start
 		//		args.end) (end is mandatory, start is optional)
 
-		args.node = dojo.byId(args.node);
+		args.node = dom.byId(args.node);
 		var fArgs = _mixin({ properties: {} }, args),
 			props = (fArgs.properties.opacity = {});
 
 		props.start = !("start" in fArgs) ?
 			function(){
-				return +dojo.style(fArgs.node, "opacity")||0;
+				return +style.get(fArgs.node, "opacity")||0;
 			} : fArgs.start;
 		props.end = fArgs.end;
 
 		var anim = dojo.animateProperty(fArgs);
-		dojo.connect(anim, "beforeBegin", dojo.partial(_makeFadeable, fArgs.node));
+		connect.connect(anim, "beforeBegin", lang.partial(_makeFadeable, fArgs.node));
 
 		return anim; // dojo.Animation
 	};
@@ -428,9 +428,9 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 		this._properties = properties;
 		for(var p in properties){
 			var prop = properties[p];
-			if(prop.start instanceof dojo.Color){
+			if(prop.start instanceof Color){
 				// create a reusable temp color object to keep intermediate results
-				prop.tempColor = new dojo.Color();
+				prop.tempColor = new Color();
 			}
 		}
 	};
@@ -440,9 +440,9 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 		for(var p in this._properties){
 			var prop = this._properties[p],
 				start = prop.start;
-			if(start instanceof dojo.Color){
-				ret[p] = dojo.blendColors(start, prop.end, r, prop.tempColor).toCss();
-			}else if(!dojo.isArray(start)){
+			if(start instanceof Color){
+				ret[p] = Color.blendColors(start, prop.end, r, prop.tempColor).toCss();
+			}else if(!lang.isArray(start)){
 				ret[p] = ((prop.end - start) * r) + start + (p != "opacity" ? prop.units || "px" : 0);
 			}
 		}
@@ -549,11 +549,11 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 		//	|	}).play();
 		//
 
-		var n = args.node = dojo.byId(args.node);
+		var n = args.node = dom.byId(args.node);
 		if(!args.easing){ args.easing = dojo._defaultEasing; }
 
 		var anim = new dojo.Animation(args);
-		dojo.connect(anim, "beforeBegin", anim, function(){
+		connect.connect(anim, "beforeBegin", anim, function(){
 			var pm = {};
 			for(var p in this.properties){
 				// Make shallow copy of properties into pm because we overwrite
@@ -564,15 +564,15 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 					this.node.display = "block";
 				}
 				var prop = this.properties[p];
-				if(dojo.isFunction(prop)){
+				if(lang.isFunction(prop)){
 					prop = prop(n);
 				}
-				prop = pm[p] = _mixin({}, (dojo.isObject(prop) ? prop: { end: prop }));
+				prop = pm[p] = _mixin({}, (lang.isObject(prop) ? prop: { end: prop }));
 
-				if(dojo.isFunction(prop.start)){
+				if(lang.isFunction(prop.start)){
 					prop.start = prop.start(n);
 				}
-				if(dojo.isFunction(prop.end)){
+				if(lang.isFunction(prop.end)){
 					prop.end = prop.end(n);
 				}
 				var isColor = (p.toLowerCase().indexOf("color") >= 0);
@@ -580,7 +580,7 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 					// dojo.style(node, "height") can return "auto" or "" on IE; this is more reliable:
 					var v = { height: node.offsetHeight, width: node.offsetWidth }[p];
 					if(v !== undefined){ return v; }
-					v = dojo.style(node, p);
+					v = style.get(node, p);
 					return (p == "opacity") ? +v : (isColor ? v : parseFloat(v));
 				}
 				if(!("end" in prop)){
@@ -590,15 +590,15 @@ define(["./kernel", "./lang", "./Color", "./connect", "./lang", "./html", "./sni
 				}
 
 				if(isColor){
-					prop.start = new dojo.Color(prop.start);
-					prop.end = new dojo.Color(prop.end);
+					prop.start = new Color(prop.start);
+					prop.end = new Color(prop.end);
 				}else{
 					prop.start = (p == "opacity") ? +prop.start : parseFloat(prop.start);
 				}
 			}
 			this.curve = new PropLine(pm);
 		});
-		dojo.connect(anim, "onAnimate", dojo.hitch(dojo, "style", anim.node));
+		connect.connect(anim, "onAnimate", lang.hitch(dojo, "style", anim.node));
 		return anim; // dojo.Animation
 	};
 

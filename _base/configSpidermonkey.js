c58403fe1ad3abcfe8e8a58877fb6d37b7df988d
Bill Keese <bill@dojotoolkit.org>
2011-08-14 06:49:09 +0000

    avoid warning about unused variables, refs #13036 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@26199 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/configSpidermonkey.js
+++ _base/configSpidermonkey.js
@@ -68,7 +68,8 @@ dojo._loadUri = function(uri){
 	// is what we want).
 	// TODO: sigh, load() does not return a useful value.
 	// Perhaps it is returning the value of the last thing evaluated?
-	var ok = load(uri);
+	// var ok =
+	load(uri);
 	// console.log("spidermonkey load(", uri, ") returned ", ok);
 	return 1;
 };

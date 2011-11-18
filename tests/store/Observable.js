60eb46de1260e977776e0c1b8e6dde890734a31c
Kris Zyp <kriszyp@gmail.com>
2011-11-18 23:29:40 +0000

    Adjusted unit tests for reverting connecting separate pages, refs #14268 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@27058 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/store/Observable.js
+++ tests/store/Observable.js
@@ -114,13 +114,11 @@ dojo.require("dojo.store.Observable");
 				    bigStore.query({}, dojo.delegate(opts, {start: 75}))
 				];
 				var observations = [];
-				var lastR;
 				dojo.forEach(results, function(r, i){
 				    r.observe(function(obj, from, to){
 				    	observations.push({from: from, to: to});
 				        console.log(i, " observed: ", obj, from, to);
-				    }, true, lastR);
-				    lastR = r;
+				    }, true);
 				});
 				bigStore.add({id: 101, name: "one oh one", order: 2.5});
 				t.is(results[0].length, 26);
@@ -131,7 +129,7 @@ dojo.require("dojo.store.Observable");
 				bigStore.remove(101);
 				t.is(observations.length, 2);
 				t.is(results[0].length, 25);
-				bigStore.add({id: 102, name: "one oh two", order: 24.5});
+				bigStore.add({id: 102, name: "one oh two", order: 26.5});
 				t.is(results[0].length, 25);
 				t.is(results[1].length, 26);
 				t.is(results[2].length, 25);

2cafee1590b22f32429b57158396a5769eacbd27
Rawld Gill <rgill@altoviso.com>
2012-02-07 09:20:01 +0000

    improved require to take a fast path if the dependency vector is empty; fixes #14724; !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@27766 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo.js
+++ dojo.js
@@ -691,43 +691,46 @@
 			}
 			if(isArray(a1)){
 				// signature is (requestList [,callback])
+				if(!a1.length){
+					a2 && a2();
+				}else{
+					syntheticMid = "require*" + uid();
 
-				syntheticMid = "require*" + uid();
-
-				// resolve the request list with respect to the reference module
-				for(var mid, deps = [], i = 0; i < a1.length;){
-					mid = a1[i++];
-					if(mid in {exports:1, module:1}){
-						throw makeError("illegalModuleId", mid);
+					// resolve the request list with respect to the reference module
+					for(var mid, deps = [], i = 0; i < a1.length;){
+						mid = a1[i++];
+						if(mid in {exports:1, module:1}){
+							throw makeError("illegalModuleId", mid);
+						}
+						deps.push(getModule(mid, referenceModule));
 					}
-					deps.push(getModule(mid, referenceModule));
+
+					// construct a synthetic module to control execution of the requestList, and, optionally, callback
+					module = mix(makeModuleInfo("", syntheticMid, 0, ""), {
+						injected: arrived,
+						deps: deps,
+						def: a2 || noop,
+						require: referenceModule ? referenceModule.require : req
+					});
+					modules[module.mid] = module;
+
+					// checkComplete!=0 holds the idle signal; we're not idle if we're injecting dependencies
+					injectDependencies(module);
+
+					// try to immediately execute
+					// if already traversing a factory tree, then strict causes circular dependency to abort the execution; maybe
+					// it's possible to execute this require later after the current traversal completes and avoid the circular dependency.
+					// ...but *always* insist on immediate in synch mode
+					var strict = checkCompleteGuard && req.async;
+					checkCompleteGuard++;
+					execModule(module, strict);
+					checkIdle();
+					if(!module.executed){
+						// some deps weren't on board or circular dependency detected and strict; therefore, push into the execQ
+						execQ.push(module);
+					}
+					checkComplete();
 				}
-
-				// construct a synthetic module to control execution of the requestList, and, optionally, callback
-				module = mix(makeModuleInfo("", syntheticMid, 0, ""), {
-					injected: arrived,
-					deps: deps,
-					def: a2 || noop,
-					require: referenceModule ? referenceModule.require : req
-				});
-				modules[module.mid] = module;
-
-				// checkComplete!=0 holds the idle signal; we're not idle if we're injecting dependencies
-				injectDependencies(module);
-
-				// try to immediately execute
-				// if already traversing a factory tree, then strict causes circular dependency to abort the execution; maybe
-				// it's possible to execute this require later after the current traversal completes and avoid the circular dependency.
-				// ...but *always* insist on immediate in synch mode
-				var strict = checkCompleteGuard && req.async;
-				checkCompleteGuard++;
-				execModule(module, strict);
-				checkIdle();
-				if(!module.executed){
-					// some deps weren't on board or circular dependency detected and strict; therefore, push into the execQ
-					execQ.push(module);
-				}
-				checkComplete();
 			}
 			return contextRequire;
 		},

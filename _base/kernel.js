c81f46b4978e4b69c47535bea6f18742b90f3664
Rawld Gill <rgill@altoviso.com>
2012-02-08 08:27:46 +0000

    missed file that should have been in [27791]; refs #14406; !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@27798 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/kernel.js
+++ _base/kernel.js
@@ -110,20 +110,34 @@ define(["../has", "./config", "require", "module"], function(has, config, requir
 	// is migrated. Absent specific advice otherwise, set extend-dojo to truthy.
 	has.add("extend-dojo", 1);
 
-	dojo.eval = function(/* scriptText, hint */){
+
+	dojo.eval = function(scriptText){
 		//	summary:
 		//		A legacy method created for use exclusively by internal Dojo methods. Do not use this method
 		//		directly unless you understand its possibly-different implications on the platforms your are targeting.
 		//	description:
-		//		Makes an attempt to evaluate arguments[0] in the global scope, including IE. Note that IE is incapable of
-		//		returning a result. A debugger hint (a string) may be provided for arugments[1], and if provided will be appended
-		//		to the end of arguments[0] as required by various debuggers
+		//		Makes an attempt to evaluate scriptText in the global scope. The function works correctly for browsers
+		//		that support indirect eval.
+		//
+		//		As usual, IE does not. On IE, the only way to implement global eval is to
+		//		use execScript. Unfortunately, execScript does not return a value and breaks some current usages of dojo.eval.
+		//		This implementation uses the technique of executing eval in the scope of a function that is a single scope
+		//		frame below the global scope; thereby coming close to the global scope. Note carefully that
+		//
+		//		dojo.eval("var pi = 3.14;");
+		//
+		//		will define global pi in non-IE environments, but define pi only in a temporary local scope for IE. If you want
+		//		to define a global variable using dojo.eval, write something like
+		//
+		//		dojo.eval("window.pi = 3.14;")
+		//	scriptText:
+		//		The text to evaluation.
 		//	returns:
-		//		The result of the evaluation. Often `undefined`; on IE, unconditionally undefined.
-
-		// note: the four forward-slashes make the firebug hint work in ie9
-		return ((has("ie") &&  dojo.global.execScript) || dojo.global.eval || eval)(arguments[0] + "\r\n////@ sourceURL=" + arguments[1]); // Object
+		//		The result of the evaluation. Often `undefined`
 	};
+
+	(Function("d", "d.eval = function(){return d.global.eval ? d.global.eval(arguments[0]) : eval(arguments[0]);}"))(dojo);
+
 
 	if(has("host-rhino")){
 		dojo.exit = function(exitcode){

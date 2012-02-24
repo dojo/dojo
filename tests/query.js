91dcb57738974eb5591f31f40279c2e0d48c5a2e
Bill Keese <bill@dojotoolkit.org>
2012-02-24 08:57:28 +0000

    Make better tests for new dojo/query module, refs #12451, #14874, #14875, #14876, #14877, #14879, #14880.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@27986 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/query.js
+++ tests/query.js
@@ -1,6 +1,12 @@
 define(["doh", "dojo/sniff", "require"], function(doh, has, require){
 	if(doh.isBrowser){
-		// TODO: refactor tests/_base/query.html, and use it to test every query engine/setting (like, css2, acme, etc.)
+		// Test dojo/query with every query engine/CSS level setting, plus with nothing specified
+		doh.register("tests.query-lite", require.toUrl("tests/query/query.html?selector=lite"), 30000);
+		doh.register("tests.query-css2", require.toUrl("tests/query/query.html?selector=css2"), 30000);
+		doh.register("tests.query-css2.1", require.toUrl("tests/query/query.html?selector=css2.1"), 30000);
+		doh.register("tests.query-css3", require.toUrl("tests/query/query.html?selector=css3"), 30000);
+		doh.register("tests.query-acme", require.toUrl("tests/query/query.html?selector=acme"), 30000);
+		doh.register("tests.query-unspecified", require.toUrl("tests/query/query.html"), 30000);
 
 		// Test loading XHTML document.   Commented out on IE since it causes a warning dialog
 		if(!has("ie")){

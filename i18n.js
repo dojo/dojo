e33893652ffa3590859a33f1ce1fd57f1054eaa3
Rawld Gill <rgill@altoviso.com>
2012-03-09 05:12:51 +0000

    improved evalBundle to handle flattened, built, AMD NLS layers; refs #14092; !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@28124 560b804f-0ae3-0310-86f3-f6aa0a117693

--- i18n.js
+++ i18n.js
@@ -143,31 +143,42 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 	if(has("dojo-v1x-i18n-Api")){
 		// this code path assumes the dojo loader and won't work with a standard AMD loader
 		var
-			__evalError = {},
-
 			evalBundle=
 				// use the function ctor to keep the minifiers away (also come close to global scope, but this is secondary)
-				// if bundle is an AMD bundle, then __amdResult will be defined; otherwise it's a pre-amd bundle and the bundle value is returned by eval
+				new Function(
+					"__bundle",                // the bundle to evalutate
+					"__checkForLegacyModules", // a function that checks if __bundle defined __mid in the global space
+					"__mid",                   // the mid that __bundle is intended to define
 
-				new Function("bundle", "__evalError", "__checkForLegacyModules", "__mid",
-					"var __amdResult, define = function(x){__amdResult= x;};" +
-					"return [(function(){" +
-								"try{eval(arguments[0]);}catch(e){}" +
-								"if(__amdResult)return 0;" +
-								"if((__checkForLegacyModules = __checkForLegacyModules(__mid)))return __checkForLegacyModules;" +
-								"try{return eval('('+arguments[0]+')');}" +
-								"catch(e){__evalError.e = e; return __evalError;}" +
-							"})(bundle),__amdResult];"
+					// returns one of:
+					//		1 => the bundle was an AMD bundle
+					//		a legacy bundle object that is the value of __mid
+					//		instance of Error => could not figure out how to evaluate bundle
+
+					  // used to detect when __bundle calls define
+					  "var define = function(){define.called = 1;};"
+
+					+ "try{"
+					+		"defined.called = 0;"
+					+		"eval(__bundle);"
+					+		"if(define.called==1)"
+								// bundle called define; therefore signal it's an AMD bundle
+					+			"return 1;"
+
+					+		"if((__checkForLegacyModules = __checkForLegacyModules(__mid)))"
+								// bundle was probably a v1.6- built NLS flattened NLS bundle that defined __mid in the global space
+					+			"return __checkForLegacyModules;"
+
+					+ "}catch(e){}"
+					// evaulating the bundle was *neither* an AMD *nor* a legacy flattened bundle
+					// either way, re-eval *after* surrounding with parentheses
+
+					+ "try{"
+					+ 		"return eval('('+__bundle+')');"
+					+ "}catch(e){"
+					+ 		"return e;"
+					+ "}"
 				),
-
-			fixup= function(url, preAmdResult, amdResult){
-				// nls/<locale>/<bundle-name> indicates not the root.
-				if(preAmdResult===__evalError){
-					console.error("failed to evaluate i18n bundle; url=" + url, __evalError.e);
-					return {};
-				}
-				return preAmdResult ? (/nls\/[^\/]+\/[^\/]+$/.test(url) ? preAmdResult : {root:preAmdResult, _v1x:1}) : amdResult;
-			},
 
 			syncRequire= function(deps, callback){
 				var results= [];
@@ -191,8 +202,26 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 								url:url,
 								sync:true,
 								load:function(text){
-									var result = evalBundle(text, __evalError, checkForLegacyModules, mid);
-									results.push(cache[url]= fixup(url, result[0], result[1]));
+									if(/^\/\/>>built/.test(text)){
+										require.eval(text);
+										require([mid], function(bundle){
+											results.push(cache[url]= bundle);
+										});
+									}else{
+										var result = evalBundle(text, checkForLegacyModules, mid);
+										if(result===1){
+											require([mid], function(bundle){
+												results.push(cache[url]= bundle);
+											});
+										}else{
+											if(result instanceof Error){
+												console.error("failed to evaluate i18n bundle; url=" + url, result);
+												result = {};
+											}
+											// nls/<locale>/<bundle-name> indicates not the root.
+											results.push(cache[url] = (/nls\/[^\/]+\/[^\/]+$/.test(url) ? result : {root:result, _v1x:1}));
+										}
+									}
 								},
 								error:function(){
 									results.push(cache[url]= {});
@@ -283,27 +312,23 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 				doh.register("tests.i18n.unit", function(t){
 					var check;
 
-					check = evalBundle("{prop:1}", __evalError);
-					t.is({prop:1}, check[0]); t.is(undefined, check[1]);
+					check = evalBundle("{prop:1}");
+					t.is({prop:1}, check); t.is(undefined, check[1]);
 
-					check = evalBundle("({prop:1})", __evalError);
-					t.is({prop:1}, check[0]); t.is(undefined, check[1]);
+					check = evalBundle("({prop:1})");
+					t.is({prop:1}, check); t.is(undefined, check[1]);
 
-					check = evalBundle("{'prop-x':1}", __evalError);
-					t.is({'prop-x':1}, check[0]); t.is(undefined, check[1]);
+					check = evalBundle("{'prop-x':1}");
+					t.is({'prop-x':1}, check); t.is(undefined, check[1]);
 
-					check = evalBundle("({'prop-x':1})", __evalError);
-					t.is({'prop-x':1}, check[0]); t.is(undefined, check[1]);
+					check = evalBundle("({'prop-x':1})");
+					t.is({'prop-x':1}, check); t.is(undefined, check[1]);
 
-					check = evalBundle("define({'prop-x':1})", __evalError);
-					t.is(0, check[0]); t.is({'prop-x':1}, check[1]);
+					check = evalBundle("define({'prop-x':1})");
+					t.is(1, check);
 
-					check = evalBundle("define({'prop-x':1});", __evalError);
-					t.is(0, check[0]); t.is({'prop-x':1}, check[1]);
-
-					check = evalBundle("this is total nonsense and should throw an error", __evalError);
-					t.is(__evalError, check[0]); t.is(undefined, check[1]);
-					t.is({}, fixup("some/url", check[0], check[1]));
+					check = evalBundle("this is total nonsense and should throw an error");
+					t.is(check instanceof Error, true);
 				});
 			});
 		}

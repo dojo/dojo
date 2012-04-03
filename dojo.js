489f0b1c35f55384f7b65db565233fd19dec1b14
Rawld Gill <rgill@altoviso.com>
2012-04-03 04:10:23 +0000

    improves [28273] to not break ie; thanks bill; refs #15096; !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@28274 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo.js
+++ dojo.js
@@ -1530,7 +1530,7 @@
 	}
 
 	if(has("dom")){
-		has.add("ie-event-behavior", (typeof opera === "undefined" || opera.toString() != "[object Opera]") && doc.attachEvent);
+		has.add("ie-event-behavior", (typeof opera === "undefined" || opera.toString() != "[object Opera]") && !!doc.attachEvent);
 	}
 
 	if(has("dom") && (has("dojo-inject-api") || has("dojo-dom-ready-api"))){

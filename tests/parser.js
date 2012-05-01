94312c8244fa6c081ef14eefb086f16bf5dab827
Bill Keese <bill@dojotoolkit.org>
2012-05-01 17:45:46 +0000

    Unit tests for parseOnLoad: true,  also testing auto-require and declarative require, refs  #14591, #15118 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@28482 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/parser.js
+++ tests/parser.js
@@ -1,6 +1,8 @@
 define(["doh", "require"], function(doh, require){
 	if(doh.isBrowser){
-		doh.register("tests.parser", require.toUrl("tests/parser/parser.html"), 30000);
-		doh.register("tests.parser-args", require.toUrl("tests/parser/parser-args.html"), 30000);
+		doh.register("parser", require.toUrl("tests/parser/parser.html"), 30000);
+		doh.register("parseOnLoad-auto-require", require.toUrl("tests/parser/parseOnLoadAutoRequire.html"), 30000);
+		doh.register("parseOnLoad-declarative-require", require.toUrl("tests/parser/parseOnLoadDeclarativeRequire.html"), 30000);
+		doh.register("parser-args", require.toUrl("tests/parser/parser-args.html"), 30000);
 	}
 });

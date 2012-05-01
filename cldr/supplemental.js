0443c6dc8d9f52af8cd675edf1fca3766a5f03c6
Adam Peller <github@peller.org>
2012-05-01 21:35:33 +0000

    Refs #13906 v21.0.1 update
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@28487 560b804f-0ae3-0310-86f3-f6aa0a117693

--- cldr/supplemental.js
+++ cldr/supplemental.js
@@ -20,7 +20,7 @@ supplemental.getFirstDayOfWeek = function(/*String?*/locale){
 		ae:6,af:6,bh:6,dj:6,dz:6,eg:6,iq:6,ir:6,jo:6,kw:6,
 		ly:6,ma:6,om:6,qa:6,sa:6,sd:6,sy:6,ye:6,
 		ag:0,ar:0,as:0,au:0,br:0,bs:0,bt:0,bw:0,by:0,bz:0,ca:0,cn:0,
-		co:0,dm:0,'do':0,et:0,gt:0,gu:0,hk:0,hn:0,id:0,il:0,'in':0,
+		co:0,dm:0,'do':0,et:0,gt:0,gu:0,hk:0,hn:0,id:0,ie:0,il:0,'in':0,
 		jm:0,jp:0,ke:0,kh:0,kr:0,la:0,mh:0,mm:0,mo:0,mt:0,mx:0,mz:0,
 		ni:0,np:0,nz:0,pa:0,pe:0,ph:0,pk:0,pr:0,py:0,sg:0,sv:0,th:0,
 		tn:0,tt:0,tw:0,um:0,us:0,ve:0,vi:0,ws:0,za:0,zw:0

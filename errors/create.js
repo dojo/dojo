d2ceb956dc939d646d09f8c7e0bf35561238135b
Mark Wubben <mark@novemberborn.net>
2012-06-07 10:27:11 +0000

    Also add dojo/errors/create
    Refs #14615 !strict
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@28755 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ errors/create.js
@@ -0,0 +1,25 @@
+define(["dojo/_base/lang"], function(lang){
+	return function(name, ctor, base, props){
+		base = base || Error;
+
+		var ErrorCtor = function(message){
+			if(base === Error){
+				if(Error.captureStackTrace){
+					Error.captureStackTrace(this, ErrorCtor);
+				}
+				base.call(this, message);
+			}else{
+				base.apply(this, arguments);
+			}
+			if(ctor){
+				ctor.apply(this, arguments);
+			}
+		};
+
+		ErrorCtor.prototype = lang.delegate(base.prototype, props);
+		ErrorCtor.prototype.name = name;
+		ErrorCtor.prototype.constructor = ErrorCtor;
+
+		return ErrorCtor;
+	};
+});

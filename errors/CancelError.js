26ce4e7282b52b16c476d8b7a69b345f0d83ddcf
Mark Wubben <mark@novemberborn.net>
2012-06-07 10:31:03 +0000

    Restore CancelError
    Refs #14615 !strict
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@28756 560b804f-0ae3-0310-86f3-f6aa0a117693

--- errors/CancelError.js
+++ errors/CancelError.js
@@ -1,8 +1,8 @@
-define([], function(){
+define(["./create"], function(create){
 	// module:
 	//		dojo/errors/CancelError
 	// summary:
 	//		Default error if a promise is canceled without a reason.
-	return;
-	//return create("CancelError");
+
+	return create("CancelError");
 });

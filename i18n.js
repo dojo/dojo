92c01b8776e96b25b4e6b76450068b519d9d04c8
Bill Keese <bill@dojotoolkit.org>
2012-07-08 23:48:04 +0000

    dojo/i18n: use dojodoc format for comments, refs #13101 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@29165 560b804f-0ae3-0310-86f3-f6aa0a117693

--- i18n.js
+++ i18n.js
@@ -1,8 +1,8 @@
 define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config", "./_base/lang", "./has!host-browser?./_base/xhr", "./json"],
 	function(dojo, require, has, array, config, lang, xhr, json){
+
 	// module:
 	//		dojo/i18n
-
 
 	has.add("dojo-preload-i18n-Api",
 		// if true, define the preload localizations machinery
@@ -41,18 +41,22 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 			bundlePath,
 			bundleName
 		){
-			// return a vector of module ids containing all available locales with respect to the target locale
-			// For example, assuming:
-			//	 * the root bundle indicates specific bundles for "fr" and "fr-ca",
-			//	 * bundlePath is "myPackage/nls"
-			//	 * bundleName is "myBundle"
-			// Then a locale argument of "fr-ca" would return
-			//	 ["myPackage/nls/myBundle", "myPackage/nls/fr/myBundle", "myPackage/nls/fr-ca/myBundle"]
-			// Notice that bundles are returned least-specific to most-specific, starting with the root.
+			// summary:
+			//		return a vector of module ids containing all available locales with respect to the target locale
+			//		For example, assuming:
 			//
-			// If root===false indicates we're working with a pre-AMD i18n bundle that doesn't tell about the available locales;
-			// therefore, assume everything is available and get 404 errors that indicate a particular localization is not available
+			//		- the root bundle indicates specific bundles for "fr" and "fr-ca",
+			//		-  bundlePath is "myPackage/nls"
+			//		- bundleName is "myBundle"
 			//
+			//		Then a locale argument of "fr-ca" would return
+			//
+			//			["myPackage/nls/myBundle", "myPackage/nls/fr/myBundle", "myPackage/nls/fr-ca/myBundle"]
+			//
+			//		Notice that bundles are returned least-specific to most-specific, starting with the root.
+			//
+			//		If root===false indicates we're working with a pre-AMD i18n bundle that doesn't tell about the available locales;
+			//		therefore, assume everything is available and get 404 errors that indicate a particular localization is not available
 
 			for(var result = [bundlePath + bundleName], localeParts = locale.split("-"), current = "", i = 0; i<localeParts.length; i++){
 				current += (current ? "-" : "") + localeParts[i];
@@ -75,7 +79,8 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 		},
 
 		doLoad = function(require, bundlePathAndName, bundlePath, bundleName, locale, load){
-			// get the root bundle which instructs which other bundles are required to construct the localized bundle
+			// summary:
+			//		get the root bundle which instructs which other bundles are required to construct the localized bundle
 			require([bundlePathAndName], function(root){
 				var current = lang.clone(root.root),
 					availableLocales = getAvailableLocales(!root._v1x && root, locale, bundlePath, bundleName);
@@ -92,9 +97,10 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 		},
 
 		normalize = function(id, toAbsMid){
-			// id may be relative
-			// preload has form *preload*<path>/nls/<module>*<flattened locales> and
-			// therefore never looks like a relative
+			// summary:
+			//		id may be relative.
+			//		preload has form `*preload*<path>/nls/<module>*<flattened locales>` and
+			//		therefore never looks like a relative
 			return /^\./.test(id) ? toAbsMid(id) : id;
 		},
 
@@ -106,130 +112,130 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 		},
 
 		load = function(id, require, load){
+			// summary:
+			//		id is in one of the following formats
 			//
-			// id is in one of the following formats
+			//		1. <path>/nls/<bundle>
+			//			=> load the bundle, localized to config.locale; load all bundles localized to
+			//			config.extraLocale (if any); return the loaded bundle localized to config.locale.
 			//
-			//	1. <path>/nls/<bundle>
-			//		=> load the bundle, localized to config.locale; load all bundles localized to
-			//		config.extraLocale (if any); return the loaded bundle localized to config.locale.
+			//		2. <path>/nls/<locale>/<bundle>
+			//			=> load then return the bundle localized to <locale>
 			//
-			//	2. <path>/nls/<locale>/<bundle>
-			//		=> load then return the bundle localized to <locale>
+			//		3. *preload*<path>/nls/<module>*<JSON array of available locales>
+			//			=> for config.locale and all config.extraLocale, load all bundles found
+			//			in the best-matching bundle rollup. A value of 1 is returned, which
+			//			is meaningless other than to say the plugin is executing the requested
+			//			preloads
 			//
-			//	3. *preload*<path>/nls/<module>*<JSON array of available locales>
-			//		=> for config.locale and all config.extraLocale, load all bundles found
-			//		   in the best-matching bundle rollup. A value of 1 is returned, which
-			//		   is meaningless other than to say the plugin is executing the requested
-			//		   preloads
+			//		In cases 1 and 2, <path> is always normalized to an absolute module id upon entry; see
+			//		normalize. In case 3, it <path> is assumed to be absolute; this is arranged by the builder.
 			//
-			// In cases 1 and 2, <path> is always normalized to an absolute module id upon entry; see
-			// normalize. In case 3, it <path> is assumed to be absolute; this is arranged by the builder.
+			//		To load a bundle means to insert the bundle into the plugin's cache and publish the bundle
+			//		value to the loader. Given <path>, <bundle>, and a particular <locale>, the cache key
 			//
-			// To load a bundle means to insert the bundle into the plugin's cache and publish the bundle
-			// value to the loader. Given <path>, <bundle>, and a particular <locale>, the cache key
+			//			<path>/nls/<bundle>/<locale>
 			//
-			//	 <path>/nls/<bundle>/<locale>
+			//		will hold the value. Similarly, then plugin will publish this value to the loader by
 			//
-			// will hold the value. Similarly, then plugin will publish this value to the loader by
+			//			define("<path>/nls/<bundle>/<locale>", <bundle-value>);
 			//
-			//	 define("<path>/nls/<bundle>/<locale>", <bundle-value>);
+			//		Given this algorithm, other machinery can provide fast load paths be preplacing
+			//		values in the plugin's cache, which is public. When a load is demanded the
+			//		cache is inspected before starting any loading. Explicitly placing values in the plugin
+			//		cache is an advanced/experimental feature that should not be needed; use at your own risk.
 			//
-			// Given this algorithm, other machinery can provide fast load paths be preplacing
-			// values in the plugin's cache, which is public. When a load is demanded the
-			// cache is inspected before starting any loading. Explicitly placing values in the plugin
-			// cache is an advanced/experimental feature that should not be needed; use at your own risk.
+			//		For the normal AMD algorithm, the root bundle is loaded first, which instructs the
+			//		plugin what additional localized bundles are required for a particular locale. These
+			//		additional locales are loaded and a mix of the root and each progressively-specific
+			//		locale is returned. For example:
 			//
-			// For the normal AMD algorithm, the root bundle is loaded first, which instructs the
-			// plugin what additional localized bundles are required for a particular locale. These
-			// additional locales are loaded and a mix of the root and each progressively-specific
-			// locale is returned. For example:
+			//		1. The client demands "dojo/i18n!some/path/nls/someBundle
 			//
-			// 1. The client demands "dojo/i18n!some/path/nls/someBundle
+			//		2. The loader demands load(some/path/nls/someBundle)
 			//
-			// 2. The loader demands load(some/path/nls/someBundle)
+			//		3. This plugin require's "some/path/nls/someBundle", which is the root bundle.
 			//
-			// 3. This plugin require's "some/path/nls/someBundle", which is the root bundle.
+			//		4. Assuming config.locale is "ab-cd-ef" and the root bundle indicates that localizations
+			//		are available for "ab" and "ab-cd-ef" (note the missing "ab-cd", then the plugin
+			//		requires "some/path/nls/ab/someBundle" and "some/path/nls/ab-cd-ef/someBundle"
 			//
-			// 4. Assuming config.locale is "ab-cd-ef" and the root bundle indicates that localizations
-			//	  are available for "ab" and "ab-cd-ef" (note the missing "ab-cd", then the plugin
-			//	  requires "some/path/nls/ab/someBundle" and "some/path/nls/ab-cd-ef/someBundle"
+			//		5. Upon receiving all required bundles, the plugin constructs the value of the bundle
+			//		ab-cd-ef as...
 			//
-			// 5. Upon receiving all required bundles, the plugin constructs the value of the bundle
-			//	  ab-cd-ef as...
+			//				mixin(mixin(mixin({}, require("some/path/nls/someBundle"),
+			//		  			require("some/path/nls/ab/someBundle")),
+			//					require("some/path/nls/ab-cd-ef/someBundle"));
 			//
-			//		mixin(mixin(mixin({}, require("some/path/nls/someBundle"),
-			//		  require("some/path/nls/ab/someBundle")),
-			//			require("some/path/nls/ab-cd-ef/someBundle"));
+			//		This value is inserted into the cache and published to the loader at the
+			//		key/module-id some/path/nls/someBundle/ab-cd-ef.
 			//
-			//	  This value is inserted into the cache and published to the loader at the
-			//	  key/module-id some/path/nls/someBundle/ab-cd-ef.
+			//		The special preload signature (case 3) instructs the plugin to stop servicing all normal requests
+			//		(further preload requests will be serviced) until all ongoing preloading has completed.
 			//
-			// The special preload signature (case 3) instructs the plugin to stop servicing all normal requests
-			// (further preload requests will be serviced) until all ongoing preloading has completed.
+			//		The preload signature instructs the plugin that a special rollup module is available that contains
+			//		one or more flattened, localized bundles. The JSON array of available locales indicates which locales
+			//		are available. Here is an example:
 			//
-			// The preload signature instructs the plugin that a special rollup module is available that contains
-			// one or more flattened, localized bundles. The JSON array of available locales indicates which locales
-			// are available. Here is an example:
+			//			*preload*some/path/nls/someModule*["root", "ab", "ab-cd-ef"]
 			//
-			//	 *preload*some/path/nls/someModule*["root", "ab", "ab-cd-ef"]
+			//		This indicates the following rollup modules are available:
 			//
-			// This indicates the following rollup modules are available:
+			//			some/path/nls/someModule_ROOT
+			//			some/path/nls/someModule_ab
+			//			some/path/nls/someModule_ab-cd-ef
 			//
-			//	 some/path/nls/someModule_ROOT
-			//	 some/path/nls/someModule_ab
-			//	 some/path/nls/someModule_ab-cd-ef
+			//		Each of these modules is a normal AMD module that contains one or more flattened bundles in a hash.
+			//		For example, assume someModule contained the bundles some/bundle/path/someBundle and
+			//		some/bundle/path/someOtherBundle, then some/path/nls/someModule_ab would be expressed as follows:
 			//
-			// Each of these modules is a normal AMD module that contains one or more flattened bundles in a hash.
-			// For example, assume someModule contained the bundles some/bundle/path/someBundle and
-			// some/bundle/path/someOtherBundle, then some/path/nls/someModule_ab would be expressed as follows:
+			//			define({
+			//				some/bundle/path/someBundle:<value of someBundle, flattened with respect to locale ab>,
+			//				some/bundle/path/someOtherBundle:<value of someOtherBundle, flattened with respect to locale ab>,
+			//			});
 			//
-			// define({
-			//	 some/bundle/path/someBundle:<value of someBundle, flattened with respect to locale ab>,
-			//	 some/bundle/path/someOtherBundle:<value of someOtherBundle, flattened with respect to locale ab>,
-			// });
+			//		E.g., given this design, preloading for locale=="ab" can execute the following algorithm:
 			//
-			// E.g., given this design, preloading for locale=="ab" can execute the following algorithm:
+			//			require(["some/path/nls/someModule_ab"], function(rollup){
+			//				for(var p in rollup){
+			//					var id = p + "/ab",
+			//					cache[id] = rollup[p];
+			//					define(id, rollup[p]);
+			//				}
+			//			});
 			//
-			// require(["some/path/nls/someModule_ab"], function(rollup){
-			//	 for(var p in rollup){
-			//	   var id = p + "/ab",
-			//	   cache[id] = rollup[p];
-			//	   define(id, rollup[p]);
-			//	 }
-			// });
+			//		Similarly, if "ab-cd" is requested, the algorithm can determine that "ab" is the best available and
+			//		load accordingly.
 			//
-			// Similarly, if "ab-cd" is requested, the algorithm can determine that "ab" is the best available and
-			// load accordingly.
+			//		The builder will write such rollups for every layer if a non-empty localeList  profile property is
+			//		provided. Further, the builder will include the following cache entry in the cache associated with
+			//		any layer.
 			//
-			// The builder will write such rollups for every layer if a non-empty localeList  profile property is
-			// provided. Further, the builder will include the following cache entry in the cache associated with
-			// any layer.
+			//			"*now":function(r){r(['dojo/i18n!*preload*<path>/nls/<module>*<JSON array of available locales>']);}
 			//
-			//	 "*now":function(r){r(['dojo/i18n!*preload*<path>/nls/<module>*<JSON array of available locales>']);}
+			//		The *now special cache module instructs the loader to apply the provided function to context-require
+			//		with respect to the particular layer being defined. This causes the plugin to hold all normal service
+			//		requests until all preloading is complete.
 			//
-			// The *now special cache module instructs the loader to apply the provided function to context-require
-			// with respect to the particular layer being defined. This causes the plugin to hold all normal service
-			// requests until all preloading is complete.
+			//		Notice that this algorithm is rarely better than the standard AMD load algorithm. Consider the normal case
+			//		where the target locale has a single segment and a layer depends on a single bundle:
 			//
-			// Notice that this algorithm is rarely better than the standard AMD load algorithm. Consider the normal case
-			// where the target locale has a single segment and a layer depends on a single bundle:
+			//		Without Preloads:
 			//
-			// Without Preloads:
+			//		1. Layer loads root bundle.
+			//		2. bundle is demanded; plugin loads single localized bundle.
 			//
-			//	 1. Layer loads root bundle.
-			//	 2. bundle is demanded; plugin loads single localized bundle.
+			//		With Preloads:
 			//
-			// With Preloads:
+			//		1. Layer causes preloading of target bundle.
+			//		2. bundle is demanded; service is delayed until preloading complete; bundle is returned.
 			//
-			//	 1. Layer causes preloading of target bundle.
-			//	 2. bundle is demanded; service is delayed until preloading complete; bundle is returned.
-			//
-			// In each case a single transaction is required to load the target bundle. In cases where multiple bundles
-			// are required and/or the locale has multiple segments, preloads still requires a single transaction whereas
-			// the normal path requires an additional transaction for each additional bundle/locale-segment. However all
-			// of these additional transactions can be done concurrently. Owing to this analysis, the entire preloading
-			// algorithm can be discard during a build by setting the has feature dojo-preload-i18n-Api to false.
-			//
+			//		In each case a single transaction is required to load the target bundle. In cases where multiple bundles
+			//		are required and/or the locale has multiple segments, preloads still requires a single transaction whereas
+			//		the normal path requires an additional transaction for each additional bundle/locale-segment. However all
+			//		of these additional transactions can be done concurrently. Owing to this analysis, the entire preloading
+			//		algorithm can be discard during a build by setting the has feature dojo-preload-i18n-Api to false.
+
 			if(has("dojo-preload-i18n-Api")){
 				var split = id.split("*"),
 					preloadDemand = split[1] == "preload";
@@ -298,7 +304,6 @@ define(["./_base/kernel", "require", "./has", "./_base/array", "./_base/config",
 			preloadL10n = thisModule._preloadLocalizations = function(/*String*/bundlePrefix, /*Array*/localesGenerated, /*boolean?*/ guaranteedAmdFormat, /*function?*/ contextRequire){
 				// summary:
 				//		Load available flattened resource bundles associated with a particular module for dojo.locale and all dojo.config.extraLocale (if any)
-				//
 				// description:
 				//		Only called by built layer files. The entire locale hierarchy is loaded. For example,
 				//		if locale=="ab-cd", then ROOT, "ab", and "ab-cd" are loaded. This is different than v1.6-

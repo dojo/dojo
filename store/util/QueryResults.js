3703e2d6d2e3a598aee3f4f624c485e09ba4fcf6
Bill Keese <bill@dojotoolkit.org>
2012-07-21 00:05:24 +0000

    doc fixes, for !QueryResults, refs #13101
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@29357 560b804f-0ae3-0310-86f3-f6aa0a117693

--- store/util/QueryResults.js
+++ store/util/QueryResults.js
@@ -8,7 +8,6 @@ var QueryResults = function(results){
 	// summary:
 	//		A function that wraps the results of a store query with additional
 	//		methods.
-	//
 	// description:
 	//		QueryResults is a basic wrapper that allows for array-like iteration
 	//		over any kind of returned data from a query.  While the simplest store
@@ -17,13 +16,10 @@ var QueryResults = function(results){
 	//		the same.
 	//
 	//		Additional methods include `forEach`, `filter` and `map`.
-	//
-	//	results: Array | dojo/promise
+	// results: Array|dojo/promise/Promise
 	//		The result set as an array, or a promise for an array.
-	//
-	// returns: Object
+	// returns:
 	//		An array-like object that can be used for iterating over.
-	//
 	// example:
 	//		Query a store and iterate over the results.
 	//
@@ -57,7 +53,7 @@ var QueryResults = function(results){
 			return results.length;
 		});
 	}
-	return results;
+	return results; // Object
 };
 
 lang.setObject("dojo.store.util.QueryResults", QueryResults);

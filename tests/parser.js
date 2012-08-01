416276c4a187d30792d12da6f77b767f42fd6104
Bill Keese <bill@dojotoolkit.org>
2012-08-01 00:24:23 +0000

    use relative paths in require.toUrl(), rather than depending on weird mapping from "tests" to "dojo/tests", fixes parser tests in built version of dojo, refs #14592
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@29414 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/parser.js
+++ tests/parser.js
@@ -1,8 +1,8 @@
 define(["doh/main", "require"], function(doh, require){
 	if(doh.isBrowser){
-		doh.register("parser", require.toUrl("tests/parser/parser.html"), 30000);
-		doh.register("parseOnLoad-auto-require", require.toUrl("tests/parser/parseOnLoadAutoRequire.html"), 30000);
-		doh.register("parseOnLoad-declarative-require", require.toUrl("tests/parser/parseOnLoadDeclarativeRequire.html"), 30000);
-		doh.register("parser-args", require.toUrl("tests/parser/parser-args.html"), 30000);
+		doh.register("parser", require.toUrl("./parser/parser.html"), 30000);
+		doh.register("parseOnLoad-auto-require", require.toUrl("./parser/parseOnLoadAutoRequire.html"), 30000);
+		doh.register("parseOnLoad-declarative-require", require.toUrl("./parser/parseOnLoadDeclarativeRequire.html"), 30000);
+		doh.register("parser-args", require.toUrl("./parser/parser-args.html"), 30000);
 	}
 });

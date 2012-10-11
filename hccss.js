3340930792feb4c19cb4638dd14f55c5f8096356
Bill Keese <bill@dojotoolkit.org>
2012-10-11 03:36:50 +0000

    Fix mixed content warning on IE7, fixes #16013 on 1.8 branch !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.8/dojo@29806 560b804f-0ae3-0310-86f3-f6aa0a117693

--- hccss.js
+++ hccss.js
@@ -2,12 +2,11 @@ define([
 	"require",			// require.toUrl
 	"./_base/config", // config.blankGif
 	"./dom-class", // domClass.add
-	"./dom-construct", // domConstruct.destroy
 	"./dom-style", // domStyle.getComputedStyle
 	"./has",
 	"./ready", // ready
 	"./_base/window" // win.body
-], function(require, config, domClass, domConstruct, domStyle, has, ready, win){
+], function(require, config, domClass, domStyle, has, ready, win){
 
 	// module:
 	//		dojo/hccss
@@ -34,7 +33,11 @@ define([
 			hc = (cs.borderTopColor == cs.borderRightColor) ||
 				(bkImg && (bkImg == "none" || bkImg == "url(invalid-url:)" ));
 
-		domConstruct.destroy(div);
+		if(has("ie") < 8){
+			div.outerHTML = "";		// prevent mixed-content warning, see http://support.microsoft.com/kb/925014
+		}else{
+			win.body().removeChild(div);
+		}
 
 		return hc;
 	});

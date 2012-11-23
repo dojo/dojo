91b085ab5a9bdb428cb2b4619f6bc4ba6f178d1a
Colin Snover <github.com@zetafleet.com>
2012-11-23 23:40:16 +0000

    Backport improved query test suite from 1.8 to 1.7. Refs #16113.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.7/dojo@30039 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tests/query.js
@@ -0,0 +1,16 @@
+define(["doh/main", "dojo/_base/sniff", "require"], function(doh, has, require){
+	if(doh.isBrowser){
+		// Test dojo/query with every query engine/CSS level setting, plus with nothing specified
+		doh.register("tests.query-lite", require.toUrl("./query/query.html?selector=lite"), 30000);
+		doh.register("tests.query-css2", require.toUrl("./query/query.html?selector=css2"), 30000);
+		doh.register("tests.query-css2.1", require.toUrl("./query/query.html?selector=css2.1"), 30000);
+		doh.register("tests.query-css3", require.toUrl("./query/query.html?selector=css3"), 30000);
+		doh.register("tests.query-acme", require.toUrl("./query/query.html?selector=acme"), 30000);
+		doh.register("tests.query-unspecified", require.toUrl("./query/query.html"), 30000);
+
+		// Test loading XHTML document.   Commented out on IE since it causes a warning dialog
+		if(!has("ie")){
+			doh.register("tests.query-xml", require.toUrl("./query/xml.xhtml"), 30000);
+		}
+	}
+});

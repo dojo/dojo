38652d3b96966274de3874ed1fbe90789cd809ac
Colin Snover <github.com@zetafleet.com>
2012-11-23 06:17:18 +0000

    Disable more quirks tests in IE9+. Refs #14321. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@30020 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/window.js
+++ tests/window.js
@@ -1,5 +1,6 @@
-define(["doh/main", "require"], function(doh, require){
+define(["dojo/has", "doh/main", "require"], function(has, doh, require){
 	doh.register("tests.window.viewport", require.toUrl("./window/viewport.html"));
-	doh.register("tests.window.viewportQuirks", require.toUrl("./window/viewportQuirks.html"));
+	// IE9+ cannot handle quirks mode in test runner, see #14321
+	has("ie") >= 9 || doh.register("tests.window.viewportQuirks", require.toUrl("./window/viewportQuirks.html"));
 	doh.register("tests.window.test_scroll", require.toUrl("./window/test_scroll.html"), 99999999);
 });

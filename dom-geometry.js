676e225387389701e7a773280cdf44cf5cd1a712
Colin Snover <github.com@zetafleet.com>
2012-11-23 05:15:28 +0000

    As it turns out, has("ie") returns undefined, not false, so it does not need to be checked twice. Refs #12342. Backport to 1.8.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.8/dojo@30010 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dom-geometry.js
+++ dom-geometry.js
@@ -529,7 +529,7 @@ define(["./sniff", "./_base/window","./dom", "./dom-style"],
 			ret = node.getBoundingClientRect();
 		ret = {x: ret.left, y: ret.top, w: ret.right - ret.left, h: ret.bottom - ret.top};
 
-		if(has("ie") && has("ie") < 9){
+		if(has("ie") < 9){
 			// On IE<9 there's a 2px offset that we need to adjust for, see dojo.getIeDocumentElementOffset()
 			var offset = geom.getIeDocumentElementOffset(node.ownerDocument);
 

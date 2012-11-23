aa03051fa8a331bb2942621212472f240bbec687
Colin Snover <github.com@zetafleet.com>
2012-11-23 06:41:38 +0000

    Disable more quirks tests in IE9+. Refs #14321. Backport to 1.7. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.7/dojo@30024 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/uacss.js
+++ tests/uacss.js
@@ -1,6 +1,7 @@
-define(["doh", "require"], function(doh, require){
+define(["dojo/has", "doh", "require"], function(has, doh, require){
 
-	doh.register("tests.uacss.sniffQuirks", require.toUrl("./uacss/sniffQuirks.html"));
+	// IE9+ cannot handle quirks mode in test runner, see #14321
+	has("ie") >= 9 || doh.register("tests.uacss.sniffQuirks", require.toUrl("./uacss/sniffQuirks.html"));
 	doh.register("tests.uacss.sniffStandards", require.toUrl("./uacss/sniffStandards.html"));
 
 });

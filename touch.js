0c14d6f9246adc4018be106fb234948562c57855
Bill Keese <bill@dojotoolkit.org>
2013-03-21 00:56:53 +0000

    Don't call evt.preventDefault() for <textarea>, same as <input type=text>, refs #15878 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojo/trunk@30949 560b804f-0ae3-0310-86f3-f6aa0a117693

--- touch.js
+++ touch.js
@@ -115,7 +115,8 @@ function(dojo, aspect, dom, domClass, lang, on, has, mouse, domReady, win){
 								((new Date()).getTime() <= clickTime + 1000 &&
 									!(e.target.tagName == "INPUT" && domClass.contains(e.target, "dijitOffScreen"))))){
 							e.stopImmediatePropagation();
-							if(e.target.tagName != "INPUT" || e.target.type == "radio" || e.target.type == "checkbox"){
+							if((e.target.tagName != "INPUT" || e.target.type == "radio" || e.target.type == "checkbox")
+								&& e.target.tagName != "TEXTAREA"){
 								 // preventDefault() breaks textual <input>s on android, keyboard doesn't popup,
 								 // but it is still needed for checkboxes and radio buttons, otherwise in some cases
 								 // the checked state becomes inconsistent with the widget's state

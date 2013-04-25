bbf50e9221455105a951ce67673f5de66e8ebc94
Colin Snover <github.com@zetafleet.com>
2013-04-25 03:10:29 +0000

    Fix dojo/request XML test for people that still have short_open_tag turned on in PHP. Fixes #15631. Backport to 1.8
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.8/dojo@31341 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/request/xhrXml.php
+++ tests/request/xhrXml.php
@@ -5,5 +5,5 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-cache, must-revalidate");
 header("Pragma: no-cache");
 header("Content-type: application/xml");
-?><?xml version="1.0" encoding="UTF-8" ?>
+?><?php echo '<?xml version="1.0" encoding="UTF-8" ?>'; ?>
 <foo><bar baz='thonk'>blarg</bar><bar>blah</bar></foo>

334bf36195c30650eb3f44ad821929f185a1c81a
Colin Snover <github.com@zetafleet.com>
2013-04-25 05:30:24 +0000

    Using r31351 to create a tag for the 1.9.0rc2 release.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.9.0rc2/dojo@31352 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ trunk/cldr/nls/en-gb/currency.js
@@ -0,0 +1,14 @@
+define(
+//begin v1.x content
+{
+	"CAD_symbol": "CA$",
+	"EUR_displayName": "Euro",
+	"GBP_displayName": "British Pound",
+	"GBP_symbol": "£",
+	"HKD_symbol": "HK$",
+	"AUD_symbol": "AU$",
+	"CNY_symbol": "CN¥",
+	"EUR_symbol": "€"
+}
+//end v1.x content
+);

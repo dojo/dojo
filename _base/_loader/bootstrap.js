ff124e5893f25a78d04dbbfb9eed83f26837774e
Colin Snover <github.com@zetafleet.com>
2013-08-21 21:54:16 -0700

    Updating metadata for 1.6.2

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -228,7 +228,7 @@ dojo.global = {
 =====*/
 	dojo.locale = d.config.locale;
 
-	var rev = "$Rev$".match(/\d+/);
+	var rev = "$Rev: c67ea3e $".match(/\d+/);
 
 /*=====
 	dojo.version = function(){
@@ -252,7 +252,7 @@ dojo.global = {
 	}
 =====*/
 	dojo.version = {
-		major: 1, minor: 6, patch: 0, flag: "dev",
+		major: 1, minor: 6, patch: 2, flag: "",
 		revision: rev ? +rev[0] : NaN,
 		toString: function(){
 			with(d.version){

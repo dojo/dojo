2964bb5a71f8ce25cecd20528e912aca07239dda
Jos Braaksma <jos@tiltedsolutions.com>
2013-10-01 18:15:21 +0200

    Revert "fix to allow deferreds from wrapped store"
    This reverts commit 2f5c6aab535069b2f8563615c5a3418d37e9ac12.

--- store/Observable.js
+++ store/Observable.js
@@ -50,7 +50,7 @@ var Observable = function(/*Store*/ store){
 	store.query = function(query, options){
 		options = options || {};
 		var results = originalQuery.apply(this, arguments);
-		when(results, function(resultsArray){
+		if(results && results.forEach){
 			var nonPagedOptions = lang.mixin({}, options);
 			delete nonPagedOptions.start;
 			delete nonPagedOptions.count;
@@ -58,71 +58,73 @@ var Observable = function(/*Store*/ store){
 			var queryExecutor = store.queryEngine && store.queryEngine(query, nonPagedOptions);
 			var queryRevision = revision;
 			var listeners = [], queryUpdater;
-			resultsArray.observe = function(listener, includeObjectUpdates){
+			results.observe = function(listener, includeObjectUpdates){
 				if(listeners.push(listener) == 1){
 					// first listener was added, create the query checker and updater
 					queryUpdaters.push(queryUpdater = function(changed, existingId){
-						var atEnd = resultsArray.length != options.count;
-						var i, l, listener;
-						if(++queryRevision != revision){
-							throw new Error("Query is out of date, you must observe() the query prior to any data modifications");
-						}
-						var removedObject, removedFrom = -1, insertedInto = -1;
-						if(existingId !== undef){
-							// remove the old one
-							for(i = 0, l = resultsArray.length; i < l; i++){
-								var object = resultsArray[i];
-								if(store.getIdentity(object) == existingId){
-									removedObject = object;
-									removedFrom = i;
-									if(queryExecutor || !changed){// if it was changed and we don't have a queryExecutor, we shouldn't remove it because updated objects would be eliminated
-										resultsArray.splice(i, 1);
-									}
-									break;
-								}
+						when(results, function(resultsArray){
+							var atEnd = resultsArray.length != options.count;
+							var i, l, listener;
+							if(++queryRevision != revision){
+								throw new Error("Query is out of date, you must observe() the query prior to any data modifications");
 							}
-						}
-						if(queryExecutor){
-							// add the new one
-							if(changed &&
-									// if a matches function exists, use that (probably more efficient)
-									(queryExecutor.matches ? queryExecutor.matches(changed) : queryExecutor([changed]).length)){
-
-								var firstInsertedInto = removedFrom > -1 ? 
-									removedFrom : // put back in the original slot so it doesn't move unless it needs to (relying on a stable sort below)
-									resultsArray.length;
-								resultsArray.splice(firstInsertedInto, 0, changed); // add the new item
-								insertedInto = array.indexOf(queryExecutor(resultsArray), changed); // sort it
-								// we now need to push the change back into the original results array
-								resultsArray.splice(firstInsertedInto, 1); // remove the inserted item from the previous index
-								
-								if((options.start && insertedInto == 0) ||
-									(!atEnd && insertedInto == resultsArray.length)){
-									// if it is at the end of the page, assume it goes into the prev or next page
-									insertedInto = -1;
-								}else{
-									resultsArray.splice(insertedInto, 0, changed); // and insert into the results array with the correct index
-								}
-							}
-						}else if(changed){
-							// we don't have a queryEngine, so we can't provide any information
-							// about where it was inserted or moved to. If it is an update, we leave it's position alone, other we at least indicate a new object
+							var removedObject, removedFrom = -1, insertedInto = -1;
 							if(existingId !== undef){
-								// an update, keep the index the same
-								insertedInto = removedFrom;
-							}else if(!options.start){
-								// a new object
-								insertedInto = store.defaultIndex || 0;
-								resultsArray.splice(insertedInto, 0, changed);
+								// remove the old one
+								for(i = 0, l = resultsArray.length; i < l; i++){
+									var object = resultsArray[i];
+									if(store.getIdentity(object) == existingId){
+										removedObject = object;
+										removedFrom = i;
+										if(queryExecutor || !changed){// if it was changed and we don't have a queryExecutor, we shouldn't remove it because updated objects would be eliminated
+											resultsArray.splice(i, 1);
+										}
+										break;
+									}
+								}
 							}
-						}
-						if((removedFrom > -1 || insertedInto > -1) &&
-								(includeObjectUpdates || !queryExecutor || (removedFrom != insertedInto))){
-							var copyListeners = listeners.slice();
-							for(i = 0;listener = copyListeners[i]; i++){
-								listener(changed || removedObject, removedFrom, insertedInto);
+							if(queryExecutor){
+								// add the new one
+								if(changed &&
+										// if a matches function exists, use that (probably more efficient)
+										(queryExecutor.matches ? queryExecutor.matches(changed) : queryExecutor([changed]).length)){
+
+									var firstInsertedInto = removedFrom > -1 ? 
+										removedFrom : // put back in the original slot so it doesn't move unless it needs to (relying on a stable sort below)
+										resultsArray.length;
+									resultsArray.splice(firstInsertedInto, 0, changed); // add the new item
+									insertedInto = array.indexOf(queryExecutor(resultsArray), changed); // sort it
+									// we now need to push the change back into the original results array
+									resultsArray.splice(firstInsertedInto, 1); // remove the inserted item from the previous index
+									
+									if((options.start && insertedInto == 0) ||
+										(!atEnd && insertedInto == resultsArray.length)){
+										// if it is at the end of the page, assume it goes into the prev or next page
+										insertedInto = -1;
+									}else{
+										resultsArray.splice(insertedInto, 0, changed); // and insert into the results array with the correct index
+									}
+								}
+							}else if(changed){
+								// we don't have a queryEngine, so we can't provide any information
+								// about where it was inserted or moved to. If it is an update, we leave it's position alone, other we at least indicate a new object
+								if(existingId !== undef){
+									// an update, keep the index the same
+									insertedInto = removedFrom;
+								}else if(!options.start){
+									// a new object
+									insertedInto = store.defaultIndex || 0;
+									resultsArray.splice(insertedInto, 0, changed);
+								}
 							}
-						}
+							if((removedFrom > -1 || insertedInto > -1) &&
+									(includeObjectUpdates || !queryExecutor || (removedFrom != insertedInto))){
+								var copyListeners = listeners.slice();
+								for(i = 0;listener = copyListeners[i]; i++){
+									listener(changed || removedObject, removedFrom, insertedInto);
+								}
+							}
+						});
 					});
 				}
 				var handle = {};
@@ -140,7 +142,7 @@ var Observable = function(/*Store*/ store){
 				};
 				return handle;
 			};
-		});
+		}
 		return results;
 	};
 	var inMethod;

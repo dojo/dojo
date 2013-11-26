772563ca6cedfd1126def94ca3f7b35849e38a23
Colin Snover <github.com@zetafleet.com>
2013-11-26 22:58:30 +0000

    Revert "Fix IE11 sniffing. Fixes #17311."
    This reverts commit f58c0e19ce509398985908688a768c8fb41191b8.
    
    (cherry picked from commit 477d62923e7cd1e32799395301f21c008dc55de6)
    (cherry picked from commit 788afa04681010da261850e81f847089826c7dba)

--- sniff.js
+++ sniff.js
@@ -34,15 +34,19 @@ define(["./has"], function(has){
 				has.add("opera", tv >= 9.8 ? parseFloat(dua.split("Version/")[1]) || tv : tv);
 			}
 
-			var isIE = 0;
-			if(document.all && !has("opera")){
-				// IE < 11
-				isIE = parseFloat(dav.split("MSIE ")[1]) || undefined;
-			}else if(dav.indexOf("Trident")){
-				// IE >= 9
-				isIE = parseFloat(dav.split("rv:")[1]) || undefined;
+			// Mozilla and firefox
+			if(dua.indexOf("Gecko") >= 0 && !has("khtml") && !has("webkit")){
+				has.add("mozilla", tv);
 			}
-			if(isIE){
+			if(has("mozilla")){
+				//We really need to get away from this. Consider a sane isGecko approach for the future.
+				has.add("ff", parseFloat(dua.split("Firefox/")[1] || dua.split("Minefield/")[1]) || undefined);
+			}
+
+			// IE
+			if(document.all && !has("opera")){
+				var isIE = parseFloat(dav.split("MSIE ")[1]) || undefined;
+
 				//In cases where the page has an HTTP header or META tag with
 				//X-UA-Compatible, then it is in emulation mode.
 				//Make sure isIE reflects the desired version.
@@ -55,15 +59,6 @@ define(["./has"], function(has){
 				}
 
 				has.add("ie", isIE);
-			}
-			
-			// Mozilla and firefox
-			if(!has("ie") && dua.indexOf("Gecko") >= 0 && !has("khtml") && !has("webkit")){
-				has.add("mozilla", tv);
-			}
-			if(has("mozilla")){
-				//We really need to get away from this. Consider a sane isGecko approach for the future.
-				has.add("ff", parseFloat(dua.split("Firefox/")[1] || dua.split("Minefield/")[1]) || undefined);
 			}
 
 			// Wii

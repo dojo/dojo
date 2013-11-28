7edc2a0e4c7bbad2cdb932df60f81b670db5c8b5
Stephen Simpson <me@simpo.org>
2013-11-28 15:50:49 +0000

    Added tests for the loading of Dojo inside a WebWorker.  These tests are a little crude to say the least but in theory they fail if it isn't working.

--- tests/_base/configWebWorker.js
+++ tests/_base/configWebWorker.js
@@ -1,6 +1,13 @@
 define([
     "doh/main"
 ], function(doh){
+    // summary:
+    //      Test the loading of Dojo in the WebWorker environment.
+
+    function isFunction(value){
+        var getType = {};
+        return value && getType.toString.call(value) === '[object Function]';
+    }
 
     var fixtures = {
         "blank": function(){},
@@ -15,11 +22,58 @@ define([
 
 
     doh.register("tests._base.configWebWorker", [{
-        "name": "TEST",
+        "name": "Platform has WebWorkers",
         "setUp": fixtures.blank,
         "tearDown": tearDowns.blank,
         "runTest": function(){
+            // summary:
+            //      This test will fail on older browsers, without workers.
 
+            doh.assertTrue(isFunction(Worker));
+        }
+    }, {
+        "name": "Loading Dojo core inside worker",
+        "setUp": fixtures.deferred,
+        "tearDown": tearDowns.blank,
+        "timeout": 5000,
+        "runTest": function(){
+            // summary:
+            //      Test whether dojo can be loaded in the worker
+
+            var self = this;
+            var worker = new Worker("../../dojo/tests/_base/configWebWorker/worker1.js");
+
+            worker.addEventListener("message", function(e) {
+                if(e.data.value){
+                    self.deferred.resolve();
+                }else{
+                    self.deferred.reject();
+                }
+            }, false);
+
+            return this.deferred;
+        }
+    }, {
+        "name": "Load a dojo script via require",
+        "setUp": fixtures.deferred,
+        "tearDown": tearDowns.blank,
+        "timeout": 5000,
+        "runTest": function(){
+            // summary:
+            //      Test whether require works in the worker.
+
+            var self = this;
+            var worker = new Worker("../../dojo/tests/_base/configWebWorker/worker2.js");
+
+            worker.addEventListener("message", function(e) {
+                if(e.data.value){
+                    self.deferred.resolve();
+                }else{
+                    self.deferred.reject();
+                }
+            }, false);
+
+            return this.deferred;
         }
     }]);
 });

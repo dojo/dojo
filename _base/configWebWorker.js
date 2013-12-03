b914111e7d3eb1e0c3a09a96f76c1d0f0ca58801
Stephen Simpson <me@simpo.org>
2013-12-03 14:01:08 +0000

    Fixed indentation inconsistencies.

--- _base/configWebWorker.js
+++ _base/configWebWorker.js
@@ -1,18 +1,18 @@
 function webworkerDojoConfig(config, global, has){
-	var hasCache = {
-		"host-browser": 0,
-		"dom": 0,
-		"dojo-dom-ready-api": 0,
-		"dojo-sniff": 0,
-		"dojo-inject-api": 1,
-		"host-webworker": 1
-	};
+    var hasCache = {
+        "host-browser": 0,
+        "dom": 0,
+        "dojo-dom-ready-api": 0,
+        "dojo-sniff": 0,
+        "dojo-inject-api": 1,
+        "host-webworker": 1
+    };
 
-	for(var p in hasCache){
-		config.hasCache[p] = hasCache[p];
-	}
+    for(var p in hasCache){
+        config.hasCache[p] = hasCache[p];
+    }
 
-	global.window = global;
+    global.window = global;
 
     var cacheBust = new Date().getTime().toString();
 
@@ -24,35 +24,35 @@ function webworkerDojoConfig(config, global, has){
         return url;
     };
 
-	// reset some configuration switches with webworker-appropriate values
-	var webworkerConfig = {
-		"loaderPatch": {
-			injectUrl: function(url, callback){
-				try{
-					importScripts(url);
-					callback();
-				}catch(e){
-					console.info("failed to load resource (" + url + ")");
-					console.error(e);
-				}
-			},
-			getText: function(url, async, onLoad){
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', fixupUrl(url), false);
-				xhr.send(null);
-				if(xhr.status == 200 || (!location.host && !xhr.status)){
-					if(onLoad){
-						onLoad(xhr.responseText, async);
-					}
-				}else{
-					console.error("xhrFailed", xhr.status);
-				}
-				return xhr.responseText;
-			}
-		}
-	};
+    // reset some configuration switches with webworker-appropriate values
+    var webworkerConfig = {
+        "loaderPatch": {
+            injectUrl: function(url, callback){
+                try{
+                    importScripts(url);
+                    callback();
+                }catch(e){
+                    console.info("failed to load resource (" + url + ")");
+                    console.error(e);
+                }
+            },
+            getText: function(url, async, onLoad){
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', fixupUrl(url), false);
+                xhr.send(null);
+                if(xhr.status == 200 || (!location.host && !xhr.status)){
+                    if(onLoad){
+                        onLoad(xhr.responseText, async);
+                    }
+                }else{
+                    console.error("xhrFailed", xhr.status);
+                }
+                return xhr.responseText;
+            }
+        }
+    };
 
-	for(p in webworkerConfig){
-		config[p] = webworkerConfig[p];
-	}
+    for(p in webworkerConfig){
+        config[p] = webworkerConfig[p];
+    }
 }

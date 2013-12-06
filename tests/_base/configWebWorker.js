37111b8eb0d3331cb06c20fb51d66ec604794b81
Stephen Simpson <me@simpo.org>
2013-12-06 14:23:23 +0000

    Test for blob workers added.

--- tests/_base/configWebWorker.js
+++ tests/_base/configWebWorker.js
@@ -80,20 +80,52 @@ define([
                 return this.deferred;
             }
         }, {
-            name: "Test for loading in a subworker",
+            name: "Test for loading in a blob worker",
             setUp: fixtures.deferred,
             timeout: 5000,
             runTest: function(){
                 // summary:
-                //      Test whether require works in the worker when in async mode.
+                //      Test for loading dojo and using require in a blob worker
+
+                function getBaseAbsoluteUrl(){
+                    // TODO:
+                    //      Is there a better way of calculating the absolute url base path?
+
+                    var baseUrl = require.rawConfig.baseUrl.split("/");
+                    var absoluteUrl = location.pathname.split("/");
+                    absoluteUrl.pop();
+                    baseUrl.pop();
+
+                    console.log(location.protocol+"//"+location.host+absoluteUrl.join("/")+"/"+baseUrl.join("/")+"/");
+
+                    return location.protocol+"//"+location.host+absoluteUrl.join("/")+"/"+baseUrl.join("/")+"/";
+                }
+
+                var blob = new Blob([
+                    'var dojoConfig = {' +
+                        '"baseUrl":"' + getBaseAbsoluteUrl() + '",' +
+                        '"packages":[{"name":"dojo", "location":"dojo"}]' +
+                    '};' +
+
+                    'importScripts(dojoConfig.baseUrl+"dojo/dojo.js");' +
+
+                    'try{'+
+                        'require(["dojo/_base/configWebWorker"], function(config){' +
+                            'this.postMessage({"test":"require is working", "value":true});' +
+                        '});' +
+                    '}catch(e){' +
+                        'this.postMessage({' +
+                            '"test":"require is working", "value":false' +
+                        '});' +
+                    '}'
+                ]);
 
                 var self = this;
-                var worker = new Worker("../../dojo/tests/_base/configWebWorker/worker4.js");
+                var blobURL = window.URL.createObjectURL(blob);
+                var worker = new Worker(blobURL);
+
                 worker.addEventListener("message", function(e) {
                     if(e.data.value){
-                        if(e.data.warn !== ""){
-                            console.warn(e.data.warn)
-                        }
                         self.deferred.resolve();
                     }else{
                         self.deferred.reject();

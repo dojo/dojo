424fe535b1c5cf80cdd4ba30173c9b5428c49b13
Stephen Simpson <me@simpo.org>
2013-12-16 14:17:08 +0000

    Dosen't look like this was committed previously! Added worker6.js test.

---
+++ tests/_base/loader/hostenv_webworkers/worker6.js
@@ -0,0 +1,43 @@
+// summary:
+//      Test the use of dojo/on without access to dom in a webworker.
+
+var dojoConfig = {
+    baseUrl:"../../../../../",
+    async: true,
+    packages:[
+        {name:"dojo", location:"dojo"}
+    ]
+}
+
+importScripts("../../../../dojo.js");
+
+var self = this;
+try{
+    require(["dojo/on"], function(on){
+        on(self, "message", function(message){
+            if(message.data.type === "gotMessage"){
+                this.postMessage({
+                    type:"testResult",
+                    test:"dojo/on in a worker is working",
+                    value:true
+                });
+            }else{
+                this.postMessage({
+                    type:"testResult",
+                    test:"dojo/on in a worker is working",
+                    value:false
+                });
+            }
+        });
+
+        this.postMessage({
+            type:"requestMessage"
+        });
+    });
+}catch(e){
+    this.postMessage({
+        type:"testResult",
+        test:"dojo/on in a worker is working",
+        value:false
+    });
+}

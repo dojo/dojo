cc9c7664b166cfa002e0ffebff9874d47375577f
Stephen Simpson <me@simpo.org>
2013-12-16 14:08:23 +0000

    Renamed the workers directory and test file so it better matches test names used elsewhere.

---
+++ tests/_base/loader/hostenv_webworkers/worker3.js
@@ -0,0 +1,26 @@
+// summary:
+//      Test whether the require function loads modules as it should in async mode.
+
+var dojoConfig = {
+    baseUrl:"../../../../../",
+    async: true,
+    packages:[
+        {name:"dojo", location:"dojo"}
+    ]
+}
+
+importScripts("../../../../dojo.js");
+
+try{
+    require(["dojo/tests/_base/loader/hostenv_webworkers/strings"], function(strings){
+        this.postMessage({
+            test:"require is working",
+            value:true
+        });
+    });
+}catch(e){
+    this.postMessage({
+        test:"require is working",
+        value:false
+    });
+}

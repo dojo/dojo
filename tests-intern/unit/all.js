810323e86692782b8d445dcfc2d044a18cf12119
dylans <dylan@dojotoolkit.org>
2014-01-14 10:45:34 -0600

    Initial DOH to Intern port of dojo/node tests

--- tests-intern/unit/all.js
+++ tests-intern/unit/all.js
@@ -11,5 +11,6 @@ define([
 	'intern/dojo/has!host-browser?./dom-form',
 	'intern/dojo/has!host-browser?./hash',
 	'intern/dojo/has!host-browser?./io/iframe',
+	'intern/dojo/has!host-node?./node',
 	'intern/dojo/has!host-browser?./router'
 ], function(){});

318d91e36776ac9c6ca80c67f69b20720b5ecb65
Bill Keese <bill@dojotoolkit.org>
2014-04-14 11:00:09 +0900

    escape dash in regexp.escapeString, fixes #17861

--- tests/regexp.js
+++ tests/regexp.js
@@ -4,6 +4,7 @@ doh.register("tests.regexp",
 		function test_regexp_escape(t){
 			t.assertTrue(new RegExp(regexp.escapeString("\f\b\n\t\r+.$?*|{}()[]\\/^")).test("TEST\f\b\n\t\r+.$?*|{}()[]\\/^TEST"));
 			t.assertTrue(new RegExp(regexp.escapeString("\f\b\n\t\r+.$?*|{}()[]\\/^", ".")).test("TEST\f\b\n\t\r+X$?*|{}()[]\\/^TEST"));
+			t.is("a\\-z", regexp.escapeString("a-z"));
 		}
 );
 

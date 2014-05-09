c68907df322eb146da4404f5058c6624994685f8
dylans <dylan@dojotoolkit.org>
2014-05-09 16:29:24 -0700

    fixes #17968, remove failing jsonp rpc test due to yahoo search API service no longer being available

--- tests/rpc.js
+++ tests/rpc.js
@@ -101,40 +101,6 @@ define(["doh/main", "require", "../rpc/RpcService", "../rpc/JsonService", "../rp
 					return new Error("Error loading and/or parsing an smd file");
 				}
 			}
-		},
-
-		{
-			name: "JsonP_test",
-			timeout: 10000,
-			setUp: function(){
-				this.svc = new JsonpService(require.toUrl("dojo/tests/resources/yahoo_smd_v1.smd"), {appid: "foo"});
-			},
-			runTest: function(){
-				var d = new doh.Deferred();
-
-				if (window.location.protocol=="file:"){
-					var err= new Error("This Test requires a webserver and will fail intentionally if loaded from file://");
-					d.errback(err);
-					return d;
-				}
-
-				var td = this.svc.webSearch({query:"dojotoolkit"});
-
-				td.addCallbacks(function(result){
-					return true;
-					if (result["ResultSet"]["Result"][0]["DisplayUrl"]=="dojotoolkit.org/"){
-						return true;
-					}else{
-						return new Error("JsonRpc_SMD_Loading_Test failed, resultant content didn't match");
-					}
-				}, function(result){
-					return new Error(result);
-				});
-
-				td.addBoth(d, "callback");
-
-				return d;
-			}
 		}
 	]);
 

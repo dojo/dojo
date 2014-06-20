191ce9d2a64c17f0748d2df5dd2f59f9f5940694
Bryan Forbes <bryan@reigndropsfall.net>
2014-06-20 17:34:34 -0500

    Only run mouse unit tests in a browser

--- tests-intern/unit/all.js
+++ tests-intern/unit/all.js
@@ -3,7 +3,6 @@ define([
 	'./AdapterRegistry',
 	'./colors',
 	'./io-query',
-	'./mouse',
 	'./request',
 	'./store/Memory',
 	'./string',
@@ -22,6 +21,7 @@ define([
 	'intern/dojo/has!host-browser?./dom-form',
 	'intern/dojo/has!host-browser?./hash',
 	'intern/dojo/has!host-browser?./io/iframe',
+	'intern/dojo/has!host-browser?./mouse',
 	'intern/dojo/has!host-node?./node',
 	'intern/dojo/has!host-browser?./require/require',
 	'intern/dojo/has!host-browser?./router'

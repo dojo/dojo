395cbc1cf9f4e72303268add46570aa52f0009d4
Bill Keese <bill@dojotoolkit.org>
2014-07-10 10:22:49 +0900

    Fail gracefully if there's an error detecting high contrast mode, fixes #16541.

--- hccss.js
+++ hccss.js
@@ -24,22 +24,25 @@ define([
 	has.add("highcontrast", function(){
 		// note: if multiple documents, doesn't matter which one we use
 		var div = win.doc.createElement("div");
-		div.style.cssText = "border: 1px solid; border-color:red green; position: absolute; height: 5px; top: -999px;" +
-			"background-image: url(\"" + (config.blankGif || require.toUrl("./resources/blank.gif")) + "\");";
-		win.body().appendChild(div);
+		try{
+			div.style.cssText = "border: 1px solid; border-color:red green; position: absolute; height: 5px; top: -999px;" +
+				"background-image: url(\"" + (config.blankGif || require.toUrl("./resources/blank.gif")) + "\");";
+			win.body().appendChild(div);
 
-		var cs = domStyle.getComputedStyle(div),
-			bkImg = cs.backgroundImage,
-			hc = (cs.borderTopColor == cs.borderRightColor) ||
+			var cs = domStyle.getComputedStyle(div),
+				bkImg = cs.backgroundImage;
+			return cs.borderTopColor == cs.borderRightColor ||
 				(bkImg && (bkImg == "none" || bkImg == "url(invalid-url:)" ));
-
-		if(has("ie") <= 8){
-			div.outerHTML = "";		// prevent mixed-content warning, see http://support.microsoft.com/kb/925014
-		}else{
-			win.body().removeChild(div);
+		}catch(e){
+			console.warn("hccss: exception detecting high-contrast mode, document is likely hidden: " + e.toString());
+			return false;
+		}finally{
+			if(has("ie") <= 8){
+				div.outerHTML = "";		// prevent mixed-content warning, see http://support.microsoft.com/kb/925014
+			}else{
+				win.body().removeChild(div);
+			}
 		}
-
-		return hc;
 	});
 
 	domReady(function(){

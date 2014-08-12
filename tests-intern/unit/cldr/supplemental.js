becccd9524832577ba809708b4898224c84e425b
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2014-08-12 11:35:06 -0400

    cldr/supplemental Intern tests: Add getFirstDayOfWeek, use strictEqual

--- tests-intern/unit/cldr/supplemental.js
+++ tests-intern/unit/cldr/supplemental.js
@@ -1,16 +1,22 @@
 define([
 	'intern!object',
 	'intern/chai!assert',
-	'dojo/cldr/supplemental'
+	'dojo-testing/cldr/supplemental'
 ], function (registerSuite, assert, supplemental) {
 	registerSuite({
 		name: 'dojo/cldr/supplemental',
 
+		'.getFirstDayOfWeek': function () {
+			assert.strictEqual(supplemental.getFirstDayOfWeek('en-us'), 0);
+			assert.strictEqual(supplemental.getFirstDayOfWeek('en-gb'), 1);
+			assert.strictEqual(supplemental.getFirstDayOfWeek('es'), 1);
+		},
+
 		'.getWeekend': function () {
-			assert.equal(supplemental.getWeekend('en-us').start, 6);
-			assert.equal(supplemental.getWeekend('en').end, 0);
-			assert.equal(supplemental.getWeekend('he-il').start, 5);
-			assert.equal(supplemental.getWeekend('he').end, 6);
+			assert.strictEqual(supplemental.getWeekend('en-us').start, 6);
+			assert.strictEqual(supplemental.getWeekend('en').end, 0);
+			assert.strictEqual(supplemental.getWeekend('he-il').start, 5);
+			assert.strictEqual(supplemental.getWeekend('he').end, 6);
 		}
 	});
 });

33e102dfe99c201efdb817931167261cf4985e9b
Colin Snover <github.com@zetafleet.com>
2014-08-23 05:42:28 +0000

    Fix bad logic in xhr that tries to avoid double-setting Content-Type
    Fixes #18237.

--- _base/xhr.js
+++ _base/xhr.js
@@ -710,8 +710,10 @@ define([
 		xhr.open(method, ioArgs.url, args.sync !== true, args.user || undefined, args.password || undefined);
 		if(args.headers){
 			for(var hdr in args.headers){
-				if(hdr.toLowerCase() === "content-type" && !args.contentType){
-					args.contentType = args.headers[hdr];
+				if(hdr.toLowerCase() === "content-type"){
+					if(!args.contentType){
+						args.contentType = args.headers[hdr];
+					}
 				}else if(args.headers[hdr]){
 					//Only add header if it has a value. This allows for instnace, skipping
 					//insertion of X-Requested-With by specifying empty value.

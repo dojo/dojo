f9262e835e411592de43744e10cfae6e6fc9eea9
Dylan Schiemann <dylan@dojotoolkit.org>
2014-08-30 05:57:04 -0700

    refs #18225, dom-construct inline documentation update

--- dom-construct.js
+++ dom-construct.js
@@ -231,24 +231,24 @@ define(["exports", "./_base/kernel", "./sniff", "./_base/window", "./dom", "./do
 		//		a fragment, and allowing for a convenient optional attribute setting step,
 		//		as well as an optional DOM placement reference.
 		//
-		//		Attributes are set by passing the optional object through `dojo.setAttr`.
-		//		See `dojo.setAttr` for noted caveats and nuances, and API if applicable.
+		//		Attributes are set by passing the optional object through `dojo/dom-attr.set`.
+		//		See `dojo/dom-attr.set` for noted caveats and nuances, and API if applicable.
 		//
-		//		Placement is done via `dojo.place`, assuming the new node to be the action
-		//		node, passing along the optional reference node and position.
+		//		Placement is done via `dojo/dom-construct.place`, assuming the new node to be
+		//		the action node, passing along the optional reference node and position.
 		// tag: DOMNode|String
 		//		A string of the element to create (eg: "div", "a", "p", "li", "script", "br"),
 		//		or an existing DOM node to process.
 		// attrs: Object
 		//		An object-hash of attributes to set on the newly created node.
 		//		Can be null, if you don't want to set any attributes/styles.
-		//		See: `dojo.setAttr` for a description of available attributes.
+		//		See: `dojo/dom-attr.set` for a description of available attributes.
 		// refNode: DOMNode|String?
-		//		Optional reference node. Used by `dojo.place` to place the newly created
+		//		Optional reference node. Used by `dojo/dom-construct.place` to place the newly created
 		//		node somewhere in the dom relative to refNode. Can be a DomNode reference
 		//		or String ID of a node.
 		// pos: String?
-		//		Optional positional reference. Defaults to "last" by way of `dojo.place`,
+		//		Optional positional reference. Defaults to "last" by way of `dojo/domConstruct.place`,
 		//		though can be set to "first","after","before","last", "replace" or "only"
 		//		to further control the placement of the new node relative to the refNode.
 		//		'refNode' is required if a 'pos' is specified.
@@ -266,8 +266,8 @@ define(["exports", "./_base/kernel", "./sniff", "./_base/window", "./dom", "./do
 		//
 		// example:
 		//		Place a new DIV in the BODY, with no attributes set
-		//	|	require(["dojo/dom-construct"], function(domConstruct){
-		//	|		var n = domConstruct.create("div", null, dojo.body());
+		//	|	require(["dojo/dom-construct", "dojo/_base/window"], function(domConstruct, win){
+		//	|		var n = domConstruct.create("div", null, win.body());
 		//	|	});
 		//
 		// example:
@@ -284,8 +284,8 @@ define(["exports", "./_base/kernel", "./sniff", "./_base/window", "./dom", "./do
 		//
 		// example:
 		//		Create an anchor, with an href. Place in BODY:
-		//	|	require(["dojo/dom-construct"], function(domConstruct){
-		//	|		domConstruct.create("a", { href:"foo.html", title:"Goto FOO!" }, dojo.body());
+		//	|	require(["dojo/dom-construct", "dojo/_base/window"], function(domConstruct, win){
+		//	|		domConstruct.create("a", { href:"foo.html", title:"Goto FOO!" }, win.body());
 		//	|	});
 
 		var doc = win.doc;

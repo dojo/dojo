c6363807b518d108775d48322e07372b63784829
Bryan Forbes <bryan@reigndropsfall.net>
2014-09-26 16:03:11 -0500

    Don't load _base/browser tests in non-browser environments

--- tests-intern/unit/_base.js
+++ tests-intern/unit/_base.js
@@ -1,9 +1,9 @@
 define([
 	'./_base/array',
-	'./_base/browser',
 	'./_base/Color',
 	'./_base/connect',
 	'./_base/declare',
 	'./_base/lang',
+	'dojo/has!host-browser?./_base/browser',
 	'dojo/has!host-browser?./_base/xhr'
 ], function () {});

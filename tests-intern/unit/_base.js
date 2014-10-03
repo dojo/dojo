f24470f0bee18d374cd21719efe1e53c50d5025b
James Morrin <jmorrin@sitepen.com>
2014-10-03 17:42:26 -0500

    Converted dojo/_base/Deferred to intern.

--- tests-intern/unit/_base.js
+++ tests-intern/unit/_base.js
@@ -3,6 +3,7 @@ define([
 	'./_base/Color',
 	'./_base/connect',
 	'./_base/declare',
+	'./_base/Deferred',
 	'./_base/lang',
 	'dojo/has!host-browser?./_base/browser',
 	'dojo/has!host-browser?./_base/xhr'

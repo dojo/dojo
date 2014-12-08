2290375e2b7555991080bdcb6a97d633fb6c1999
Colin Snover <github.com@zetafleet.com>
2014-12-08 09:11:46 -0800

    Updating source version to 1.5.5-pre

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -218,7 +218,7 @@ dojo.global = {
 =====*/
 	dojo.locale = d.config.locale;
 
-	var rev = "$Rev: 4695f6a $".match(/[0-9a-f]{7,}/);
+	var rev = "$Rev$".match(/[0-9a-f]{7,}/);
 
 /*=====
 	dojo.version = function(){
@@ -242,7 +242,7 @@ dojo.global = {
 	}
 =====*/
 	dojo.version = {
-		major: 1, minor: 5, patch: 4, flag: "",
+		major: 1, minor: 5, patch: 5, flag: "-pre",
 		revision: rev ? rev[0] : NaN,
 		toString: function(){
 			with(d.version){

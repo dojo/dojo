eebba42d8955a5528a4afe25bd7dd39b58111435
MikeMac <mikemaccs@gmail.com>
2014-12-09 16:46:36 -0500

    Add finally method to Promises

--- tests/promise/Promise.js
+++ tests/promise/Promise.js
@@ -32,6 +32,42 @@ define([
 			t.t(otherwiseResult === thenResult);
 		},
 
+		"finally(…) is always called": function(t){
+			var obj = {};
+			var deferred1 = new Deferred();
+			var thenResult, finallyResult;
+			deferred1.promise.then(function(result){ thenResult = result; });
+			deferred1.promise.finally(function(result){ finallyResult = result; });
+			deferred1.resolve(obj);
+			t.t(finallyResult === obj);
+			t.t(finallyResult === thenResult);
+
+			var deferred2 = new Deferred();
+			var thenResult2, finallyResult2;
+			deferred2.promise.then(null, function(result){ thenResult2 = result; });
+			deferred2.promise.finally(function(result){ finallyResult2 = result; });
+			deferred2.reject(obj);
+			t.t(finallyResult2 === obj);
+			t.t(finallyResult2 === thenResult2);
+		},
+
+		"Promise from finally(…) is resolved or rejected with original resolved or rejected value": function(t){
+			var obj = {};
+			var deferred1 = new Deferred();
+			var finallyResult;
+			var finallyPromise1 = deferred1.promise.finally(function(result){ finallyResult = result; });
+			deferred1.resolve(obj);
+			t.t(finallyResult === obj);
+			t.t(finallyPromise1.isResolved());
+
+			var deferred2 = new Deferred();
+			var finallyResult2;
+			var finallyPromise2 = deferred2.promise.finally(function(result){ finallyResult2 = result; });
+			deferred2.reject(obj);
+			t.t(finallyResult2 === obj);
+			t.t(finallyPromise2.isRejected());
+		},
+
 		"trace() returns the same promise": function(t){
 			var promise = this.deferred.promise.trace();
 			t.t(promise === this.deferred.promise);
@@ -53,7 +89,7 @@ define([
 	}
 
 	function setUp(){
-		this.deferred = new Deferred;
+		this.deferred = new Deferred();
 	}
 
 	doh.register("tests.promise.Promise", wrapped);

2fe9c1b4625f260449348bbc6fa7c7a67d4e2e34
Bryan Forbes <bryan@reigndropsfall.net>
2014-12-17 07:02:45 -0600

    Add failing tests for yet-to-be-converted tests

---
+++ tests/unit/_base/window.js
@@ -0,0 +1,12 @@
+define([
+	'intern!object',
+	'intern/chai!assert'
+], function (registerSuite, assert) {
+	registerSuite({
+		name: 'dojo/_base/window',
+
+		'TODO: convert tests': function () {
+			assert.fail();
+		}
+	});
+});
---
+++ tests/unit/throttle.js
@@ -0,0 +1,12 @@
+define([
+	'intern!object',
+	'intern/chai!assert'
+], function (registerSuite, assert) {
+	registerSuite({
+		name: 'dojo/throttle',
+
+		'TODO: convert tests': function () {
+			assert.fail();
+		}
+	});
+});

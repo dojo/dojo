87b41acd0a70b8cd2593ba69261ec56ca07dd438
Bill Keese <bill@dojotoolkit.org>
2015-03-18 09:57:54 +0900

    Clean up parseOnLoadDeclarativeRequire test so it can be run standalone, independent of Intern.  Refs #18427.

--- tests/functional/parser/parseOnLoadDeclarativeRequire.js
+++ tests/functional/parser/parseOnLoadDeclarativeRequire.js
@@ -9,23 +9,17 @@ define([
 
 		test: function () {
 			return ready(this.get('remote'), require.toUrl('./parseOnLoadDeclarativeRequire.html'))
-				.setExecuteAsyncTimeout(5000)
-				.executeAsync(function (done) {
-					require([ 'dojo/ready' ], function (dojoReady) {
-						dojoReady(function () {
-							/* global dr1, dr2, dr3, dr4, dr5 */
-							done({
-								typeofDr1: typeof dr1,
-								dr1Foo: dr1.params.foo,
-								typeofDr2: typeof dr2,
-								dr2Foo: dr2.params.foo,
-								typeofDr3: typeof dr3,
-								dr3Foo: dr3.params.foo,
-								dr4Foo: dr4.params.foo,
-								dr5Method1: dr5.method1(1)
-							});
-						});
-					});
+				.execute(function () {
+					return {
+						typeofDr1: typeof dr1,
+						dr1Foo: dr1.params.foo,
+						typeofDr2: typeof dr2,
+						dr2Foo: dr2.params.foo,
+						typeofDr3: typeof dr3,
+						dr3Foo: dr3.params.foo,
+						dr4Foo: dr4.params.foo,
+						dr5Method1: dr5.method1(1)
+					};
 				})
 				.then(function (results) {
 					assert.deepEqual(results, {

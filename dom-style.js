eaaaa2287584c2bc2f091b3e4e843c10584b316b
Bill Keese <bill@dojotoolkit.org>
2015-10-01 10:18:36 +0900

    Run code to correct style values on all browsers. Fixes failure on Edge.  Fixes #18725.

--- dom-style.js
+++ dom-style.js
@@ -199,19 +199,20 @@ define(["./sniff", "./dom"], function(has, dom){
 	function _toStyleValue(node, type, value){
 		//TODO: should we really be doing string case conversion here? Should we cache it? Need to profile!
 		type = type.toLowerCase();
-		if(has("ie") || has("trident")){
-			if(value == "auto"){
-				if(type == "height"){ return node.offsetHeight; }
-				if(type == "width"){ return node.offsetWidth; }
-			}
-			if(type == "fontweight"){
-				switch(value){
-					case 700: return "bold";
-					case 400:
-					default: return "normal";
-				}
+
+		// Adjustments for IE and Edge
+		if(value == "auto"){
+			if(type == "height"){ return node.offsetHeight; }
+			if(type == "width"){ return node.offsetWidth; }
+		}
+		if(type == "fontweight"){
+			switch(value){
+				case 700: return "bold";
+				case 400:
+				default: return "normal";
 			}
 		}
+
 		if(!(type in _pixelNamesCache)){
 			_pixelNamesCache[type] = _pixelRegExp.test(type);
 		}

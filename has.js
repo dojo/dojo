1d3a775302afdf33df9dea85457625aa9860de71
Bill Keese <bill@dojotoolkit.org>
2015-11-23 11:40:06 +0900

    Fix has("MSPointer") and has("pointer-events") on embedded C# WebBrowser Controls. Fixes #18602 again.  First try apparently didn't work.

--- has.js
+++ has.js
@@ -109,9 +109,13 @@ define(["require", "module"], function(require, module){
 		// Touch events support
 		has.add("touch-events", "ontouchstart" in document);
 
-		// Pointer Events support
-		has.add("pointer-events", "onpointerdown" in document);
-		has.add("MSPointer", "MSPointerEvent" in window); //IE10 (+IE11 preview)
+		// Test if pointer events are supported and enabled, with either standard names ("pointerdown" etc.) or
+		// IE specific names ("MSPointerDown" etc.).  Tests are designed to work on embedded C# WebBrowser Controls
+		// in addition to IE, Edge, and future versions of Firefox and Chrome.
+		// Note that on IE11, has("pointer-events") and has("MSPointer") are both true.
+		has.add("pointer-events", "pointerEnabled" in window.navigator ?
+				window.navigator.pointerEnabled : "PointerEvent" in window);
+		has.add("MSPointer", window.navigator.msPointerEnabled);
 
 		// I don't know if any of these tests are really correct, just a rough guess
 		has.add("device-width", screen.availWidth || innerWidth);

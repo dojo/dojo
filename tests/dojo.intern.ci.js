c3af8cd843ea3d5321340849e7bd54d8f202fef9
Kitson Kelly <me@kitsonkelly.com>
2015-12-31 16:27:19 +0000

    Move back to BrowserStack, add code covergae uploading

--- tests/dojo.intern.ci.js
+++ tests/dojo.intern.ci.js
@@ -1,32 +1,22 @@
 define([
 	'./dojo.intern'
 ], function (intern) {
+	intern.tunnel = 'BrowserStackTunnel';
+
 	intern.capabilities = {
+		'selenium-version': '2.43.1',
+		'idle-timeout': 30,
 		project: 'Dojo',
-		name: 'dojo/dojo',
-		'record-screenshots': false,
-		'sauce-advisor': false,
-		'video-upload-on-pass': false,
-		'max-duration': 2100  // test can seriously take over 30 minutes!!!
+		name: 'dojo/dojo'
 	};
 
 	intern.environments = [
-		{ browserName: 'internet explorer', version: [ '6', '7' ], platform: 'Windows XP',
-			'prerun': 'http://localhost:9001/tests/support/prerun.bat' },
-		{ browserName: 'internet explorer', version: [ '8', '9', '10' ], platform: 'Windows 7',
-		 	'prerun': 'http://localhost:9001/tests/support/prerun.bat' },
-		// { browserName: 'internet explorer', version: '10', platform: 'Windows 8',
-		// 	'prerun': 'http://localhost:9001/tests/support/prerun.bat' },
-		// { browserName: 'internet explorer', version: '11', platform: 'Windows 10',
-			// 'prerun': 'http://localhost:9001/tests/support/prerun.bat' },
-		{ browserName: 'firefox', platform: [ 'OS X 10.11', 'Windows 7', 'Windows 10' ] },
-		{ browserName: 'chrome', platform: [ 'OS X 10.11', 'Windows 7', 'Windows 10' ] }
-		// { browserName: 'safari', version: '7', platform: 'OS X 10.9' },
-		// { browserName: 'safari', version: '8', platform: 'OS X 10.10' },
-		// { browserName: 'safari', version: '9', platform: 'OS X 10.11' }
+		{ browserName: 'internet explorer', version: [ '6', '7', '8', '9', '10', '11' ], platform: 'WINDOWS' },
+		{ browserName: 'firefox', platform: [ 'WINDOWS', 'MAC' ] },
+		{ browserName: 'chrome', platform: [ 'WINDOWS', 'MAC' ] },
 	];
 
-	intern.maxConcurrency = 5;
+	intern.maxConcurrency = 1;
 
 	return intern;
 });

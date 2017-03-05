cacdabeb7c4be172421add7e7356d9e95202479e
Dylan Schiemann <dylan@dojotoolkit.org>
2017-03-05 07:40:15 -0800

    Updating metadata for 1.4.8

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -218,7 +218,7 @@ dojo.global = {
 =====*/
 	dojo.locale = d.config.locale;
 
-	var rev = "$Rev$".match(/[0-9a-f]{7,}/);
+	var rev = "$Rev: f7c2965 $".match(/[0-9a-f]{7,}/);
 
 /*=====
 	dojo.version = function(){
@@ -242,7 +242,7 @@ dojo.global = {
 	}
 =====*/
 	dojo.version = {
-		major: 1, minor: 4, patch: 8, flag: "-pre",
+		major: 1, minor: 4, patch: 8, flag: "",
 		revision: rev ? rev[0] : NaN,
 		toString: function(){
 			with(d.version){

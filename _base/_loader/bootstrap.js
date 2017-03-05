e01f9491beed20743d5d3ce8fb8926b32014cf03
Dylan Schiemann <dylan@dojotoolkit.org>
2017-03-05 07:40:15 -0800

    Updating source version to 1.4.9-pre

--- _base/_loader/bootstrap.js
+++ _base/_loader/bootstrap.js
@@ -218,7 +218,7 @@ dojo.global = {
 =====*/
 	dojo.locale = d.config.locale;
 
-	var rev = "$Rev: f7c2965 $".match(/[0-9a-f]{7,}/);
+	var rev = "$Rev$".match(/[0-9a-f]{7,}/);
 
 /*=====
 	dojo.version = function(){
@@ -242,7 +242,7 @@ dojo.global = {
 	}
 =====*/
 	dojo.version = {
-		major: 1, minor: 4, patch: 8, flag: "",
+		major: 1, minor: 4, patch: 9, flag: "-pre",
 		revision: rev ? rev[0] : NaN,
 		toString: function(){
 			with(d.version){

7ce6a2ae400be9772ff076b96ce884bef5968a27
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-04-03 21:48:07 -0700

    Fixes 18998
    Use fully-qualified URL in `request/xhr`'s `has('native-xhr2-blob)'`.
    Relative URLs with XMLHttpRequest do not work in a Web Worker generated from a blob.

--- request/xhr.js
+++ request/xhr.js
@@ -34,7 +34,9 @@ define([
 	has.add('native-xhr2-blob', function(){
 		if(!has('native-response-type')){ return; }
 		var x = new XMLHttpRequest();
-		x.open('GET', '/', true);
+		// URL should be fully-qualified as XMLHTTPRequest within a Worker
+		// created from a Blob does not support relative URL paths
+		x.open('GET', 'https://dojotoolkit.org/', true);
 		x.responseType = 'blob';
 		// will not be set if unsupported
 		var responseType = x.responseType;

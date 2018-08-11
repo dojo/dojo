ae28982346f272db3ce8776b619d2b4c7eabea0c
dylans <dylan@dojotoolkit.org>
2018-08-11 16:04:37 -0700

    Update source version to 1.15.0-pre

--- _base/kernel.js
+++ _base/kernel.js
@@ -79,7 +79,7 @@ define(["../global", "../has", "./config", "require", "module"], function(global
 	dojo.isAsync = !has("dojo-loader") || require.async;
 	dojo.locale = config.locale;
 
-	var rev = "$Rev: b27d4da $".match(/[0-9a-f]{7,}/);
+	var rev = "$Rev:$".match(/[0-9a-f]{7,}/);
 	dojo.version = {
 		// summary:
 		//		Version number of the Dojo Toolkit
@@ -92,7 +92,7 @@ define(["../global", "../has", "./config", "require", "module"], function(global
 		//		- flag: String: Descriptor flag. If total version is "1.2.0beta1", will be "beta1"
 		//		- revision: Number: The Git rev from which dojo was pulled
 
-		major: 1, minor: 14, patch: 0, flag: "",
+		major: 1, minor: 15, patch: 0, flag: "-pre",
 		revision: rev ? rev[0] : NaN,
 		toString: function(){
 			var v = dojo.version;

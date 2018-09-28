19236126890950e77bf13ac3f84f9f3725d16a9a
David Schontzler <2080286+schontz@users.noreply.github.com>
2018-09-28 05:35:21 -0700

    fix for 320 (#321)
    Add URL to scriptError
    (cherry picked from commit 139903007452bdc53f474fb5fbf2844261408bae)

--- dojo.js
+++ dojo.js
@@ -1660,7 +1660,7 @@
 					errorDisconnector = domOn(node, "error", "onerror", function(e){
 						loadDisconnector();
 						errorDisconnector();
-						signal(error, makeError("scriptError", [url, e]));
+						signal(error, makeError("scriptError: " + url, [url, e]));
 					});
 
 				node.type = "text/javascript";

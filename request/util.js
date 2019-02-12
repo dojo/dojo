a00abff885e360784d61f8e44969d3be42d5effd
Gerhard Presser <gerhard.presser@groiss.com>
2019-02-12 15:12:52 +0100

    handle 'window' not being a global object in node.js

--- request/util.js
+++ request/util.js
@@ -10,8 +10,6 @@ define([
 	'../has'
 ], function(exports, RequestError, CancelError, Deferred, ioQuery, array, lang, Promise, has){
 
-	var HAS_FORM_ELEMENT = !!window.HTMLFormElement;
-	
 	function isArrayBuffer(value) {
 		return has('native-arraybuffer') && value instanceof ArrayBuffer
 	}
@@ -21,8 +19,11 @@ define([
 	}
 	
 	function isFormElement(value) {
-		return HAS_FORM_ELEMENT && value instanceof HTMLFormElement //all other
-				|| !HAS_FORM_ELEMENT && value.tagName === "FORM"; //IE<=7
+		if(typeof HTMLFormElement !== 'undefined') { //all other
+			return value instanceof HTMLFormElement;
+		} else { //IE<=7
+			value.tagName === "FORM"
+		}
 	}
 
 	function isFormData(value) {
